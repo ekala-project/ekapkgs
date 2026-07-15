1c0acc6cfad5afdb5a12fb611a3855e75b619000
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:01:53 -0700

    libmikmod: init at 3.3.13

---
+++ pkgs/libmikmod/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  texinfo,
+  alsa-lib,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libmikmod";
+  version = "3.3.13";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/mikmod/libmikmod-${finalAttrs.version}.tar.gz";
+    sha256 = "sha256-n8F5n36mqVx8WILemL6F/H0gugpKb8rK4RyMazgrsgc=";
+  };
+
+  buildInputs = [ texinfo ] ++ lib.optional stdenv.hostPlatform.isLinux alsa-lib;
+
+  outputs = [
+    "out"
+    "dev"
+    "man"
+  ];
+
+  env = lib.optionalAttrs stdenv.hostPlatform.isLinux {
+    NIX_LDFLAGS = "-lasound";
+  };
+
+  enableParallelBuilding = true;
+
+  postInstall = ''
+    moveToOutput bin/libmikmod-config "$dev"
+  '';
+
+  meta = {
+    description = "Library for playing tracker music module files";
+    mainProgram = "libmikmod-config";
+    homepage = "https://mikmod.shlomifish.org/";
+    license = lib.licenses.lgpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
