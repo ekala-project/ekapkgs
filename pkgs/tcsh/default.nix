a11fcc2dfdfe27f9c6739971ab71a569baf43fea
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 11:46:40 -0700

    tcsh: init at 6.24.16

---
+++ pkgs/tcsh/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  libxcrypt,
+  ncurses,
+  buildPackages,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tcsh";
+  version = "6.24.16";
+
+  src = fetchurl {
+    url = "mirror://tcsh/tcsh-${finalAttrs.version}.tar.gz";
+    hash = "sha256-QgjPRjD7ZNkdgZh/hU+VcKWg6KABqSgn3vN9Dtjzc2Q=";
+  };
+
+  strictDeps = true;
+
+  depsBuildBuild = [
+    buildPackages.stdenv.cc
+  ];
+
+  buildInputs = [
+    libxcrypt
+    ncurses
+  ];
+
+  passthru.shellPath = "/bin/tcsh";
+
+  meta = {
+    homepage = "https://www.tcsh.org/";
+    description = "Enhanced version of the Berkeley UNIX C shell (csh)";
+    mainProgram = "tcsh";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
