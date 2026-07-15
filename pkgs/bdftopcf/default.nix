c27626ebb5999e88434ff412b12565ee378e90f9
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 15:12:53 -0700

    bdftopcf: init at 1.1.2

---
+++ pkgs/bdftopcf/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  xorgproto,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "bdftopcf";
+  version = "1.1.2";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/util/bdftopcf-${finalAttrs.version}.tar.xz";
+    hash = "sha256-vGC+WQQzD6qj3dKu14dL7i8p5Dh8JF1nh1UvBn6wUjo=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ xorgproto ];
+
+  meta = {
+    description = "Converts X font from Bitmap Distribution Format to Portable Compiled Format";
+    homepage = "https://gitlab.freedesktop.org/xorg/util/bdftopcf";
+    license = lib.licenses.mitOpenGroup;
+    mainProgram = "bdftopcf";
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
