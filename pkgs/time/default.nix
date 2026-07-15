850b5b0d062f0ec82996424c9a8967849f30754c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 13:02:06 -0700

    time: init at 1.10

---
+++ pkgs/time/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "time";
+  version = "1.10";
+
+  src = fetchurl {
+    url = "mirror://gnu/time/time-${finalAttrs.version}.tar.xz";
+    hash = "sha256-cGv3uERMqeuQN+ntoY4dDrfCMnrn2MLOOkgjxfgMexE=";
+  };
+
+  meta = {
+    description = "Tool that runs programs and summarizes the system resources they use";
+    license = lib.licenses.gpl3Plus;
+    homepage = "https://www.gnu.org/software/time/";
+    mainProgram = "time";
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
