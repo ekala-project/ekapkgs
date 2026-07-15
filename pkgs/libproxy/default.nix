5d3bb5d1c50fb42019eba368ffa42b5bd94c3372
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 12:20:31 -0700

    libproxy: init at 0.5.9

---
+++ pkgs/libproxy/default.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  curl,
+  duktape,
+  glib,
+  meson,
+  ninja,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libproxy";
+  version = "0.5.9";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "libproxy";
+    repo = "libproxy";
+    rev = finalAttrs.version;
+    hash = "sha256-Z70TjLk5zulyYMAK+uMDhpsdvLa6m25pY8jahUA6ASE=";
+  };
+
+  postPatch = ''
+    chmod +x data/install-git-hook.sh
+    patchShebangs data/install-git-hook.sh
+
+    substituteInPlace src/libproxy/meson.build \
+      --replace-fail "requires_private: 'gobject-2.0'" "requires: 'gobject-2.0'"
+  '';
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    curl
+    duktape
+    glib
+  ];
+
+  mesonFlags = [
+    "-Drelease=true"
+    "-Ddocs=false"
+    "-Dintrospection=false"
+    "-Dconfig-gnome=false"
+  ];
+
+  meta = {
+    description = "Library that provides automatic proxy configuration management";
+    homepage = "https://libproxy.github.io/libproxy/";
+    license = lib.licenses.lgpl21Plus;
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    mainProgram = "proxy";
+    maintainers = [ ];
+  };
+})
