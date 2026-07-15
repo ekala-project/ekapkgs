1346a74582c06addf59ea48a2a7b7f8edf647816
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 13:30:20 -0700

    frei0r: init at 2.5.1

---
+++ pkgs/frei0r/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cairo,
+  cmake,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "frei0r-plugins";
+  version = "2.5.1";
+
+  src = fetchFromGitHub {
+    owner = "dyne";
+    repo = "frei0r";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-3gUWvO5izOrJt+XwcNBNiLfu+iMqo4nuPbx++TYzao0=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    pkg-config
+  ];
+
+  buildInputs = [
+    cairo
+  ];
+
+  meta = {
+    homepage = "https://frei0r.dyne.org";
+    description = "Minimalist, cross-platform, shared video plugins";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
