5f9a25de938b291aa0fe988ca7757864ebfd41a1
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:58:19 -0700

    tslib: init at 1.24

---
+++ pkgs/tslib/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tslib";
+  version = "1.24";
+
+  src = fetchFromGitHub {
+    owner = "libts";
+    repo = "tslib";
+    tag = finalAttrs.version;
+    hash = "sha256-WrzOTZlceYnFXi5AI5vb+ZDSRoqUDk/yyCdBUWKn0sM=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  meta = {
+    description = "Touchscreen access library";
+    homepage = "http://www.tslib.org/";
+    license = lib.licenses.lgpl21;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
