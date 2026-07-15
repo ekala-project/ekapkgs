e488441eeb4f0fc6e4431f312f80bea937e7be6e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 12:01:57 -0700

    atkmm: init at 2.28.4

---
+++ pkgs/atkmm/default.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  atk,
+  glibmm,
+  pkg-config,
+  meson,
+  ninja,
+  python3,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "atkmm";
+  version = "2.28.4";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
+    sha256 = "sha256-ChQqgSj4PAAe+4AU7kY+mnZgVO+EaGr5UxNeBNKP2rM=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  propagatedBuildInputs = [
+    atk
+    glibmm
+  ];
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    meson.configurePhaseHook
+    ninja
+    python3
+  ];
+
+  doCheck = true;
+
+  meta = {
+    description = "C++ wrappers for ATK accessibility toolkit";
+    homepage = "https://gtkmm.org";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
