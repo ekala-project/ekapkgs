3a56804ac448f780f2a893b53f7a4b9f41fdbf1c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 13:00:33 -0700

    libatomic_ops: init at 7.10.0

---
+++ pkgs/libatomic_ops/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libatomic_ops";
+  version = "7.10.0";
+
+  src = fetchurl {
+    urls = [
+      "http://www.ivmaisoft.com/_bin/atomic_ops/libatomic_ops-${finalAttrs.version}.tar.gz"
+      "https://github.com/ivmai/libatomic_ops/releases/download/v${finalAttrs.version}/libatomic_ops-${finalAttrs.version}.tar.gz"
+    ];
+    sha256 = "sha256-DbPr/3VdsXD2XnSmTsRRGBLp7jGFwjLu/+rNJ0GQ37A=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+    "doc"
+  ];
+
+  meta = {
+    description = "Library for semi-portable access to hardware-provided atomic memory update operations";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
