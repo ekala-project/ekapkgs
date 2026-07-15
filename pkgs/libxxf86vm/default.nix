6e9851816b4f2de4516ff1fbb919a4d6cdbc20e0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 12:07:36 -0700

    libxxf86vm: init at 1.1.6

---
+++ pkgs/libxxf86vm/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  libx11,
+  libxext,
+  xorgproto,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libxxf86vm";
+  version = "1.1.6";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/lib/libXxf86vm-${finalAttrs.version}.tar.xz";
+    sha256 = "1qryzfzf3qr2xx1sipdn8kw310zs4ygpzgh4mm4m87fffd643bwn";
+  };
+
+  hardeningDisable = [
+    "bindnow"
+    "relro"
+  ];
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    libx11
+    libxext
+    xorgproto
+  ];
+
+  meta = {
+    description = "X11 XFree86 video mode extension library";
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libxxf86vm";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
