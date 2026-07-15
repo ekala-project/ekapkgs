237be4047920bbd5f748c8d7657dc329199ca684
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:26:33 -0700

    lshw: init at 02.20

---
+++ pkgs/lshw/default.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  hwdata,
+  pkg-config,
+  gettext,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "lshw";
+  version = "02.20";
+
+  src = fetchFromGitHub {
+    owner = "lyonel";
+    repo = "lshw";
+    rev = "B.${finalAttrs.version}";
+    hash = "sha256-4etC7ymMgn1Q4f98DNASv8vn0AT55dYPdacZo6GRDw0=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    gettext
+  ];
+
+  buildInputs = [
+    hwdata
+  ];
+
+  makeFlags = [
+    "PREFIX=$(out)"
+    "VERSION=${finalAttrs.src.rev}"
+  ];
+
+  hardeningDisable = lib.optionals stdenv.hostPlatform.isStatic [ "fortify" ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "Provide detailed information on the hardware configuration of the machine";
+    homepage = "https://ezix.org/project/wiki/HardwareLiSter";
+    license = lib.licenses.gpl2;
+    mainProgram = "lshw";
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
