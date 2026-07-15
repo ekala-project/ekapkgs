9fd8a0c7065973f937e2d0cde48a649ebb4d441c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-14 23:04:28 -0700

    pkgs: add vim, libexif, libidn, wavpack, gsl, libsigcxx, neon
    Port from nixpkgs. All have available or no dependencies.

---
+++ pkgs/libidn/default.nix
@@ -0,0 +1,34 @@
+{
+  fetchurl,
+  lib,
+  stdenv,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libidn";
+  version = "1.43";
+
+  src = fetchurl {
+    url = "mirror://gnu/libidn/libidn-${finalAttrs.version}.tar.gz";
+    sha256 = "sha256-vcZiwS0EGyU50OY486bnQRMM2zOmRO80lpY6RDSC0WQ=";
+  };
+
+  outputs = [
+    "bin"
+    "dev"
+    "out"
+    "info"
+    "devdoc"
+  ];
+
+  hardeningDisable = [ "format" ];
+
+  meta = {
+    homepage = "https://www.gnu.org/software/libidn/";
+    description = "Library for internationalized domain names";
+    mainProgram = "idn";
+    license = lib.licenses.lgpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
---
+++ pkgs/libsigcxx/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  meson,
+  ninja,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "libsigc++";
+  version = "2.12.1";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/libsigc++/${lib.versions.majorMinor version}/libsigc++-${version}.tar.xz";
+    sha256 = "sha256-qdvuMjNR0Qm3ruB0qcuJyj57z4rY7e8YUfTPNZvVCEM=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    meson.configurePhaseHook
+    ninja
+  ];
+
+  mesonBuildType = "release";
+
+  meta = {
+    homepage = "https://libsigcplusplus.github.io/libsigcplusplus/";
+    description = "Typesafe callback system for standard C++";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}
