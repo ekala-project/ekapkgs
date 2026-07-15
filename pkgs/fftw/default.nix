b3cbc4cec92cb82a874d8276badfb51bf9578a55
Jonathan Ringer <jonringer117@gmail.com>
2026-07-14 22:51:48 -0700

    pkgs: add libtheora, openjpeg, fftw, speexdsp, speex, libvpx
    Port from nixpkgs. Depends on phase 1+2 packages (libogg, libvorbis,
    lcms2, yasm).

---
+++ pkgs/fftw/default.nix
@@ -0,0 +1,68 @@
+{
+  fetchurl,
+  stdenv,
+  lib,
+  gfortran,
+  precision ? "double",
+}:
+
+assert lib.elem precision [
+  "single"
+  "double"
+  "long-double"
+  "quad-precision"
+];
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "fftw-${precision}";
+  version = "3.3.11";
+
+  src = fetchurl {
+    urls = [
+      "https://fftw.org/fftw-${finalAttrs.version}.tar.gz"
+      "ftp://ftp.fftw.org/pub/fftw/fftw-${finalAttrs.version}.tar.gz"
+    ];
+    hash = "sha256-VjDCTN6zOxMWEvfrSxqZNCNHVPnziP+GF0WNC+byOaE=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+    "man"
+  ];
+  outputBin = "dev";
+
+  nativeBuildInputs = [ gfortran ];
+
+  configureFlags = [
+    "--enable-shared"
+    "--enable-threads"
+    "--enable-openmp"
+    "--disable-doc"
+  ]
+  ++ lib.optional (precision != "double") "--enable-${precision}"
+  ++ lib.optionals (stdenv.hostPlatform.isx86_64 && (precision == "single" || precision == "double")) [
+    "--enable-sse2"
+    "--enable-avx"
+    "--enable-avx2"
+    "--enable-avx512"
+    "--enable-avx128-fma"
+  ]
+  ++ lib.optionals (stdenv.hostPlatform.isAarch64 && (precision == "single" || precision == "double")) [
+    "--enable-neon"
+  ];
+
+  postPatch = ''
+    substituteInPlace configure --replace-fail "-mtune=native" "-mtune=generic"
+  '';
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "Fastest Fourier Transform in the West library";
+    homepage = "https://www.fftw.org/";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
---
+++ pkgs/libtheora/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  libogg,
+  libvorbis,
+  pkg-config,
+  validatePkgConfig,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libtheora";
+  version = "1.2.0";
+
+  src = fetchFromGitHub {
+    owner = "xiph";
+    repo = "theora";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-kzZh4V6wZX9MetDutuqjRenmdpy4PHaRU9MgtIwPpiU=";
+  };
+
+  configureFlags = [ "--disable-examples" ];
+
+  outputs = [
+    "out"
+    "dev"
+    "devdoc"
+  ];
+  outputDoc = "devdoc";
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    validatePkgConfig
+  ];
+
+  propagatedBuildInputs = [
+    libogg
+    libvorbis
+  ];
+
+  meta = {
+    description = "Library for Theora, a free and open video compression format";
+    homepage = "https://www.theora.org/";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
