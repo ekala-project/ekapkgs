13dddb4a383f93c366d11bc0c7ba98368fe96848
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:01:54 -0700

    unicode-character-database: init at 17.0.0

---
+++ pkgs/unicode-character-database/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchurl,
+  unzip,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "unicode-character-database";
+  version = "17.0.0";
+
+  src = fetchurl {
+    url = "https://www.unicode.org/Public/${finalAttrs.version}/ucd/UCD.zip";
+    sha256 = "sha256-IGbRkJsuqTkWzgktocDuSAjqPvhAfJS08U9bfrJj0o4=";
+  };
+
+  nativeBuildInputs = [
+    unzip
+  ];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/unicode
+    cp -r * $out/share/unicode
+    rm $out/share/unicode/env-vars
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Unicode Character Database";
+    homepage = "https://www.unicode.org/";
+    license = lib.licenses.unicode-dfs-2016;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
