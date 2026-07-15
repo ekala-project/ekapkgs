3b021647a67b474acb53e8ef74e27d9b6cae631a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:25:23 -0700

    asio: init at 1.38.0

---
+++ pkgs/asio/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "asio";
+  version = "1.38.0";
+
+  src = fetchFromGitHub {
+    owner = "chriskohlhoff";
+    repo = "asio";
+    tag = "asio-${lib.replaceStrings [ "." ] [ "-" ] finalAttrs.version}";
+    hash = "sha256-pkSu8XMibmRPMoS3v5hO34oJb077bYc9KWELj3t8D6M=";
+  };
+
+  patches = [
+    ./boost-1.89.patch
+  ];
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    homepage = "https://think-async.com/Asio";
+    description = "Cross-platform C++ library for network and low-level I/O programming";
+    license = lib.licenses.boost;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
