c38f49e884c5d275167f437b7e9995d14bc83e3b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:35:17 -0700

    serd: init at 0.32.8

---
+++ pkgs/serd/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  meson,
+  ninja,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "serd";
+  version = "0.32.8";
+
+  src = fetchurl {
+    url = "https://download.drobilla.net/serd-${finalAttrs.version}.tar.xz";
+    hash = "sha256-9HJZvDi6VTsN64ttq2tbc9NjBGmnyUOczcqA4G18Hs4=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+  ];
+
+  mesonFlags = [
+    "-Ddocs=disabled"
+    "-Dman_html=disabled"
+    "-Dtests=disabled"
+  ];
+
+  meta = {
+    description = "Lightweight C library for RDF syntax which supports reading and writing Turtle and NTriples";
+    homepage = "https://drobilla.net/software/serd";
+    license = lib.licenses.isc;
+    mainProgram = "serdi";
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
