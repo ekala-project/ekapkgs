b9efa57d9956b3e2c14d4319ecbbb08367b6877b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:35:46 -0700

    lv2: init at 1.18.10

---
+++ pkgs/lv2/default.nix
@@ -0,0 +1,42 @@
+{
+  stdenv,
+  lib,
+  fetchurl,
+  meson,
+  ninja,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "lv2";
+  version = "1.18.10";
+
+  src = fetchurl {
+    url = "https://lv2plug.in/spec/lv2-${finalAttrs.version}.tar.xz";
+    hash = "sha256-eMUbzyG1Tli7Yymsy7Ta4Dsu15tSD5oB5zS9neUwlT8=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+  ];
+
+  mesonFlags = [
+    "-Dplugins=disabled"
+    "-Dtests=disabled"
+    "-Ddocs=disabled"
+  ];
+
+  meta = {
+    homepage = "https://lv2plug.in";
+    description = "Plugin standard for audio systems";
+    mainProgram = "lv2_validate";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
