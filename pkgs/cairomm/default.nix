c0a852fe17ff4aa56d4e01f518bd524267764d8a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 12:00:57 -0700

    cairomm: init at 1.14.5

---
+++ pkgs/cairomm/default.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  meson,
+  ninja,
+  boost,
+  cairo,
+  fontconfig,
+  libsigcxx,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "cairomm";
+  version = "1.14.5";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "https://www.cairographics.org/releases/cairomm-${version}.tar.xz";
+    sha256 = "cBNiA1QMiE6Jzhye37Y2m5lTk39s1ZbZfHjJdYpdSNs=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    boost
+    fontconfig
+  ];
+
+  propagatedBuildInputs = [
+    cairo
+    libsigcxx
+  ];
+
+  mesonFlags = [
+    "-Dbuild-tests=true"
+  ];
+
+  doCheck = !stdenv.hostPlatform.isDarwin;
+
+  meta = {
+    description = "C++ bindings for the Cairo vector graphics library";
+    homepage = "https://www.cairographics.org/";
+    license = with lib.licenses; [
+      lgpl2Plus
+      mpl10
+    ];
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
