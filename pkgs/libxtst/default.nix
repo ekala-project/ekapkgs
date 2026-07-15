188abf9e20b3e8c2688c236b73471274ec5862ef
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 10:00:09 -0700

    libxtst: init at 1.2.5

---
+++ pkgs/libxtst/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  xorgproto,
+  libx11,
+  libxext,
+  libxi,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libxtst";
+  version = "1.2.5";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/lib/libXtst-${finalAttrs.version}.tar.xz";
+    hash = "sha256-tQ1MJblwCadEcGwQOcWY9NjmSRDJ/eOBmU4criNdkkI=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    xorgproto
+    libx11
+    libxext
+    libxi
+  ];
+
+  meta = {
+    description = "Library for the XTEST and RECORD X11 extensions";
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libxtst";
+    license = with lib.licenses; [
+      mitOpenGroup
+      hpndSellVariant
+      hpndDoc
+      x11
+      hpndDocSell
+    ];
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
