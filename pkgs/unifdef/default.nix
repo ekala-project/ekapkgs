698f1dcea8205e980ed2e7ac4218ba9a820b1282
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:29:03 -0700

    unifdef: init at 2.12

---
+++ pkgs/unifdef/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "unifdef";
+  version = "2.12";
+
+  src = fetchurl {
+    url = "https://dotat.at/prog/unifdef/unifdef-${finalAttrs.version}.tar.xz";
+    hash = "sha256-Q84PAuzc3HI7JHVXVWPdsZLpiMiG02gmC8CmOu46xAA=";
+  };
+
+  patches = [
+    ./unifdef-fix-build-with-gcc15.patch
+  ];
+
+  makeFlags = [
+    "prefix=$(out)"
+    "DESTDIR="
+  ];
+
+  meta = {
+    homepage = "https://dotat.at/prog/unifdef/";
+    description = "Selectively remove C preprocessor conditionals";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
