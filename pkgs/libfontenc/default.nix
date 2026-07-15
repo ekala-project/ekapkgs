952f92ff2749a2ec2a0429bff6f6c70e2720deba
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 11:44:53 -0700

    libfontenc: init at 1.1.9

---
+++ pkgs/libfontenc/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  xorgproto,
+  zlib,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libfontenc";
+  version = "1.1.9";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/lib/libfontenc-${finalAttrs.version}.tar.xz";
+    hash = "sha256-nYOScFyxCAPV/h0n0jbLqz9mTiaEHOAZFru+Qwzyc+I=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    xorgproto
+    zlib
+  ];
+
+  meta = {
+    description = "X font encoding library";
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libfontenc";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
