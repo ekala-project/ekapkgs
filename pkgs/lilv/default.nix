0c3937d4882b991a508642cb214167729d71b894
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:37:57 -0700

    lilv: init at 0.26.4

---
+++ pkgs/lilv/default.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  lv2,
+  meson,
+  ninja,
+  pkg-config,
+  python3,
+  libsndfile,
+  serd,
+  sord,
+  sratom,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "lilv";
+  version = "0.26.4";
+
+  src = fetchurl {
+    url = "https://download.drobilla.net/lilv-${version}.tar.xz";
+    hash = "sha256-HItfy3hxgXPmfXblGtQj9RE6n/aEY/JWYZWuRjlgieM=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    python3
+  ];
+
+  buildInputs = [
+    libsndfile
+    serd
+    sord
+    sratom
+  ];
+
+  propagatedBuildInputs = [ lv2 ];
+
+  mesonFlags = [
+    "-Ddocs=disabled"
+    "-Dtests=disabled"
+  ] ++ lib.optional stdenv.hostPlatform.isLinux (
+    lib.mesonOption "default_lv2_path" "~/.lv2:/usr/local/lib/lv2:/usr/lib/lv2:~/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2"
+  );
+
+  meta = {
+    homepage = "http://drobilla.net/software/lilv";
+    description = "C library to make the use of LV2 plugins";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
