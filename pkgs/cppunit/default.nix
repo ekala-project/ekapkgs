628773172e87cce235d812f0a349a7ad0c68bf40
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 11:51:39 -0700

    cppunit: init at 1.15.1

---
+++ pkgs/cppunit/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "cppunit";
+  version = "1.15.1";
+
+  src = fetchurl {
+    url = "https://dev-www.libreoffice.org/src/cppunit-${finalAttrs.version}.tar.gz";
+    sha256 = "19qpqzy66bq76wcyadmi3zahk5v1ll2kig1nvg96zx9padkcdic9";
+  };
+
+  configureFlags = [ "--disable-werror" ];
+
+  meta = {
+    homepage = "https://freedesktop.org/wiki/Software/cppunit/";
+    description = "C++ unit testing framework";
+    mainProgram = "DllPlugInTester";
+    license = lib.licenses.lgpl21;
+    maintainers = [ ];
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+})
