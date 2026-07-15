8a8a0dbddd368e980fe835ed499c54b2ff0382ad
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 13:40:44 -0700

    docbook_xsl: init at 1.79.2

--- top-level.nix
+++ top-level.nix
@@ -1,6 +1,7 @@
 # These will be added to the pkgs scope
 final: prev: {
   libmpg123 = final.mpg123;
+  docbook_xsl = final.docbook-xsl;
   gdk-pixbuf = prev.gdk-pixbuf.overrideAttrs (old: {
     nativeBuildInputs = old.nativeBuildInputs ++ [ final.meson.configurePhaseHook ];
   });
