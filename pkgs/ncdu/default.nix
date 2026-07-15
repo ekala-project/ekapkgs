d00b5db1fb9de6d9bed4b3446b678b2bbdd4e24d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:25:05 -0700

    ncdu: init at 1.22

---
+++ pkgs/ncdu/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  ncurses,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "ncdu";
+  version = "1.22";
+
+  src = fetchurl {
+    url = "https://dev.yorhel.nl/download/ncdu-${finalAttrs.version}.tar.gz";
+    sha256 = "sha256-CtbAltwE1RIFgRBHYMAbj06X1BkdbJ73llT6PGkaF2s=";
+  };
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ ncurses ];
+
+  meta = {
+    description = "Disk usage analyzer with an ncurses interface";
+    homepage = "https://dev.yorhel.nl/ncdu";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+    mainProgram = "ncdu";
+  };
+})
