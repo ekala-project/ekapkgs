69f4c3e54f7a5afc6da58fd9a000620054f7f1e2
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 12:03:30 -0700

    libnotify: init at 0.8.6

---
+++ pkgs/libnotify/default.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  meson,
+  ninja,
+  pkg-config,
+  libxslt,
+  docbook-xsl-ns,
+  glib,
+  gdk-pixbuf,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "libnotify";
+  version = "0.8.6";
+
+  outputs = [
+    "out"
+    "man"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
+    hash = "sha256-xVQKrvtg4dY7HFh8BfIoTr5y7OfQwOXkp3jP1YRLa1g=";
+  };
+
+  mesonFlags = [
+    "-Dtests=false"
+    "-Ddocbook_docs=disabled"
+    "-Dgtk_doc=false"
+    "-Dintrospection=disabled"
+  ];
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    libxslt
+    docbook-xsl-ns
+    glib
+  ];
+
+  propagatedBuildInputs = [
+    gdk-pixbuf
+    glib
+  ];
+
+  meta = {
+    description = "Library that sends desktop notifications to a notification daemon";
+    homepage = "https://gitlab.gnome.org/GNOME/libnotify";
+    license = lib.licenses.lgpl21;
+    mainProgram = "notify-send";
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
