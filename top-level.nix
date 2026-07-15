6e4f9b62da38f99ec53faabfc377c1c163891fa3
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 10:07:51 -0700

    top-level: fix gdk-pixbuf missing meson hook
    Upstream gdk-pixbuf is missing meson.configurePhaseHook, causing
    build failures for gtk2 and other dependents.

--- top-level.nix
+++ top-level.nix
@@ -1,4 +1,7 @@
 # These will be added to the pkgs scope
 final: prev: {
   libmpg123 = final.mpg123;
+  gdk-pixbuf = prev.gdk-pixbuf.overrideAttrs (old: {
+    nativeBuildInputs = old.nativeBuildInputs ++ [ final.meson.configurePhaseHook ];
+  });
 }
