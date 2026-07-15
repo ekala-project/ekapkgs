c7288889a685e251d5c6b19ba7c80162f5bf5250
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 14:40:01 -0700

    tre: init at 0.9.0

---
+++ pkgs/tre/default.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoconf,
+  automake,
+  gettext,
+  libtool,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tre";
+  version = "0.9.0";
+
+  src = fetchFromGitHub {
+    owner = "laurikari";
+    repo = "tre";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-5O8yqzv+SR8x0X7GtC2Pjo94gp0799M2Va8wJ4EKyf8=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  nativeBuildInputs = [
+    autoconf
+    automake
+    gettext
+    libtool
+  ];
+
+  preConfigure = ''
+    ./utils/autogen.sh
+  '';
+
+  meta = {
+    description = "Lightweight and robust POSIX compliant regexp matching library";
+    homepage = "https://laurikari.net/tre/";
+    changelog = "https://github.com/laurikari/tre/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.bsd2;
+    mainProgram = "agrep";
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
