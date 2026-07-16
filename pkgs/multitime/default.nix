b17d61c0d8e40d6230eff6c8b8b09c59cbf4478c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:32:49 -0700

    multitime: init at 1.5

---
+++ pkgs/multitime/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "multitime";
+  version = "1.5";
+
+  src = fetchFromGitHub {
+    owner = "ltratt";
+    repo = "multitime";
+    rev = "multitime-${finalAttrs.version}";
+    sha256 = "sha256-oLtBUJbu+tVhzsUv+toz2oLeXCVLYKHQXUNsqpCZBGc=";
+  };
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  meta = {
+    description = "Time command execution over multiple executions";
+    license = lib.licenses.mit;
+    homepage = "https://tratt.net/laurie/src/multitime/";
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    mainProgram = "multitime";
+  };
+})
