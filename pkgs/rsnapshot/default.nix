0eb7519ec1de5020f804549b9aeea7f521685573
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 20:36:50 -0700

    rsnapshot: init at 1.5.1

---
+++ pkgs/rsnapshot/default.nix
@@ -0,0 +1,43 @@
+{
+  fetchurl,
+  lib,
+  stdenv,
+  perl,
+  openssh,
+  rsync,
+  logger,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "rsnapshot";
+  version = "1.5.1";
+
+  src = fetchurl {
+    url = "https://github.com/rsnapshot/rsnapshot/releases/download/${finalAttrs.version}/rsnapshot-${finalAttrs.version}.tar.gz";
+    hash = "sha256-j2r4BG7msCk7JjidCMtpUMf33f/8H3Tu/LCHvUnUT2I=";
+  };
+
+  propagatedBuildInputs = [
+    perl
+    openssh
+    rsync
+    logger
+  ];
+
+  configureFlags = [ "--sysconfdir=/etc --prefix=/" ];
+  makeFlags = [ "DESTDIR=$(out)" ];
+
+  patchPhase = ''
+    substituteInPlace "Makefile.in" --replace \
+      "/usr/bin/pod2man" "${perl}/bin/pod2man"
+  '';
+
+  meta = {
+    description = "Filesystem snapshot utility for making backups of local and remote systems";
+    homepage = "https://rsnapshot.org/";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+    mainProgram = "rsnapshot";
+  };
+})
