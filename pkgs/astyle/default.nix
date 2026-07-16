c7e21ff577ec20eee0e4b3125da691815b5cfe7e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 19:02:38 -0700

    astyle: init at 3.6.16

---
+++ pkgs/astyle/default.nix
@@ -0,0 +1,33 @@
+{
+  stdenv,
+  lib,
+  fetchurl,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "astyle";
+  version = "3.6.16";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/astyle/astyle-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-QU6dpM/e6zXY98Fw4V70OvX6AGbJ9ZKnRvcHHzVuzac=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  # upstream repo includes a build/ directory
+  cmakeBuildDir = "_build";
+
+  meta = {
+    description = "Source code indenter, formatter, and beautifier for C, C++, C# and Java";
+    mainProgram = "astyle";
+    homepage = "https://astyle.sourceforge.net/";
+    license = lib.licenses.lgpl3;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
