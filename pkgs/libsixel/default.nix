6197e35e1abc43050748564be055234db0da7f52
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:21:09 -0700

    libsixel: init at 1.10.5

---
+++ pkgs/libsixel/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libsixel";
+  version = "1.10.5";
+
+  src = fetchFromGitHub {
+    owner = "libsixel";
+    repo = "libsixel";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-obzBZAknN3N7+Bvtd0+JHuXcemVb7wRv+Pt4VjS6Bck=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+  ];
+
+  doCheck = true;
+
+  mesonFlags = [
+    "-Dtests=enabled"
+    "-Dimg2sixel=enabled"
+    "-Dsixel2png=enabled"
+    "-Dgd=disabled"
+    "-Djpeg=disabled"
+    "-Dpng=disabled"
+  ];
+
+  meta = {
+    description = "SIXEL library for console graphics, and converter programs";
+    homepage = "https://github.com/libsixel/libsixel";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
