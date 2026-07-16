a8aa72673f4c35a8050b028bc2f3bcb70cc8a097
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 20:30:36 -0700

    fbset: init at 2.1

---
+++ pkgs/fbset/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  bison,
+  flex,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "fbset";
+  version = "2.1";
+
+  src = fetchFromGitHub {
+    owner = "sudipm-mukherjee";
+    repo = "fbset";
+    rev = "debian/${finalAttrs.version}-33";
+    hash = "sha256-nwWkQAA5+v5A8AmKg77mrSq2pXeSivxd0r7JyoBrs9A=";
+  };
+
+  nativeBuildInputs = [
+    bison
+    flex
+  ];
+
+  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
+
+  installFlags = [ "DESTDIR=$(out)" ];
+
+  meta = {
+    description = "Framebuffer device maintenance program";
+    homepage = "http://users.telenet.be/geertu/Linux/fbdev/";
+    license = lib.licenses.gpl2Only;
+    mainProgram = "fbset";
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
