9f993b6b2ff33c6b3e6859c715da0f60027968af
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 16:15:18 -0700

    libcue: init at 2.3.0

---
+++ pkgs/libcue/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  bison,
+  flex,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libcue";
+  version = "2.3.0";
+
+  src = fetchFromGitHub {
+    owner = "lipnitsk";
+    repo = "libcue";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-ZMUUa8CmpFNparPsM/P2yvRto9E85EdTxpID5sKQbNI=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    bison
+    flex
+  ];
+
+  # Fix the build with CMake 4.
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace-fail \
+        'CMAKE_MINIMUM_REQUIRED(VERSION 2.8 FATAL_ERROR)' \
+        'CMAKE_MINIMUM_REQUIRED(VERSION 3.10 FATAL_ERROR)'
+  '';
+
+  doCheck = true;
+
+  meta = {
+    description = "CUE Sheet Parser Library";
+    homepage = "https://github.com/lipnitsk/libcue";
+    license = lib.licenses.gpl2Only;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
