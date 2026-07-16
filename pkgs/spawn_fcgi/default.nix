111b7a1537da0e231645518f0555158b3029fb25
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:42:29 -0700

    spawn_fcgi: init at 1.6.6

---
+++ pkgs/spawn_fcgi/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  meson,
+  ninja,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "spawn-fcgi";
+  version = "1.6.6";
+
+  src = fetchurl {
+    url = "https://download.lighttpd.net/spawn-fcgi/releases-1.6.x/spawn-fcgi-${finalAttrs.version}.tar.xz";
+    hash = "sha256-yWI0XuzwVT7dm/XPYe5F59EYN/NANwZ/vaFlz0rdzhg=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+  ];
+
+  meta = {
+    homepage = "https://redmine.lighttpd.net/projects/spawn-fcgi";
+    description = "Provides an interface to external programs that support the FastCGI interface";
+    mainProgram = "spawn-fcgi";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
