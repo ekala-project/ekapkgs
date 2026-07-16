f26ff3e622682a8e2d0a38bc05d5822adc1cf95d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:29:10 -0700

    acpi: init at 1.8

---
+++ pkgs/acpi/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "acpi";
+  version = "1.8";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/acpiclient/${finalAttrs.version}/acpi-${finalAttrs.version}.tar.gz";
+    hash = "sha256-5kxuALU815dCfqMqFgUTQlsD7U8HdzP3Hx8J/zQPIws=";
+  };
+
+  meta = {
+    description = "Show battery status and other ACPI information";
+    mainProgram = "acpi";
+    homepage = "https://sourceforge.net/projects/acpiclient/";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
