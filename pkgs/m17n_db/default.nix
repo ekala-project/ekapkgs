d0a2ebe758f313b670a102e6ebf941b7c6fbecfd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 19:08:44 -0700

    m17n_db: init at 1.8.10

---
+++ pkgs/m17n_db/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  gettext,
+  gawk,
+  bash,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "m17n-db";
+  version = "1.8.10";
+
+  src = fetchurl {
+    url = "mirror://savannah/m17n/m17n-db-${finalAttrs.version}.tar.gz";
+    hash = "sha256-MQJOBRNTNEi5sx6jKU01pkJuZpDrRGKGgHMaqVXAwWw=";
+  };
+
+  nativeBuildInputs = [ gettext ];
+  buildInputs = [
+    gettext
+    gawk
+    bash
+  ];
+
+  strictDeps = true;
+
+  configureFlags = [ "--with-charmaps=${stdenv.cc.libc}/share/i18n/charmaps" ];
+
+  meta = {
+    homepage = "https://www.nongnu.org/m17n/";
+    description = "Multilingual text processing library (database)";
+    mainProgram = "m17n-db";
+    license = lib.licenses.lgpl21Plus;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
