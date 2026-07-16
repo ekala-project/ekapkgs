11f95ed07e8173c6be997e4407cddc0099b24b8b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 19:53:13 -0700

    libxcb-util: init at 0.4.1

---
+++ pkgs/libxcb-util/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  libxcb,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libxcb-util";
+  version = "0.4.1";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/xcb/xcb-util-${finalAttrs.version}.tar.xz";
+    hash = "sha256-Wr47u9jlTw+j7JRSkbfo+oz9PMzENxj4dYQw+UEm5RI=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ libxcb ];
+  propagatedBuildInputs = [ libxcb ];
+
+  meta = {
+    description = "XCB utility libraries";
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libxcb-util";
+    license = lib.licenses.x11;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
