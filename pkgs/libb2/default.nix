c7308b4d650ec14f32fc686a067acf76be8db54a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 16:19:04 -0700

    libb2: init at 0.98.1

---
+++ pkgs/libb2/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  libtool,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libb2";
+  version = "0.98.1";
+
+  src = fetchFromGitHub {
+    owner = "BLAKE2";
+    repo = "libb2";
+    tag = "v${finalAttrs.version}";
+    sha256 = "0qj8aaqvfcavj1vj5asm4pqm03ap7q8x4c2fy83cqggvky0frgya";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    libtool
+    pkg-config
+  ];
+
+  configureFlags = lib.optional stdenv.hostPlatform.isx86 "--enable-fat=yes";
+
+  enableParallelBuilding = true;
+
+  doCheck = true;
+
+  meta = {
+    description = "BLAKE2 family of cryptographic hash functions";
+    homepage = "https://blake2.net/";
+    platforms = lib.platforms.all;
+    license = lib.licenses.cc0;
+    maintainers = [ ];
+  };
+})
