6871d765525d0fa99cf413188792e30eabf91743
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:33:42 -0700

    memtailor: init at 1.4

---
+++ pkgs/memtailor/default.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenv,
+  autoreconfHook,
+  gtest,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "memtailor";
+  version = "1.4";
+
+  src = fetchFromGitHub {
+    owner = "Macaulay2";
+    repo = "memtailor";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-cpM/oa4GAKDxs6yrxHngpvam18cGA2u9Ftvd2WW4vdI=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+
+  checkInputs = [
+    gtest
+  ];
+
+  __structuredAttrs = true;
+
+  strictDeps = true;
+
+  configureFlags = [
+    (lib.withFeature finalAttrs.doCheck "gtest")
+  ];
+
+  enableParallelBuilding = true;
+
+  doCheck = true;
+
+  meta = {
+    description = "C++ library of special purpose memory allocators";
+    homepage = "https://github.com/Macaulay2/memtailor";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
