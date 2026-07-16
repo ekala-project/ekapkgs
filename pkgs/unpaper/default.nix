509dbe2e4447d6e8ef9c178a0fb3d4472c976ddf
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 20:33:11 -0700

    unpaper: init at 7.0.0

---
+++ pkgs/unpaper/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  meson,
+  ninja,
+  pkg-config,
+  sphinx,
+  buildPackages,
+  ffmpeg-headless,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "unpaper";
+  version = "7.0.0";
+
+  src = fetchurl {
+    url = "https://www.flameeyes.eu/files/unpaper-${finalAttrs.version}.tar.xz";
+    hash = "sha256-JXX7vybCJxnRy4grWWAsmQDH90cRisEwiD9jQZvkaoA=";
+  };
+
+  outputs = [
+    "out"
+    "man"
+  ];
+
+  nativeBuildInputs = [
+    buildPackages.libxslt.bin
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    sphinx
+  ];
+
+  buildInputs = [
+    ffmpeg-headless
+  ];
+
+  doCheck = false;
+
+  meta = {
+    homepage = "https://www.flameeyes.eu/projects/unpaper";
+    changelog = "https://github.com/unpaper/unpaper/blob/unpaper-${finalAttrs.version}/NEWS";
+    description = "Post-processing tool for scanned sheets of paper";
+    license = lib.licenses.gpl2Only;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+    mainProgram = "unpaper";
+  };
+})
