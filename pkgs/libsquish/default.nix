215ea34097cb89dc25f128aa74b5aa76a955cf3f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:20:27 -0700

    libsquish: init at 1.15

---
+++ pkgs/libsquish/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  cmake,
+  ninja,
+  libpng,
+  zlib,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libsquish";
+  version = "1.15";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/project/libsquish/libsquish-${finalAttrs.version}.tgz";
+    hash = "sha256-YoeW7rpgiGYYOmHQgNRpZ8ndpnI7wKPsUjJMhdIUcmk=";
+  };
+
+  sourceRoot = ".";
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    ninja
+  ];
+
+  buildInputs = [
+    libpng
+    zlib
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "BUILD_SHARED_LIBS" true)
+    (lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.5")
+  ]
+  ++ lib.optional (!stdenv.hostPlatform.isx86) (lib.cmakeBool "BUILD_SQUISH_WITH_SSE2" false);
+
+  meta = {
+    description = "Library for compressing images with the DXT/S3TC standard";
+    homepage = "https://libsquish.sourceforge.io";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
