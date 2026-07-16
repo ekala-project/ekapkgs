f299ed13f6eb6d7d2e1dc02c2caf8fe436b8d416
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 21:06:26 -0700

    wayvnc: init at 0.10.0

---
+++ pkgs/wayvnc/default.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  scdoc,
+  wayland-scanner,
+  aml,
+  jansson,
+  libdrm,
+  libxkbcommon,
+  libgbm,
+  neatvnc,
+  pam,
+  pixman,
+  wayland,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "wayvnc";
+  version = "0.10.0";
+
+  src = fetchFromGitHub {
+    owner = "any1";
+    repo = "wayvnc";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-+CAH2jcIIQqImonWeWxMQyTtEEuuQlaGyl/ajPfClh8=";
+  };
+
+  strictDeps = true;
+
+  depsBuildBuild = [
+    pkg-config
+  ];
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    scdoc
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    aml
+    jansson
+    libdrm
+    libxkbcommon
+    libgbm
+    neatvnc
+    pam
+    pixman
+    wayland
+  ];
+
+  mesonFlags = [
+    (lib.mesonBool "tests" false)
+  ];
+
+  meta = {
+    description = "VNC server for wlroots based Wayland compositors";
+    homepage = "https://github.com/any1/wayvnc";
+    license = lib.licenses.isc;
+    platforms = lib.platforms.linux;
+    mainProgram = "wayvnc";
+    maintainers = [ ];
+  };
+})
