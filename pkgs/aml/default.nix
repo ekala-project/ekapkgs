7c5d5a1fb1d52765b17e6043c99d26c959714886
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 21:03:30 -0700

    aml: init at 1.0.0

---
+++ pkgs/aml/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "aml";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "any1";
+    repo = "aml";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-10gm6YphZrpLShj3NUj/AG24dSVLZAZbbnXr7GiF4DI=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+  ];
+
+  strictDeps = true;
+
+  meta = {
+    description = "Andri's Main Loop";
+    homepage = "https://github.com/any1/aml";
+    license = lib.licenses.isc;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
