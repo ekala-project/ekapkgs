cfc25ff40fa52d8b7e131b7656d4cd59be4e7c2b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:21:26 -0700

    dwz: init at 0.16

---
+++ pkgs/dwz/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  elfutils,
+  xxhash,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "dwz";
+  version = "0.16";
+
+  src = fetchurl {
+    url = "https://www.sourceware.org/ftp/dwz/releases/dwz-${finalAttrs.version}.tar.gz";
+    hash = "sha256-R1hT4bSebtjMLQqQnHpPwcxXHrzPxmJ4/UM0Lb4n1Q4=";
+  };
+
+  nativeBuildInputs = [ elfutils ];
+
+  buildInputs = [
+    xxhash
+    elfutils
+  ];
+
+  makeFlags = [ "prefix=${placeholder "out"}" ];
+
+  strictDeps = true;
+
+  meta = {
+    homepage = "https://sourceware.org/dwz/";
+    description = "DWARF optimization and duplicate removal tool";
+    mainProgram = "dwz";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
