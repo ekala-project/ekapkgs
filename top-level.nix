eb454a73b41824ef709bbe59964d51efe1d06fec
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 21:45:35 -0700

    waf: init at 2.1.9

--- top-level.nix
+++ top-level.nix
@@ -2,6 +2,7 @@
 final: prev: {
   libmpg123 = final.mpg123;
   docbook_xsl = final.docbook-xsl;
+  wafHook = final.waf.hook;
   gdk-pixbuf = prev.gdk-pixbuf.overrideAttrs (old: {
     nativeBuildInputs = old.nativeBuildInputs ++ [ final.meson.configurePhaseHook ];
   });
