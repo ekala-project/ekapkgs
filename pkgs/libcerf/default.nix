20de9cd12550b4e9081e207d55b672f9ab73ecf0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 19:03:13 -0700

    libcerf: init at 3.3

---
+++ pkgs/libcerf/default.nix
@@ -0,0 +1,34 @@
+{
+  stdenv,
+  lib,
+  cmake,
+  fetchFromGitLab,
+  perl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libcerf";
+  version = "3.3";
+
+  src = fetchFromGitLab {
+    domain = "jugit.fz-juelich.de";
+    owner = "mlz";
+    repo = "libcerf";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-EPloejabyLzLP+GIPSIsh6dZDk2WodSEU6CPoICRxnM=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    perl
+  ];
+
+  meta = {
+    description = "Complex error (erf), Dawson, Faddeeva, and Voigt function library";
+    homepage = "https://jugit.fz-juelich.de/mlz/libcerf";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
