39f3639470286633e27475174eee8f3b5d2ec93e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 20:44:43 -0700

    zzuf: init at 0.15

---
+++ pkgs/zzuf/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  fetchpatch,
+  pkg-config,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "zzuf";
+  version = "0.15";
+
+  src = fetchFromGitHub {
+    owner = "samhocevar";
+    repo = "zzuf";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-yYpO1K9pVp+Fki0Bn5OHI4xhGGJ2yYC7U00M10PQIVI=";
+  };
+
+  patches = [
+    (fetchpatch {
+      url = "https://src.fedoraproject.org/rpms/zzuf/raw/998c7e5e632ea4c635a53437a01bfb48cbd744ac/f/zzuf-zzat-c99.patch";
+      hash = "sha256-pQQzwsIjKg+9g+dnhFGn2PUlxHlQ5Mj+e4a1D1k2oEo=";
+    })
+    ./zzuf-glibc.patch
+  ];
+
+  nativeBuildInputs = [
+    pkg-config
+    autoreconfHook
+  ];
+
+  meta = {
+    description = "Transparent application input fuzzer";
+    homepage = "http://caca.zoy.org/wiki/zzuf";
+    license = lib.licenses.wtfpl;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
