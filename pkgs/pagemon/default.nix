19e5cb82643d88146e17244930dbd510077c8a68
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:39:47 -0700

    pagemon: init at 0.02.06

---
+++ pkgs/pagemon/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  ncurses,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "pagemon";
+  version = "0.02.06";
+
+  src = fetchFromGitHub {
+    owner = "ColinIanKing";
+    repo = "pagemon";
+    tag = "V${finalAttrs.version}";
+    hash = "sha256-nlgrPGctgP6F90LTkVXVZ1MVbkr8yDF/AI+yrvnU5Hs=";
+  };
+
+  buildInputs = [ ncurses ];
+
+  makeFlags = [
+    "BINDIR=$(out)/bin"
+    "MANDIR=$(out)/share/man/man8"
+    "BASHDIR=$(out)/share/bash-completion/completions"
+  ];
+
+  meta = {
+    homepage = "https://github.com/ColinIanKing/pagemon";
+    description = "Interactive memory/page monitor for Linux";
+    mainProgram = "pagemon";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
