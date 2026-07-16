e3e5f8fc714c87e1c9e7dd52c7dccb188ee33211
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 19:43:15 -0700

    stress-ng: init at 0.21.03

---
+++ pkgs/stress-ng/default.nix
@@ -0,0 +1,68 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  attr,
+  keyutils,
+  libaio,
+  libapparmor,
+  libbsd,
+  libcap,
+  libgcrypt,
+  lksctp-tools,
+  zlib,
+  libglvnd,
+  libgbm,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "stress-ng";
+  version = "0.21.03";
+
+  src = fetchFromGitHub {
+    owner = "ColinIanKing";
+    repo = "stress-ng";
+    tag = "V${finalAttrs.version}";
+    hash = "sha256-gk66OY9QLDRKf4qQuPnRP0jPz+nArLbTKW5sKKiw5gY=";
+  };
+
+  postPatch = ''
+    sed -i '/\#include <bsd\/string.h>/i #undef HAVE_STRLCAT\n#undef HAVE_STRLCPY' stress-ng.h
+  '';
+
+  buildInputs = [
+    libbsd
+    libgcrypt
+    zlib
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    attr
+    keyutils
+    libaio
+    libapparmor
+    libcap
+    lksctp-tools
+    libglvnd
+    libgbm
+  ];
+
+  makeFlags = [
+    "BINDIR=${placeholder "out"}/bin"
+    "MANDIR=${placeholder "out"}/share/man/man1"
+    "JOBDIR=${placeholder "out"}/share/stress-ng/example-jobs"
+    "BASHDIR=${placeholder "out"}/share/bash-completion/completions"
+  ];
+
+  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isMusl "-D_LINUX_SYSINFO_H=1";
+
+  enableParallelBuilding = (!stdenv.hostPlatform.isi686);
+
+  meta = {
+    description = "Stress test a computer system";
+    homepage = "https://github.com/ColinIanKing/stress-ng";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    mainProgram = "stress-ng";
+  };
+})
