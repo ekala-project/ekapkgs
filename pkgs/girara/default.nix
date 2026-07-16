8f2c4ac02018130ab90cf1610025f31c09562302
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 16:24:38 -0700

    girara: init at 2026.02.04

---
+++ pkgs/girara/default.nix
@@ -0,0 +1,64 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  glib,
+  gtk3,
+  gettext,
+  json-glib,
+  libintl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "girara";
+  version = "2026.02.04";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "pwmt";
+    repo = "girara";
+    tag = finalAttrs.version;
+    hash = "sha256-wTVgldfo8pWdY244nNldiogioijv/k32w1A8pEqOTRE=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    gettext
+    glib
+  ];
+
+  buildInputs = [
+    libintl
+    json-glib
+  ];
+
+  propagatedBuildInputs = [
+    glib
+    gtk3
+  ];
+
+  mesonFlags = [
+    "-Ddocs=disabled"
+    (lib.mesonEnable "tests" false)
+  ];
+
+  doCheck = false;
+
+  meta = {
+    homepage = "https://pwmt.org/projects/girara";
+    description = "User interface library";
+    license = lib.licenses.zlib;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
