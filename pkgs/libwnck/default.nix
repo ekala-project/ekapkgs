bea70b5b8e59cc6bc83dd7d0cbc4fcc9609ec230
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 16:33:12 -0700

    libwnck: init at 43.3

---
+++ pkgs/libwnck/default.nix
@@ -0,0 +1,67 @@
+{
+  stdenv,
+  lib,
+  fetchurl,
+  meson,
+  ninja,
+  pkg-config,
+  libx11,
+  glib,
+  gtk3,
+  pango,
+  cairo,
+  libxres,
+  libxi,
+  libstartup_notification,
+  gettext,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "libwnck";
+  version = "43.3";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+  outputBin = "dev";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/libwnck/${lib.versions.major version}/libwnck-${version}.tar.xz";
+    sha256 = "avisQajwZ63h08qu0lSoNCO19hrT96Rg/Ky6wuGSvfc=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    gettext
+  ];
+
+  buildInputs = [
+    libx11
+    libstartup_notification
+    pango
+    cairo
+    libxres
+    libxi
+  ];
+
+  propagatedBuildInputs = [
+    glib
+    gtk3
+  ];
+
+  mesonFlags = [
+    "-Dgtk_doc=false"
+    "-Dintrospection=disabled"
+  ];
+
+  meta = {
+    description = "Library to manage X windows and workspaces (via pagers, tasklists, etc.)";
+    license = lib.licenses.lgpl21Plus;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+}
