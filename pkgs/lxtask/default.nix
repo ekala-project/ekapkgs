94ba2e5315cd940f1bd99c741fa2d4b76ee53a5f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 16:20:18 -0700

    lxtask: init at 0.1.12

---
+++ pkgs/lxtask/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  gtk3,
+  intltool,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "lxtask";
+  version = "0.1.12";
+
+  src = fetchFromGitHub {
+    owner = "lxde";
+    repo = "lxtask";
+    tag = finalAttrs.version;
+    hash = "sha256-BI50jV/17jGX91rcmg98+gkoy35oNpdSSaVDLyagbIc=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    intltool
+    pkg-config
+  ];
+
+  buildInputs = [
+    gtk3
+  ];
+
+  configureFlags = [ "--enable-gtk3" ];
+
+  meta = {
+    homepage = "https://lxde.sourceforge.net/";
+    description = "Lightweight and desktop independent task manager";
+    mainProgram = "lxtask";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
