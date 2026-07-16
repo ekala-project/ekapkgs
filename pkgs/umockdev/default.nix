2ff7f6dc77cbc30b81b99a208f29b0123ea76129
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 19:40:22 -0700

    umockdev: init at 0.19.3

---
+++ pkgs/umockdev/default.nix
@@ -0,0 +1,88 @@
+{
+  stdenv,
+  lib,
+  docbook-xsl-nons,
+  fetchurl,
+  glib,
+  gobject-introspection,
+  gtk-doc,
+  libpcap,
+  meson,
+  ninja,
+  pkg-config,
+  replaceVars,
+  systemdMinimal,
+  vala,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "umockdev";
+  version = "0.19.3";
+
+  outputs = [
+    "bin"
+    "out"
+    "dev"
+    "devdoc"
+  ];
+
+  src = fetchurl {
+    url = "https://github.com/martinpitt/umockdev/releases/download/${finalAttrs.version}/umockdev-${finalAttrs.version}.tar.xz";
+    hash = "sha256-RuReq29la/wJJDjX4OXfTF9R0Y46gzYMK+aAsgehoLc=";
+  };
+
+  patches = [
+    ./hardcode-paths.patch
+
+    (replaceVars ./substitute-udevadm.patch {
+      udevadm = "${systemdMinimal}/bin/udevadm";
+    })
+  ];
+
+  nativeBuildInputs = [
+    docbook-xsl-nons
+    gobject-introspection
+    gtk-doc
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    vala
+  ];
+
+  buildInputs = [
+    glib
+    systemdMinimal
+    libpcap
+  ];
+
+  strictDeps = true;
+
+  mesonFlags = [
+    "-Dgtk_doc=true"
+  ];
+
+  doCheck = false;
+
+  postPatch = ''
+    substituteInPlace src/umockdev-wrapper \
+      --subst-var-by 'LIBDIR' "''${!outputLib}/lib"
+  ''
+  + lib.optionalString stdenv.hostPlatform.isMusl ''
+    substituteInPlace src/libumockdev-preload.c \
+      --replace-fail libc.so.6 libc.so
+  '';
+
+  preCheck = ''
+    mkdir -p "$out/lib"
+    ln -s "$PWD/libumockdev-preload.so.0" "$out/lib/libumockdev-preload.so.0"
+  '';
+
+  meta = {
+    homepage = "https://github.com/martinpitt/umockdev";
+    description = "Mock hardware devices for creating unit tests";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
