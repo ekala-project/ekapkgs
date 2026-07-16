c8f6860c40e6c4f505d622122ab5b085c8811856
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 19:53:14 -0700

    libxcb-image: init at 0.4.1

---
+++ pkgs/libxcb-image/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  m4,
+  xorgproto,
+  libxcb,
+  libxcb-util,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libxcb-image";
+  version = "0.4.1";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/xcb/xcb-util-image-${finalAttrs.version}.tar.xz";
+    hash = "sha256-zK2O5drbEnH9RyetFNm9d6ZOUFYIdmxOmCZ9mu3kDT0=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    pkg-config
+    m4
+  ];
+
+  buildInputs = [
+    xorgproto
+    libxcb
+    libxcb-util
+  ];
+
+  propagatedBuildInputs = [ libxcb ];
+
+  meta = {
+    description = "XCB port of Xlib's XImage and XShmImage functions";
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libxcb-image";
+    license = lib.licenses.x11;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
