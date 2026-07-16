3729c4849384e94ae2528cc5f358fa8e1b9161bf
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 19:07:06 -0700

    cmatrix: init at 2.0

---
+++ pkgs/cmatrix/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  ncurses,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "cmatrix";
+  version = "2.0";
+
+  src = fetchFromGitHub {
+    owner = "abishekvashok";
+    repo = "cmatrix";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-dWlVWSRIE1fPa6R2N3ONL9QJlDQEqxfdYIgWTSr5MsE=";
+  };
+
+  nativeBuildInputs = [ autoreconfHook ];
+  buildInputs = [ ncurses ];
+
+  meta = {
+    description = "Simulates the falling characters theme from The Matrix movie";
+    homepage = "https://github.com/abishekvashok/cmatrix";
+    changelog = "https://github.com/abishekvashok/cmatrix/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    mainProgram = "cmatrix";
+  };
+})
