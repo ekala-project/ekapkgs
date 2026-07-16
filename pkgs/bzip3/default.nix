aafefb22a894d91ca10498a225e9c8e3239c08ba
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 22:21:21 -0700

    bzip3: init at 1.5.3

---
+++ pkgs/bzip3/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "bzip3";
+  version = "1.5.3";
+
+  outputs = [
+    "bin"
+    "dev"
+    "out"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "iczelia";
+    repo = "bzip3";
+    tag = finalAttrs.version;
+    hash = "sha256-SOouMUctxsAJdkt84rJBaCbK23GKmXRH9nVgGdDodsk=";
+  };
+
+  postPatch = ''
+    echo -n "${finalAttrs.version}" > .tarball-version
+    patchShebangs build-aux
+
+    # build-aux/ax_subst_man_date.m4 calls git if the file exists
+    rm .gitignore
+  '';
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+
+  configureFlags = [
+    "--disable-arch-native"
+  ];
+
+  meta = {
+    description = "Better and stronger spiritual successor to BZip2";
+    homepage = "https://github.com/iczelia/bzip3";
+    changelog = "https://github.com/iczelia/bzip3/blob/${finalAttrs.version}/NEWS";
+    license = lib.licenses.lgpl3Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
