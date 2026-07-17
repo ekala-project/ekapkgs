3bf8a3204cb8079dfd7c236fae2e75e5f019f5c0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 20:15:57 -0700

    a52dec: init at 0.8.0

---
+++ pkgs/a52dec/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "a52dec";
+  version = "0.8.0";
+
+  src = fetchFromGitLab {
+    domain = "git.adelielinux.org";
+    owner = "community";
+    repo = "a52dec";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Z4riiwetJkhQYa+AD8qOiwB1+cuLbOyN/g7D8HM8Pkw=";
+  };
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  configureFlags = [
+    "--enable-shared"
+    "ac_cv_c_inline=yes"
+    "CFLAGS=-std=gnu17"
+  ];
+
+  makeFlags = [ "AR=${stdenv.cc.targetPrefix}ar" ];
+
+  doCheck = !stdenv.hostPlatform.isi686;
+
+  meta = {
+    description = "ATSC A/52 stream decoder";
+    homepage = "https://liba52.sourceforge.io/";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    mainProgram = "a52dec";
+    platforms = lib.platforms.unix;
+  };
+})
