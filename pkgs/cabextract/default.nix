5ab38079581ea2d00903c7568d83cc31f8a9e278
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 20:22:28 -0700

    cabextract: init at 1.11

---
+++ pkgs/cabextract/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "cabextract";
+  version = "1.11";
+
+  src = fetchurl {
+    url = "https://www.cabextract.org.uk/cabextract-${finalAttrs.version}.tar.gz";
+    sha256 = "sha256-tVRtsRVeTHGP89SyeFc2BPMN1kw8W/1GV80Im4I6OsY=";
+  };
+
+  configureFlags = lib.optionals (stdenv.buildPlatform != stdenv.hostPlatform) [
+    "ac_cv_func_fnmatch_works=yes"
+  ];
+
+  meta = {
+    homepage = "https://www.cabextract.org.uk/";
+    description = "Free Software for extracting Microsoft cabinet files";
+    platforms = lib.platforms.all;
+    license = lib.licenses.gpl3;
+    maintainers = [ ];
+    mainProgram = "cabextract";
+  };
+})
