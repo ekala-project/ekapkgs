ead67bf8fce4374a1f3680e9e8047db007e949f6
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 18:58:57 -0700

    inotify-tools: init at 4.25.9.0

---
+++ pkgs/inotify-tools/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  autoreconfHook,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "inotify-tools";
+  version = "4.25.9.0";
+
+  src = fetchFromGitHub {
+    repo = "inotify-tools";
+    owner = "inotify-tools";
+    rev = finalAttrs.version;
+    hash = "sha256-u7bnFmSEXNGVZTJ71kOTscQLymbjJblJCIY9Uj7/3mM=";
+  };
+
+  configureFlags = [
+    "--enable-fanotify"
+  ];
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  meta = {
+    homepage = "https://github.com/inotify-tools/inotify-tools/wiki";
+    description = "C library and CLI tools providing a simple interface to inotify";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
