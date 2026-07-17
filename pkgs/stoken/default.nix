863f2e90f8421b2b3a245d56efcc827221ea81a1
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:25:47 -0700

    stoken: init at 0.93

---
+++ pkgs/stoken/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+  libxml2,
+  nettle,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "stoken";
+  version = "0.93";
+
+  src = fetchFromGitHub {
+    owner = "cernekee";
+    repo = "stoken";
+    rev = "v${version}";
+    hash = "sha256-8N7TXdBu37eXWIKCBdaXVW0pvN094oRWrdlcy9raddI=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+
+  buildInputs = [
+    libxml2
+    nettle
+  ];
+
+  meta = {
+    description = "Software Token for Linux/UNIX";
+    homepage = "https://github.com/cernekee/stoken";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}
