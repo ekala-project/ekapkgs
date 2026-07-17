7b3d7d4fe2b5966fa489be2e25980d9a3fcd1f7e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 17:41:08 -0700

    cppi: init at 1.18

---
+++ pkgs/cppi/default.nix
@@ -0,0 +1,24 @@
+{
+  fetchurl,
+  lib,
+  stdenv,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "cppi";
+  version = "1.18";
+
+  src = fetchurl {
+    url = "mirror://gnu/cppi/cppi-${finalAttrs.version}.tar.xz";
+    hash = "sha256-EqUFuYhj9sXPH3SfkIC+O0Kz6sWjW1ljDme+pyQTZMo=";
+  };
+
+  meta = {
+    description = "C preprocessor directive indenter";
+    homepage = "https://savannah.gnu.org/projects/cppi/";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+    mainProgram = "cppi";
+  };
+})
