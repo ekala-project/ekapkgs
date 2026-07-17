001325c1cf9a3628e43f58790e3226e15af22a10
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:55:41 -0700

    fio: init at 3.42

---
+++ pkgs/fio/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+  libaio,
+  pkg-config,
+  zlib,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "fio";
+  version = "3.42";
+
+  src = fetchFromGitHub {
+    owner = "axboe";
+    repo = "fio";
+    tag = "fio-${finalAttrs.version}";
+    hash = "sha256-v2A2mY0Lvoje632761urfR7h1KHVcGnVDaKOMjexqis=";
+  };
+
+  buildInputs = [
+    zlib
+  ]
+  ++ lib.optional (!stdenv.hostPlatform.isDarwin) libaio;
+
+  # ./configure does not support autoconf-style --build=/--host=.
+  configurePlatforms = [ ];
+
+  configureFlags = [
+    "--disable-native"
+  ];
+
+  dontAddStaticConfigureFlags = true;
+
+  nativeBuildInputs = [
+    makeWrapper
+    pkg-config
+  ];
+
+  strictDeps = true;
+  enableParallelBuilding = true;
+
+  doCheck = false;
+
+  meta = {
+    description = "Flexible IO Tester - an IO benchmark tool";
+    homepage = "https://git.kernel.dk/cgit/fio/";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
