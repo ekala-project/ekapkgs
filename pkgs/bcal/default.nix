7f9eb8f495dfaae6d4967755baede2758be11e61
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 17:41:22 -0700

    bcal: init at 2.5

---
+++ pkgs/bcal/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  readline,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "bcal";
+  version = "2.5";
+
+  src = fetchFromGitHub {
+    owner = "jarun";
+    repo = "bcal";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-6oJEinw9KmZSinMl0s94oWiNshKsEp9HMUvWl12kLP4=";
+  };
+
+  buildInputs = [ readline ];
+
+  installFlags = [ "PREFIX=$(out)" ];
+
+  meta = {
+    description = "Storage conversion and expression calculator";
+    homepage = "https://github.com/jarun/bcal";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    mainProgram = "bcal";
+  };
+})
