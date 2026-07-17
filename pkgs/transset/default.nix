4b9540ddc793d0ed1dac83a5900c24b738fd5912
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 17:41:24 -0700

    transset: init at 1.0.4

---
+++ pkgs/transset/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  libx11,
+  xorgproto,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "transset";
+  version = "1.0.4";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/app/transset-${finalAttrs.version}.tar.xz";
+    hash = "sha256-gamrdK8TdzOqjLajf4KSlIUm/n7wa4WfwP8nLEN8Czg=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    libx11
+    xorgproto
+  ];
+
+  meta = {
+    description = "Utility for setting opacity/transparency property on a window";
+    homepage = "https://gitlab.freedesktop.org/xorg/app/transset";
+    license = with lib.licenses; [
+      mit
+      mitOpenGroup
+      hpndSellVariant
+    ];
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    mainProgram = "transset";
+  };
+})
