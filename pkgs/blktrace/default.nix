0f60eab513575d4a5a4ea5cef206e874dc838f2b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 01:10:19 -0700

    blktrace: init at 1.3.0

---
+++ pkgs/blktrace/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  libaio,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "blktrace";
+  version = "1.3.0";
+
+  src = fetchurl {
+    url = "https://brick.kernel.dk/snaps/blktrace-${finalAttrs.version}.tar.bz2";
+    sha256 = "sha256-1t7aA4Yt4r0bG5+6cpu7hi2bynleaqf3yoa2VoEacNY=";
+  };
+
+  buildInputs = [ libaio ];
+
+  makeFlags = [
+    "prefix=${placeholder "out"}"
+    "CC:=$(CC)"
+  ];
+
+  meta = {
+    description = "Block layer IO tracing mechanism";
+    maintainers = [ ];
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+  };
+})
