b169502e71f579ef16b722bce36a0407505de88d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 01:21:58 -0700

    gojq: init at 0.12.19

---
+++ pkgs/gojq/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "gojq";
+  version = "0.12.19";
+
+  src = fetchFromGitHub {
+    owner = "itchyny";
+    repo = "gojq";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-egaBNHKKzwDwaUN4GT+Xvt11Nz6ojNMSIrXbcEisyI4=";
+  };
+
+  vendorHash = "sha256-6UnX9YHh4RGcIFfqaJLxDPxuGQ6KO4UIOryXsoJUjFs=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  postInstall = ''
+    installShellCompletion --cmd gojq --zsh _gojq
+  '';
+
+  meta = {
+    description = "Pure Go implementation of jq";
+    homepage = "https://github.com/itchyny/gojq";
+    changelog = "https://github.com/itchyny/gojq/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "gojq";
+  };
+})
