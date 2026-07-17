b37a37707e556bc0646bd4ea2aae8d436a0dd856
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 20:38:10 -0700

    ladspa-header: init at 1.17

---
+++ pkgs/ladspa-header/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  stdenv,
+  ladspa-sdk,
+}:
+
+stdenv.mkDerivation {
+  pname = "ladspa-header";
+
+  inherit (ladspa-sdk) version src;
+
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/include
+    cp src/ladspa.h $out/include/ladspa.h
+  '';
+
+  meta = {
+    description = "LADSPA format audio plugins header file";
+    homepage = "https://www.ladspa.org/";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}
