7619f26b9ba01d2a8be9caff8a1bc7949d340a71
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:25:02 -0700

    earlyoom: init at 1.9.0

---
+++ pkgs/earlyoom/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenv,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "earlyoom";
+  version = "1.9.0";
+
+  src = fetchFromGitHub {
+    owner = "rfjakob";
+    repo = "earlyoom";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-eNWg96+uQn/s+iBCm8TH26pXVVzBdqbeQxVP2t4W7YA=";
+  };
+
+  patches = [ ./0000-fix-dbus-path.patch ];
+
+  makeFlags = [
+    "VERSION=${finalAttrs.version}"
+    "PREFIX=${placeholder "out"}"
+    "SYSCONFDIR=${placeholder "out"}/etc"
+    "SYSTEMDUNITDIR=${placeholder "out"}/etc/systemd/system"
+  ];
+
+  meta = {
+    homepage = "https://github.com/rfjakob/earlyoom";
+    description = "Early OOM Daemon for Linux";
+    license = lib.licenses.mit;
+    mainProgram = "earlyoom";
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
