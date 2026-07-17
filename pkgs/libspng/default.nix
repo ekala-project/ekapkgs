fc812cc60b8bcf9c6877218fa7b606120be80b92
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 17:43:44 -0700

    libspng: init at 0.7.4

---
+++ pkgs/libspng/default.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenv,
+  zlib,
+  ninja,
+  meson,
+  pkg-config,
+  libpng,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libspng";
+  version = "0.7.4";
+
+  src = fetchFromGitHub {
+    owner = "randy408";
+    repo = "libspng";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-BiRuPQEKVJYYgfUsglIuxrBoJBFiQ0ygQmAFrVvCz4Q=";
+  };
+
+  postPatch = ''
+    cat tests/images/meson.build | grep -v "'ch1n3p04'" | grep -v "'ch2n3p08'" > tests/images/meson.build-patched
+    mv tests/images/meson.build-patched tests/images/meson.build
+  '';
+
+  mesonBuildType = "release";
+
+  mesonFlags = [
+    "-Ddev_build=true"
+  ];
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  strictDeps = true;
+
+  buildInputs = [
+    zlib
+    libpng
+  ];
+
+  nativeBuildInputs = [
+    ninja
+    meson
+    meson.configurePhaseHook
+    pkg-config
+  ];
+
+  meta = {
+    description = "Simple, modern libpng alternative";
+    homepage = "https://libspng.org/";
+    license = with lib.licenses; [ bsd2 ];
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
