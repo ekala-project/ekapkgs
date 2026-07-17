ca45ea1bd52eaccdab485a7e0ff3696d432345dd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 20:19:18 -0700

    gengetopt: init at 2.23

---
+++ pkgs/gengetopt/default.nix
@@ -0,0 +1,48 @@
+{
+  fetchurl,
+  lib,
+  stdenv,
+  texinfo,
+  help2man,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "gengetopt";
+  version = "2.23";
+
+  src = fetchurl {
+    url = "mirror://gnu/gengetopt/gengetopt-${finalAttrs.version}.tar.xz";
+    sha256 = "1b44fn0apsgawyqa4alx2qj5hls334mhbszxsy6rfr0q074swhdr";
+  };
+
+  doCheck = true;
+  preCheck = ''
+    rm tests/test_conf_parser_save.sh
+  '';
+
+  enableParallelBuilding = false;
+
+  nativeBuildInputs = [
+    texinfo
+    help2man
+  ];
+
+  postPatch = ''
+    substituteInPlace configure --replace \
+      'set -o posix' \
+      'set +o posix'
+  '';
+
+  env = lib.optionalAttrs stdenv.cc.isClang {
+    CXXFLAGS = "-std=c++14";
+  };
+
+  meta = {
+    description = "Command-line option parser generator";
+    mainProgram = "gengetopt";
+    homepage = "https://www.gnu.org/software/gengetopt/";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
