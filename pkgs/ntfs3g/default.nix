7bc590288c8d09f8e3c0fd40cce3456433d9bc9d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 09:22:08 -0700

    ntfs3g: init at 2026.2.25

---
+++ pkgs/ntfs3g/default.nix
@@ -0,0 +1,87 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+  gettext,
+  mount,
+  libuuid,
+  kmod,
+  libgcrypt,
+  gnutls,
+  crypto ? false,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "ntfs3g";
+  version = "2026.2.25";
+
+  outputs = [
+    "out"
+    "dev"
+    "man"
+    "doc"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "tuxera";
+    repo = "ntfs-3g";
+    tag = finalAttrs.version;
+    hash = "sha256-uiVh87ExLXq94NVqR8MEg7Lrvamm6MrH+qP3Nosii5c=";
+  };
+
+  buildInputs = [
+    gettext
+    libuuid
+  ]
+  ++ lib.optionals crypto [
+    gnutls
+    libgcrypt
+  ];
+
+  nativeBuildInputs = [
+    autoreconfHook
+    libgcrypt
+    pkg-config
+  ];
+
+  patches = [
+    ./autoconf-sbin-helpers.patch
+    ./consistent-sbindir-usage.patch
+  ];
+
+  configureFlags = [
+    "--disable-ldconfig"
+    "--exec-prefix=\${prefix}"
+    "--enable-mount-helper"
+    "--enable-posix-acls"
+    "--enable-xattr-mappings"
+    "--${if crypto then "enable" else "disable"}-crypto"
+    "--enable-extras"
+    "--with-mount-helper=${lib.getExe' mount "mount"}"
+    "--with-umount-helper=${lib.getExe' mount "umount"}"
+    "--with-fuse=internal"
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    "--with-modprobe-helper=${lib.getExe' kmod "modprobe"}"
+  ];
+
+  postInstall = ''
+    ln -sv mount.ntfs-3g $out/sbin/mount.ntfs
+  '';
+
+  enableParallelBuilding = true;
+
+  meta = {
+    homepage = "https://github.com/tuxera/ntfs-3g";
+    description = "FUSE-based NTFS driver with full write support";
+    mainProgram = "ntfs-3g";
+    platforms = lib.platforms.linux;
+    license = with lib.licenses; [
+      gpl2Plus
+      lgpl2Plus
+    ];
+    maintainers = [ ];
+  };
+})
