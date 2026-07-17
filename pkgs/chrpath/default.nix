d91c4822e9e7e456a12203bc4f8a86b24cc22081
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 20:15:21 -0700

    chrpath: init at 0.18

---
+++ pkgs/chrpath/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "chrpath";
+  version = "0.18";
+
+  src = fetchurl {
+    url = "https://codeberg.org/pere/chrpath/archive/release-${finalAttrs.version}.tar.gz";
+    hash = "sha256-8JxJ8GGGYMoR/G2VgN3ekExyJNTG0Pby0fm83JECyao=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+
+  meta = {
+    description = "Command line tool to adjust the RPATH or RUNPATH of ELF binaries";
+    mainProgram = "chrpath";
+    homepage = "https://codeberg.org/pere/chrpath";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
