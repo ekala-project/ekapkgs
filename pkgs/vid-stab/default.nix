d1bb92466df76158492187324bc4e9b65ac03417
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 01:04:25 -0700

    vid-stab: init at 1.1.1-unstable-2025-08-21

---
+++ pkgs/vid-stab/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+}:
+
+stdenv.mkDerivation {
+  pname = "vid.stab";
+  version = "1.1.1-unstable-2025-08-21";
+
+  src = fetchFromGitHub {
+    owner = "georgmartius";
+    repo = "vid.stab";
+    rev = "4bd81e3cdd778e2e0edc591f14bba158ec40cfa1";
+    hash = "sha256-imSy1ywpGWbghP65NoPgUJBJmHUY5OsLWmIXk6Q1MQ4=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace-fail \
+        'cmake_minimum_required (VERSION 2.8.5)' \
+        'cmake_minimum_required (VERSION 3.10)'
+  '';
+
+  meta = {
+    description = "Video stabilization library";
+    homepage = "http://public.hronopik.de/vid.stab/";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}
