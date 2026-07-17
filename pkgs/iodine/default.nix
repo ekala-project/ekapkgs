f83aaf552879a5dd1f57b562d115600bb7ea0546
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:24:23 -0700

    iodine: init at 0.8.0

---
+++ pkgs/iodine/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  zlib,
+  net-tools,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "iodine";
+  version = "0.8.0";
+
+  src = fetchFromGitHub {
+    owner = "yarrick";
+    repo = "iodine";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-0vDl/F/57puugrEdOtdlpNPMF9ugO7TP3KLWo/7bP2k=";
+  };
+
+  buildInputs = [ zlib ];
+
+  env.NIX_CFLAGS_COMPILE = ''-DIFCONFIGPATH="${net-tools}/bin/" -DROUTEPATH="${net-tools}/bin/"'';
+
+  installFlags = [ "prefix=\${out}" ];
+
+  meta = {
+    homepage = "https://code.kryo.se/iodine/";
+    description = "Tool to tunnel IPv4 data through a DNS server";
+    license = lib.licenses.isc;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
