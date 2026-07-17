7cc2d018d453312846f3e3ca3b6ec29df6be8b6f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:18:39 -0700

    taplo: init at 0.10.0

---
+++ pkgs/taplo/default.nix
@@ -0,0 +1,53 @@
+{
+  stdenv,
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  installShellFiles,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "taplo";
+  version = "0.10.0";
+
+  src = fetchFromGitHub {
+    owner = "tamasfe";
+    repo = "taplo";
+    tag = "release-taplo-cli-${finalAttrs.version}";
+    hash = "sha256-FW8OQ5TRUuQK8M2NDmp4c6p22jsHodxKqzOMrcdiqXU=";
+  };
+
+  cargoPatches = [
+    ./update-reqwest.patch
+  ];
+
+  cargoHash = "sha256-FMpGo+kRcNgDj4qwYvdQKGwGazUKKMIVq0HCYMrTql0=";
+
+  buildAndTestSubdir = "crates/taplo-cli";
+
+  nativeBuildInputs = [
+    installShellFiles
+    pkg-config
+  ];
+
+  buildInputs = [ openssl ];
+
+  buildFeatures = [ "lsp" ];
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    installShellCompletion --cmd taplo \
+      --bash <($out/bin/taplo completions bash) \
+      --fish <($out/bin/taplo completions fish) \
+      --zsh <($out/bin/taplo completions zsh)
+  '';
+
+  meta = {
+    description = "TOML toolkit written in Rust";
+    homepage = "https://taplo.tamasfe.dev";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "taplo";
+  };
+})
