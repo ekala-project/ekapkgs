65ab1b56630196d32611ce282a90282498bd56ee
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 20:37:13 -0700

    sshguard: init at 2.5.1

---
+++ pkgs/sshguard/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoreconfHook,
+  bison,
+  flex,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  version = "2.5.1";
+  pname = "sshguard";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/sshguard/sshguard-${finalAttrs.version}.tar.gz";
+    sha256 = "sha256-mXoeDsKyFltHV8QviUgWLrU0GDlGr1LvxAaIXZfLifw=";
+  };
+
+  doCheck = true;
+
+  nativeBuildInputs = [
+    autoreconfHook
+    bison
+    flex
+  ];
+
+  configureFlags = [ "--sysconfdir=/etc" ];
+
+  meta = {
+    description = "Protects hosts from brute-force attacks";
+    mainProgram = "sshguard";
+    homepage = "https://sshguard.net";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
