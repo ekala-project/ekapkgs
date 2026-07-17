4c3b72c937ede0c36017489ad90bc225ceb75d03
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 18:58:21 -0700

    cunit: init at 2.1-3

---
+++ pkgs/cunit/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoconf,
+  automake,
+  libtool,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "CUnit";
+  version = "2.1-3";
+
+  nativeBuildInputs = [
+    autoreconfHook
+    autoconf
+    automake
+  ];
+  buildInputs = [ libtool ];
+
+  src = fetchurl {
+    url = "mirror://sourceforge/cunit/CUnit/${finalAttrs.version}/CUnit-${finalAttrs.version}.tar.bz2";
+    sha256 = "057j82da9vv4li4z5ri3227ybd18nzyq81f6gsvhifs5z0vr3cpm";
+  };
+
+  meta = {
+    description = "Unit Testing Framework for C";
+    homepage = "https://cunit.sourceforge.net/";
+    license = lib.licenses.lgpl2;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
