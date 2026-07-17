c3f9428ad7325f49ff24d4e1056311e73cf568ed
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:37:41 -0700

    libfabric: init at 2.5.1

---
+++ pkgs/libfabric/default.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  pkg-config,
+  autoreconfHook,
+  libuuid,
+  numactl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libfabric";
+  version = "2.5.1";
+
+  src = fetchFromGitHub {
+    owner = "ofiwg";
+    repo = "libfabric";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-d8ZdHMo3ErwBtLtUiLetjR+wu6H9bj3jnGZDYX544Jc=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+    "man"
+  ];
+
+  nativeBuildInputs = [
+    pkg-config
+    autoreconfHook
+  ];
+
+  buildInputs = [
+    libuuid
+    numactl
+  ];
+
+  configureFlags = [
+    "--disable-psm2"
+    "--enable-opx"
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    homepage = "https://ofiwg.github.io/libfabric/";
+    description = "Open Fabric Interfaces";
+    license = with lib.licenses; [
+      gpl2
+      bsd2
+    ];
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+  };
+})
