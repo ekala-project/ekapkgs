75286afc7642b8423dfe5e87b8d870611dadbd07
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 17:41:17 -0700

    smenu: init at 1.5.0

---
+++ pkgs/smenu/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  ncurses,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "smenu";
+  version = "1.5.0";
+
+  src = fetchFromGitHub {
+    owner = "p-gen";
+    repo = "smenu";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-nTQe6sCMHGRW7Djpv33xY8nL4a7ZyC9YM7PGOvmpuSM=";
+  };
+
+  buildInputs = [ ncurses ];
+
+  meta = {
+    description = "Terminal selection utility";
+    homepage = "https://github.com/p-gen/smenu";
+    license = lib.licenses.gpl2Only;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    mainProgram = "smenu";
+  };
+})
