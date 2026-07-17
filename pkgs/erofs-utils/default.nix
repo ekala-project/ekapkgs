b24ac03235549995ef034e2e75ae2e1a0fb1e1ca
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:30:54 -0700

    erofs-utils: init at 1.9.2

---
+++ pkgs/erofs-utils/default.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoreconfHook,
+  pkg-config,
+  fuse3,
+  util-linux,
+  xxhash,
+  lz4,
+  xz,
+  zlib,
+  zstd,
+  libdeflate,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "erofs-utils";
+  version = "1.9.2";
+  outputs = [
+    "out"
+    "man"
+  ];
+
+  enableParallelBuilding = true;
+  strictDeps = true;
+
+  src = fetchurl {
+    url = "https://git.kernel.org/pub/scm/linux/kernel/git/xiang/erofs-utils.git/snapshot/erofs-utils-${finalAttrs.version}.tar.gz";
+    hash = "sha256-2RW0VkapKBdJF8RKLIS6AFsWHoSrcyzV0lYDcVYLjRM=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+  buildInputs = [
+    util-linux
+    xxhash
+    lz4
+    zlib
+    xz
+    zstd
+    libdeflate
+    fuse3
+  ];
+
+  configureFlags = [
+    "MAX_BLOCK_SIZE=4096"
+    "--enable-multithreading"
+    "--with-libdeflate"
+    "--enable-fuse"
+  ];
+
+  meta = {
+    homepage = "https://git.kernel.org/pub/scm/linux/kernel/git/xiang/erofs-utils.git/about/";
+    description = "Userspace utilities for linux-erofs file system";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
