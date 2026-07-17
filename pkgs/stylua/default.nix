22b1e2da2e61ee75724b01bb5000267c11485efa
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:16:18 -0700

    stylua: init at 2.5.2

---
+++ pkgs/stylua/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "stylua";
+  version = "2.5.2";
+
+  src = fetchFromGitHub {
+    owner = "johnnymorganz";
+    repo = "stylua";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-C0X7IdYqs/hhSX96XWVyk7rfQNd9DjYxYgHL34cI+3g=";
+  };
+
+  cargoHash = "sha256-m31oocBkx4i2KxM0YC1omVWtypFi7iEoDVlXgT93iSI=";
+
+  postPatch = ''
+    rm .cargo/config.toml
+  '';
+
+  buildFeatures = [
+    "lua54"
+    "luajit"
+    "luau"
+  ];
+
+  meta = {
+    description = "Opinionated Lua code formatter";
+    homepage = "https://github.com/johnnymorganz/stylua";
+    changelog = "https://github.com/johnnymorganz/stylua/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = lib.licenses.mpl20;
+    maintainers = [ ];
+    mainProgram = "stylua";
+  };
+})
