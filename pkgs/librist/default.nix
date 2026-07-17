8c7ba40230463089be2c57689e02e700bfa67f70
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:29:53 -0700

    librist: init at 0.2.11

---
+++ pkgs/librist/default.nix
@@ -0,0 +1,49 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitLab,
+  meson,
+  ninja,
+  pkg-config,
+  cjson,
+  cmocka,
+  mbedtls,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "librist";
+  version = "0.2.11";
+
+  src = fetchFromGitLab {
+    domain = "code.videolan.org";
+    owner = "rist";
+    repo = "librist";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-xWqyQl3peB/ENReMcDHzIdKXXCYOJYbhhG8tcSh36dY=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    cjson
+    cmocka
+    mbedtls
+  ];
+
+  meta = {
+    description = "Library that can be used to easily add the RIST protocol to your application";
+    homepage = "https://code.videolan.org/rist/librist";
+    license = with lib.licenses; [
+      bsd2
+      mit
+      isc
+    ];
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
