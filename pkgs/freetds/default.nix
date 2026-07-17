d93314313a424df000a47ae6d97052e668837a9a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 09:02:40 -0700

    freetds: init at 1.5.18

---
+++ pkgs/freetds/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoreconfHook,
+  pkg-config,
+  openssl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "freetds";
+  version = "1.5.18";
+
+  src = fetchurl {
+    url = "https://www.freetds.org/files/stable/freetds-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-ayyLk7nufIOFXa90XeWHh5ADLxTbruVT2DqdIRuE3Us=";
+  };
+
+  patches = [
+    ./gettext-0.25.patch
+  ];
+
+  buildInputs = [
+    openssl
+  ];
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+
+  meta = {
+    description = "Libraries to natively talk to Microsoft SQL Server and Sybase databases";
+    homepage = "https://www.freetds.org";
+    changelog = "https://github.com/FreeTDS/freetds/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.lgpl2;
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+  };
+})
