902b79fba0ef7db97ebc30c1c75fe0ae212abf2b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 01:19:03 -0700

    capnproto: init at 1.4.0

---
+++ pkgs/capnproto/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  pkg-config,
+  clangStdenv,
+  fetchFromGitHub,
+  cmake,
+  openssl,
+  zlib,
+}:
+
+clangStdenv.mkDerivation rec {
+  pname = "capnproto";
+  version = "1.4.0";
+
+  src = fetchFromGitHub {
+    owner = "capnproto";
+    repo = "capnproto";
+    rev = "v${version}";
+    hash = "sha256-CuhKOJwU+QG25lRR8F7ina+DV45ZlLzg/UJ2swf2tZ0=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    pkg-config
+  ];
+
+  propagatedBuildInputs = [
+    openssl
+    zlib
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "BUILD_SHARED_LIBS" true)
+    (lib.cmakeBool "WITH_FIBERS" false)
+    (lib.cmakeFeature "CMAKE_BUILD_TYPE" "None")
+  ];
+
+  env = {
+    CXXFLAGS = "-std=c++20";
+  };
+
+  meta = {
+    homepage = "https://capnproto.org/";
+    description = "Cap'n Proto cerealization protocol";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+  };
+}
