0808b71cbbaa2c6310fab62024b77a4953dcaea5
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:39:34 -0700

    matio: init at 1.5.30

---
+++ pkgs/matio/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  hdf5,
+  pkg-config,
+  zlib,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "matio";
+  version = "1.5.30";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/matio/matio-${finalAttrs.version}.tar.gz";
+    hash = "sha256-i9O5R3BC7MAN1xwEdi+lhGjhTMzDL9jGgmwtoei8MQc=";
+  };
+
+  configureFlags = [ "ac_cv_va_copy=1" ];
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    hdf5
+    zlib
+  ];
+
+  meta = {
+    description = "C library for reading and writing Matlab MAT files";
+    homepage = "https://matio.sourceforge.net/";
+    license = lib.licenses.bsd2;
+    mainProgram = "matdump";
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+  };
+})
