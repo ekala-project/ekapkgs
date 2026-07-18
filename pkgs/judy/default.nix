41e5a8e4f4dcf50d8ac37d219f1d78fdb6cd5556
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 06:04:48 -0700

    judy: init at 1.0.5

---
+++ pkgs/judy/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkgsBuildBuild,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "judy";
+  version = "1.0.5";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/judy/Judy-${version}.tar.gz";
+    sha256 = "1sv3990vsx8hrza1mvq3bhvv9m6ff08y4yz7swn6znszz24l0w6j";
+  };
+
+  nativeBuildInputs = [ autoreconfHook ];
+  depsBuildBuild = [ pkgsBuildBuild.stdenv.cc ];
+  patches = [
+    ./cross.patch
+    ./fix-source-date.patch
+  ];
+
+  enableParallelBuilding = false;
+
+  meta = {
+    homepage = "https://judy.sourceforge.net/";
+    license = lib.licenses.lgpl21Plus;
+    description = "State-of-the-art C library that implements a sparse dynamic array";
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
