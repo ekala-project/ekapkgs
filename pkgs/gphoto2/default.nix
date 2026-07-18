fca9a6e1af0d4bdc1cca6a92aeb71bebc59fc5c8
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 23:57:44 -0700

    gphoto2: init at 2.5.28

---
+++ pkgs/gphoto2/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+  gettext,
+  libexif,
+  libgphoto2,
+  libjpeg,
+  libtool,
+  popt,
+  readline,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "gphoto2";
+  version = "2.5.28";
+
+  src = fetchFromGitHub {
+    owner = "gphoto";
+    repo = "gphoto2";
+    rev = "v${version}";
+    sha256 = "sha256-t5EnM4WaDbOTPM+rJW+hQxBgNErnnZEN9lZvxTKoDhA=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    gettext
+    libtool
+  ];
+
+  buildInputs = [
+    libexif
+    libgphoto2
+    libjpeg
+    popt
+    readline
+  ];
+
+  meta = {
+    description = "Ready to use set of digital camera software applications";
+    homepage = "http://www.gphoto.org/";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+    mainProgram = "gphoto2";
+  };
+}
