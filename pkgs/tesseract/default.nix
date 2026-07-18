a1036b9d2f686aa36298cbfde454633ec8e1de0c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:49:17 -0700

    tesseract: init at 5.5.1

---
+++ pkgs/tesseract/default.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+  curl,
+  leptonica,
+  libarchive,
+  libpng,
+  libtiff,
+  icu,
+  pango,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "tesseract";
+  version = "5.5.1";
+
+  src = fetchFromGitHub {
+    owner = "tesseract-ocr";
+    repo = "tesseract";
+    rev = version;
+    sha256 = "sha256-bLTYdT9CNfgrmmjP6m0rRqJDHiSOkcuGVCFwPqT12jk=";
+  };
+
+  enableParallelBuilding = true;
+
+  nativeBuildInputs = [
+    pkg-config
+    autoreconfHook
+  ];
+
+  buildInputs = [
+    curl
+    leptonica
+    libarchive
+    libpng
+    libtiff
+    icu
+    pango
+  ];
+
+  meta = {
+    description = "OCR engine";
+    homepage = "https://github.com/tesseract-ocr/tesseract";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.unix;
+    mainProgram = "tesseract";
+    maintainers = [ ];
+  };
+}
