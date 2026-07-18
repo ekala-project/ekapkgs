90ad70435b0f7b143d582e23234e259a54071cce
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:45:34 -0700

    libxmlb: init at 0.3.22

---
+++ pkgs/libxmlb/default.nix
@@ -0,0 +1,72 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  docbook_xml_dtd_43,
+  docbook-xsl-nons,
+  glib,
+  gobject-introspection,
+  gtk-doc,
+  meson,
+  ninja,
+  pkg-config,
+  python3,
+  shared-mime-info,
+  xz,
+  zstd,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "libxmlb";
+  version = "0.3.22";
+
+  outputs = [
+    "out"
+    "lib"
+    "dev"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "hughsie";
+    repo = "libxmlb";
+    rev = version;
+    hash = "sha256-6S/4X6dYsVj9v98LoDJjir6Kmb5L8PloD23yvvkiD6o=";
+  };
+
+  nativeBuildInputs = [
+    docbook_xml_dtd_43
+    docbook-xsl-nons
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    python3
+    shared-mime-info
+    gobject-introspection
+    gtk-doc
+  ];
+
+  buildInputs = [
+    glib
+    xz
+    zstd
+  ];
+
+  mesonFlags = [
+    "--libexecdir=${placeholder "out"}/libexec"
+    "-Dgtkdoc=false"
+    "-Dintrospection=true"
+    "-Dtests=false"
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Library to help create and query binary XML blobs";
+    mainProgram = "xb-tool";
+    homepage = "https://github.com/hughsie/libxmlb";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
