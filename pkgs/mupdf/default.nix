9848f53f6304f67b59a1b6cd9233584efde73c33
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 06:15:27 -0700

    mupdf: init at 1.25.3

--- pkgs/mupdf/default.nix
+++ pkgs/mupdf/default.nix
@@ -2,7 +2,6 @@
   stdenv,
   lib,
   fetchurl,
-  fetchFromGitHub,
   buildPackages,
   pkg-config,
   freetype,
@@ -11,31 +10,10 @@
   jbig2dec,
   libjpeg,
   gumbo,
-  enableX11 ? true,
-  libX11,
-  libXext,
-  libXi,
-  libXrandr,
   enableCurl ? true,
   curl,
   openssl,
-  enableGL ? true,
-  freeglut,
-  libGLU,
 }:
-
-let
-  freeglut-mupdf = freeglut.overrideAttrs (old: rec {
-    pname = "freeglut-mupdf";
-    version = "3.0.0-r${src.rev}";
-    src = fetchFromGitHub {
-      owner = "ArtifexSoftware";
-      repo = "thirdparty-freeglut";
-      rev = "13ae6aa2c2f9a7b4266fc2e6116c876237f40477";
-      hash = "sha256-0fuE0lm9rlAaok2Qe0V1uUrgP4AjMWgp3eTbw8G6PMM=";
-    };
-  });
-in
 
 stdenv.mkDerivation rec {
   version = "1.25.3";
@@ -46,10 +24,6 @@ stdenv.mkDerivation rec {
     hash = "sha256-uXTXBqloBTPRBLQRIiTHvz3pPye+fKQbS/tRVSYk8Kk=";
   };
 
-  patches = [
-    ./fix-darwin-system-deps.patch
-  ];
-
   postPatch = ''
     substituteInPlace Makerules --replace "(shell pkg-config" "(shell $PKG_CONFIG"
   '';
@@ -58,39 +32,34 @@ stdenv.mkDerivation rec {
     "prefix=$(out)"
     "shared=yes"
     "USE_SYSTEM_LIBS=yes"
+    "HAVE_X11=no"
+    "HAVE_GLUT=no"
     "PKG_CONFIG=${buildPackages.pkg-config}/bin/${buildPackages.pkg-config.targetPrefix}pkg-config"
-  ]
-  ++ lib.optionals (!enableX11) [ "HAVE_X11=no" ]
-  ++ lib.optionals (!enableGL) [ "HAVE_GLUT=no" ];
-
-  nativeBuildInputs = [
-    pkg-config
   ];
 
-  buildInputs = [
-    freetype
-    harfbuzz
-    openjpeg
-    jbig2dec
-    libjpeg
-    gumbo
-  ]
-  ++ lib.optionals enableX11 [
-    libX11
-    libXext
-    libXi
-    libXrandr
-  ]
-  ++ lib.optionals enableCurl [
-    curl
-    openssl
-  ]
-  ++ lib.optionals enableGL [
-    freeglut-mupdf
-    libGLU
-  ];
+  nativeBuildInputs = [ pkg-config ];
 
-  outputs = [ "bin" "dev" "out" "man" "doc" ];
+  buildInputs =
+    [
+      freetype
+      harfbuzz
+      openjpeg
+      jbig2dec
+      libjpeg
+      gumbo
+    ]
+    ++ lib.optionals enableCurl [
+      curl
+      openssl
+    ];
+
+  outputs = [
+    "bin"
+    "dev"
+    "out"
+    "man"
+    "doc"
+  ];
 
   preConfigure = ''
     rm -rf thirdparty/{curl,freetype,glfw,harfbuzz,jbig2dec,libjpeg,openjpeg,zlib}
@@ -111,16 +80,7 @@ stdenv.mkDerivation rec {
     EOF
 
     moveToOutput "bin" "$bin"
-  '' + (lib.optionalString (enableX11 || enableGL) ''
-    mkdir -p $bin/share/icons/hicolor/48x48/apps
-    cp docs/logo/mupdf-icon-48.png $bin/share/icons/hicolor/48x48/apps
-  '') + (
-    if enableGL then ''
-      ln -s "$bin/bin/mupdf-gl" "$bin/bin/mupdf"
-    '' else lib.optionalString enableX11 ''
-      ln -s "$bin/bin/mupdf-x11" "$bin/bin/mupdf"
-    ''
-  );
+  '';
 
   enableParallelBuilding = true;
 
@@ -133,6 +93,6 @@ stdenv.mkDerivation rec {
     license = lib.licenses.agpl3Plus;
     maintainers = [ ];
     platforms = lib.platforms.unix;
-    mainProgram = "mupdf";
+    mainProgram = "mutool";
   };
 }
