a019cda0aa70b48f7e03ee31b775ed1cc3c788c3
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:39:12 -0700

    gmime: init at 3.2.15

---
+++ pkgs/gmime/default.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  glib,
+  zlib,
+  gpgme,
+  libidn2,
+  libunistring,
+}:
+
+stdenv.mkDerivation rec {
+  version = "3.2.15";
+  pname = "gmime";
+
+  src = fetchurl {
+    url = "https://github.com/jstedfast/gmime/releases/download/${version}/gmime-${version}.tar.xz";
+    sha256 = "sha256-hM0qSBonlw7Dm1yV9y2wJnIpBKLM8/29V7KAzy0CtcQ=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    zlib
+    gpgme
+    libidn2
+    libunistring
+  ];
+
+  propagatedBuildInputs = [ glib ];
+
+  configureFlags = [
+    "--enable-introspection=no"
+    "--enable-vala=no"
+  ];
+
+  postPatch = ''
+    substituteInPlace tests/testsuite.c \
+      --replace /bin/rm rm
+  '';
+
+  enableParallelBuilding = true;
+
+  meta = {
+    homepage = "https://github.com/jstedfast/gmime/";
+    description = "C/C++ library for creating, editing and parsing MIME messages and structures";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
