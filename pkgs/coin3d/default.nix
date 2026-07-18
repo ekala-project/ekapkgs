86ff2a9e2d94adaf8283a2a02382b1e2a8ee01ae
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 12:34:22 -0700

    coin3d: init at 4.0.3

---
+++ pkgs/coin3d/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  boost,
+  cmake,
+  libGL,
+  libGLU,
+  xorg,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "coin";
+  version = "4.0.3";
+
+  src = fetchFromGitHub {
+    owner = "coin3d";
+    repo = "coin";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-dUFmcUOdNc3ZFtr+Hnh3Q3OY/JA/WxmiRJiU2RFSSus=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  buildInputs = [
+    boost
+    libGL
+    libGLU
+  ] ++ lib.optional stdenv.hostPlatform.isLinux xorg.libX11;
+
+  cmakeFlags = [ "-DCOIN_USE_CPACK=OFF" ];
+
+  meta = {
+    homepage = "https://github.com/coin3d/coin";
+    description = "High-level, retained-mode toolkit for effective 3D graphics development";
+    mainProgram = "coin-config";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+})
