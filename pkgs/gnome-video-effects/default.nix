c2b37e64d952cb847b7654f04c6db505fc70f07f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 12:04:01 -0700

    gnome-video-effects: init at 0.6.0

---
+++ pkgs/gnome-video-effects/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  meson,
+  ninja,
+  gettext,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "gnome-video-effects";
+  version = "0.6.0";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
+    sha256 = "166utGs/WoMvsuDZC0K/jGFgICylKsmt0Xr84ZLjyKg=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    gettext
+  ];
+
+  meta = with lib; {
+    description = "Collection of GStreamer effects to be used in different GNOME Modules";
+    homepage = "https://gitlab.gnome.org/GNOME/gnome-video-effects";
+    platforms = platforms.unix;
+    license = licenses.gpl2;
+    maintainers = [ ];
+  };
+}
