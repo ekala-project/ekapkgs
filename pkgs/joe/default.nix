044d70a0ab2046498e2882a0b6ec29e3d3a8daa7
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 00:10:41 -0700

    joe: init at 4.6

---
+++ pkgs/joe/default.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "joe";
+  version = "4.6";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/joe-editor/${pname}-${version}.tar.gz";
+    sha256 = "1pmr598xxxm9j9dl93kq4dv36zyw0q2dh6d7x07hf134y9hhlnj9";
+  };
+
+  meta = {
+    description = "Full featured terminal-based screen editor";
+    homepage = "https://joe-editor.sourceforge.io";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
