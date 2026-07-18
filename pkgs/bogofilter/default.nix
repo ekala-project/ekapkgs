a329a54c26399ce593e9d50baff05631f4ea5819
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 11:21:03 -0700

    bogofilter: init at 1.2.5

---
+++ pkgs/bogofilter/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  flex,
+  db,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "bogofilter";
+  version = "1.2.5";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/bogofilter/bogofilter-${version}.tar.xz";
+    hash = "sha256-MkihNzv/VSxQCDStvqS2yu4EIkUWrlgfslpMam3uieo=";
+  };
+
+  buildInputs = [
+    flex
+    db
+  ];
+
+  doCheck = false; # needs "y" tool
+
+  meta = {
+    homepage = "http://bogofilter.sourceforge.net/";
+    description = "Mail filter that classifies mail as spam or ham by statistical analysis";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+}
