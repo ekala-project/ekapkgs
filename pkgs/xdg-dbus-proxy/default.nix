a12186677508d333e694793d80df5255ae3e9074
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 23:44:21 -0700

    xdg-dbus-proxy: init at 0.1.6

---
+++ pkgs/xdg-dbus-proxy/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  docbook-xsl-nons,
+  docbook_xml_dtd_43,
+  glib,
+  libxslt,
+  meson,
+  ninja,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "xdg-dbus-proxy";
+  version = "0.1.6";
+
+  src = fetchurl {
+    url = "https://github.com/flatpak/xdg-dbus-proxy/releases/download/${finalAttrs.version}/xdg-dbus-proxy-${finalAttrs.version}.tar.xz";
+    hash = "sha256-Exv1n858fufsvF2RBtZ1D09Ze/5gmWZXMkD35JUpc6E=";
+  };
+
+  nativeBuildInputs = [
+    docbook-xsl-nons
+    docbook_xml_dtd_43
+    libxslt
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    glib
+  ];
+
+  # dbus[2345]: Failed to start message bus: Failed to open "/etc/dbus-1/session.conf": No such file or directory
+  doCheck = false;
+
+  meta = {
+    description = "DBus proxy for Flatpak and others";
+    homepage = "https://github.com/flatpak/xdg-dbus-proxy";
+    license = lib.licenses.lgpl21Plus;
+    mainProgram = "xdg-dbus-proxy";
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
