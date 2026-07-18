bf65da0b0520c8d18296240fccb1a37b97c2de66
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 12:30:55 -0700

    vulkan-utility-libraries: init at 1.4.313.0

---
+++ pkgs/vulkan-utility-libraries/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  python3,
+  vulkan-headers,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "vulkan-utility-libraries";
+  version = "1.4.313.0";
+
+  src = fetchFromGitHub {
+    owner = "KhronosGroup";
+    repo = "Vulkan-Utility-Libraries";
+    rev = "vulkan-sdk-${finalAttrs.version}";
+    hash = "sha256-MmC4UVa9P/0h7r8IBp1LhP9EztwyZv/ASWKKj8Gk1T8=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    python3
+  ];
+
+  buildInputs = [ vulkan-headers ];
+
+  meta = {
+    description = "Set of utility libraries for Vulkan";
+    homepage = "https://github.com/KhronosGroup/Vulkan-Utility-Libraries";
+    platforms = lib.platforms.all;
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
