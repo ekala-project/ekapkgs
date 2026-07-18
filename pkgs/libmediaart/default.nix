ff9b51790fae3d68cc9aa7f8cabc9fccc7f0bffd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 00:07:33 -0700

    libmediaart: init at 1.9.7

---
+++ pkgs/libmediaart/default.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  meson,
+  ninja,
+  pkg-config,
+  vala,
+  gtk-doc,
+  docbook-xsl-nons,
+  docbook_xml_dtd_412,
+  glib,
+  gdk-pixbuf,
+  gobject-introspection,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libmediaart";
+  version = "1.9.7";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/libmediaart/${lib.versions.majorMinor finalAttrs.version}/libmediaart-${finalAttrs.version}.tar.xz";
+    sha256 = "K0Pdn1Tw2NC4nirduDNBqwbXuYyxsucEODWEr5xWD2s=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    vala
+    gtk-doc
+    docbook-xsl-nons
+    docbook_xml_dtd_412
+    gobject-introspection
+  ];
+
+  buildInputs = [
+    glib
+    gdk-pixbuf
+  ];
+
+  mesonFlags = [
+    "-Dgtk_doc=false"
+  ];
+
+  meta = {
+    description = "Library tasked with managing, extracting and handling media art caches";
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
