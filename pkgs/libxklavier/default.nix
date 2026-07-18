e7d59aaf71f2056ba938f72647c9b236b0ad9db6
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:40:30 -0700

    libxklavier: init at 5.4

---
+++ pkgs/libxklavier/default.nix
@@ -0,0 +1,75 @@
+{
+  lib,
+  stdenv,
+  fetchgit,
+  autoreconfHook,
+  pkg-config,
+  gtk-doc,
+  xkeyboard-config,
+  libxml2,
+  xorg,
+  docbook_xsl,
+  glib,
+  isocodes,
+  libxkbfile,
+  gobject-introspection,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "libxklavier";
+  version = "5.4";
+
+  src = fetchgit {
+    url = "https://gitlab.freedesktop.org/archived-projects/libxklavier.git";
+    rev = "${pname}-${version}";
+    sha256 = "1w1x5mrgly2ldiw3q2r6y620zgd89gk7n90ja46775lhaswxzv7a";
+  };
+
+  patches = [
+    ./honor-XKB_CONFIG_ROOT.patch
+  ];
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  propagatedBuildInputs = [
+    xorg.libX11
+    xorg.libXi
+    xkeyboard-config
+    libxml2
+    xorg.libICE
+    glib
+    libxkbfile
+    isocodes
+  ];
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    gtk-doc
+    docbook_xsl
+    gobject-introspection
+  ];
+
+  preAutoreconf = ''
+    export NOCONFIGURE=1
+    gtkdocize
+  '';
+
+  configureFlags = [
+    "--with-xkb-base=${xkeyboard-config}/etc/X11/xkb"
+    "--with-xkb-bin-base=${xorg.xkbcomp}/bin"
+    "--disable-xmodmap-support"
+    "--disable-gtk-doc"
+  ];
+
+  meta = {
+    description = "Library providing high-level API for X Keyboard Extension known as XKB";
+    homepage = "http://freedesktop.org/wiki/Software/LibXklavier";
+    license = lib.licenses.lgpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+}
