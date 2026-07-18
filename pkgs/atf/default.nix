7af058132c98d1f4d28d1ac24324f7d520711b98
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 16:08:46 -0700

    atf: init at 0.23

---
+++ pkgs/atf/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "atf";
+  version = "0.23";
+
+  src = fetchFromGitHub {
+    owner = "freebsd";
+    repo = "atf";
+    tag = "atf-${finalAttrs.version}";
+    hash = "sha256-g9cXeiCaiyGQXtg6eyrbRQpqk4VLGSFuhfPB+ynbDIo=";
+  };
+
+  postPatch = lib.optionalString stdenv.hostPlatform.isLinux ''
+    substituteInPlace atf-c/detail/fs_test.c \
+      --replace-fail 'ATF_TP_ADD_TC(tp, eaccess);' ""
+  '';
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  enableParallelBuilding = true;
+
+  makeFlags = [
+    # ATF isn't compatible with C++17, which is the default on current clang and GCC.
+    "CXXFLAGS=-std=c++14"
+  ];
+
+  __structuredAttrs = true;
+
+  meta = {
+    description = "Libraries to write tests in C, C++, and shell";
+    homepage = "https://github.com/freebsd/atf/";
+    license = lib.licenses.bsd3;
+    mainProgram = "atf-sh";
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
