04f5598a4c058988e458ac0b1fff493a2352a435
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 09:07:00 -0700

    telepathy-glib: init at 0.24.2

---
+++ pkgs/telepathy-glib/default.nix
@@ -0,0 +1,80 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  fetchpatch,
+  dbus-glib,
+  glib,
+  python3,
+  pkg-config,
+  libxslt,
+  gobject-introspection,
+  vala,
+  glibcLocales,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "telepathy-glib";
+  version = "0.24.2";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "https://telepathy.freedesktop.org/releases/telepathy-glib/${pname}-${version}.tar.gz";
+    sha256 = "sKN013HN0IESXzjDq9B5ZXZCMBxxpUPVVeK/IZGSc/A=";
+  };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-incompatible-pointer-types.patch";
+      url = "https://github.com/TelepathyIM/telepathy-glib/commit/72412c944b771f3214ddc40fa9dea82cea3a5651.patch";
+      hash = "sha256-NXQel0eS7zK6FRbJcPsPXCQxos0xT8EN102vX94M5Vo=";
+    })
+  ];
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    pkg-config
+    libxslt
+    gobject-introspection
+    vala
+    python3
+  ];
+
+  buildInputs = [
+    glibcLocales
+  ];
+
+  propagatedBuildInputs = [
+    dbus-glib
+    glib
+  ];
+
+  configureFlags = [
+    "--enable-vala-bindings"
+  ];
+
+  LC_ALL = "en_US.UTF-8";
+
+  enableParallelBuilding = true;
+
+  preConfigure = ''
+    substituteInPlace telepathy-glib/telepathy-glib.pc.in --replace Requires.private Requires
+  '';
+
+  meta = {
+    homepage = "https://telepathy.freedesktop.org";
+    description = "GLib-based D-Bus library for the Telepathy communication framework";
+    license = with lib.licenses; [
+      bsd2
+      bsd3
+      lgpl21Plus
+    ];
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+}
