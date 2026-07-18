3d3dd05b6570e647b39382d8bd43983c84d95e0c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 23:50:09 -0700

    tomcat: init at 11.0.7

---
+++ pkgs/tomcat/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchurl,
+  jdk,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "apache-tomcat";
+  version = "11.0.7";
+
+  src = fetchurl {
+    url = "mirror://apache/tomcat/tomcat-${lib.versions.major finalAttrs.version}/v${finalAttrs.version}/bin/apache-tomcat-${finalAttrs.version}.tar.gz";
+    hash = "sha256-L87OZBxiuh8o4deyV0kxUfxE8WH7ORAV7mqV+nFjL7k=";
+  };
+
+  outputs = [
+    "out"
+    "webapps"
+  ];
+
+  installPhase = ''
+    mkdir $out
+    mv * $out
+    mkdir -p $webapps/webapps
+    mv $out/webapps $webapps/
+  '';
+
+  meta = {
+    homepage = "https://tomcat.apache.org/";
+    description = "Implementation of the Java Servlet and JavaServer Pages technologies";
+    platforms = jdk.meta.platforms;
+    license = lib.licenses.asl20;
+    sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
+    maintainers = [ ];
+  };
+})
