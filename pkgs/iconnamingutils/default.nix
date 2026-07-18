5a428622b23052795b696d9954db4f817f027fae
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:34:58 -0700

    iconnamingutils: init at 0.8.90

---
+++ pkgs/iconnamingutils/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  perl,
+  librsvg,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "icon-naming-utils";
+  version = "0.8.90";
+
+  src = fetchurl {
+    url = "http://tango.freedesktop.org/releases/${pname}-${version}.tar.gz";
+    sha256 = "071fj2jm5kydlz02ic5sylhmw6h2p3cgrm3gwdfabinqkqcv4jh4";
+  };
+
+  buildInputs = [
+    librsvg
+    (perl.withPackages (p: [ p.XMLSimple ]))
+  ];
+
+  meta = {
+    homepage = "https://tango.freedesktop.org/Standard_Icon_Naming_Specification";
+    platforms = lib.platforms.linux;
+    license = lib.licenses.gpl2;
+    maintainers = [ ];
+  };
+}
