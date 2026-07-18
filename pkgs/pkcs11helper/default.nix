3e9befbeea0f480bbab7cbf4a650307905cef86e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 12:54:00 -0700

    pkcs11helper: init at 1.30.0

---
+++ pkgs/pkcs11helper/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "pkcs11-helper";
+  version = "1.30.0";
+
+  src = fetchFromGitHub {
+    owner = "OpenSC";
+    repo = "pkcs11-helper";
+    rev = "${pname}-${version}";
+    hash = "sha256-FP3y/YHsPPqey4QfxIiC4QjruuK1K2Bg+2QL2gXDT+k=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+
+  buildInputs = [ openssl ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "Library that simplifies the interaction with PKCS#11 providers";
+    homepage = "https://github.com/OpenSC/pkcs11-helper";
+    license = with lib.licenses; [
+      bsd3
+      gpl2Only
+    ];
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+}
