266717a38f152c6d580a53cfb7b7a5b8be4cbdb2
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 23:51:17 -0700

    geocode-glib: init at 3.26.4

---
+++ pkgs/geocode-glib/default.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  meson,
+  ninja,
+  pkg-config,
+  gettext,
+  gtk-doc,
+  docbook-xsl-nons,
+  gobject-introspection,
+  libsoup,
+  json-glib,
+  glib,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "geocode-glib";
+  version = "3.26.4";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/geocode-glib/${lib.versions.majorMinor finalAttrs.version}/geocode-glib-${finalAttrs.version}.tar.xz";
+    sha256 = "LZpoJtFYRwRJoXOHEiFZbaD4Pr3P+YuQxwSQiQVqN6o=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    gettext
+    gtk-doc
+    docbook-xsl-nons
+    gobject-introspection
+  ];
+
+  buildInputs = [
+    glib
+    libsoup
+    json-glib
+  ];
+
+  mesonFlags = [
+    "-Dsoup2=false"
+    "-Denable-installed-tests=false"
+    "-Denable-introspection=false"
+    "-Denable-gtk-doc=false"
+  ];
+
+  meta = {
+    description = "Convenience library for geocoding and reverse geocoding using Nominatim service";
+    homepage = "https://gitlab.gnome.org/GNOME/geocode-glib";
+    license = lib.licenses.lgpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
