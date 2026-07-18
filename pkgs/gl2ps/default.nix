a9f4d2a4225a323e0b329caa457f51ccff01b77d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 06:10:09 -0700

    gl2ps: init at 1.4.2

---
+++ pkgs/gl2ps/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  cmake,
+  zlib,
+  libpng,
+  libGL,
+  libGLU,
+  freeglut,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "gl2ps";
+  version = "1.4.2";
+
+  src = fetchurl {
+    url = "http://geuz.org/gl2ps/src/${pname}-${version}.tgz";
+    sha256 = "1sgzv547h7hrskb9qd0x5yp45kmhvibjwj2mfswv95lg070h074d";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  buildInputs =
+    [
+      zlib
+      libpng
+    ]
+    ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
+      libGL
+      libGLU
+      freeglut
+    ];
+
+  meta = {
+    homepage = "http://geuz.org/gl2ps";
+    description = "OpenGL to PostScript printing library";
+    platforms = lib.platforms.all;
+    license = lib.licenses.lgpl2;
+    maintainers = [ ];
+  };
+}
