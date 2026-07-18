cb04e2ee0a9d6f80968573234dd7405316a7ffcd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 22:19:07 -0700

    aspell-dict-en: init at 2020.12.07-0

---
+++ pkgs/aspell-dict-en/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  aspell,
+  which,
+}:
+
+stdenv.mkDerivation {
+  pname = "aspell-dict-en";
+  version = "2020.12.07-0";
+
+  src = fetchurl {
+    url = "mirror://gnu/aspell/dict/en/aspell6-en-2020.12.07-0.tar.bz2";
+    sha256 = "1cwzqkm8gr1w51rpckwlvb43sb0b5nbwy7s8ns5vi250515773sc";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    aspell
+    which
+  ];
+
+  dontAddPrefix = true;
+  configurePlatforms = [ ];
+
+  preBuild = "makeFlagsArray=(dictdir=$out/lib/aspell datadir=$out/lib/aspell)";
+
+  postInstall = ''
+    rm -f $out/lib/aspell/u-deva.{cmap,cset}
+  '';
+
+  meta = {
+    description = "Aspell dictionary for English";
+    license = with lib.licenses; [
+      free
+      publicDomain
+      bsdOriginalUC
+    ];
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+  };
+}
