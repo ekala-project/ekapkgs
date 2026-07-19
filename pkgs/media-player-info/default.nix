ee87c870ae45cae228fecee13b6b913c3d8ba43f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 17:15:05 -0700

    media-player-info: init at 26

---
+++ pkgs/media-player-info/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  autoreconfHook,
+  pkg-config,
+  python3,
+  udev,
+  systemd,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "media-player-info";
+  version = "26";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    owner = "media-player-info";
+    repo = "media-player-info";
+    rev = version;
+    hash = "sha256-VoMr5Lxy6u/BA/9t65/S8AW41YU0FLp6eftYUVdoMjY=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    python3
+  ];
+
+  buildInputs = [
+    udev
+    systemd
+  ];
+
+  postPatch = ''
+    patchShebangs ./tools
+  '';
+
+  configureFlags = [ "--with-udevdir=${placeholder "out"}/lib/udev" ];
+
+  meta = with lib; {
+    description = "Repository of data files describing media player capabilities";
+    homepage = "https://www.freedesktop.org/wiki/Software/media-player-info/";
+    license = licenses.bsd3;
+    platforms = platforms.linux;
+    maintainers = [ ];
+  };
+}
