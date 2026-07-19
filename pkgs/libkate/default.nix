f2114bfe07456cad0fa6e444064736dcce5aec6d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 18:19:46 -0700

    libkate: init at 0.4.3

---
+++ pkgs/libkate/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  autoreconfHook,
+  bison,
+  flex,
+  libogg,
+  libpng,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libkate";
+  version = "0.4.3";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.xiph.org/";
+    owner = "xiph";
+    repo = "kate";
+    tag = "kate-${finalAttrs.version}";
+    hash = "sha256-HwDahmjDC+O321Ba7MnHoQdHOFUMpFzaNdLHQeEg11Q=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    bison
+    flex
+    pkg-config
+  ];
+
+  buildInputs = [
+    libogg
+    libpng
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "Library for encoding and decoding Kate streams";
+    homepage = "https://wiki.xiph.org/index.php/OggKate";
+    platforms = lib.platforms.unix;
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
