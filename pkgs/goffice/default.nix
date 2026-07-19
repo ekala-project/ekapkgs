6b2f09e6fca6f196450305ecc1d804b3f1875e9c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 17:46:15 -0700

    goffice: init at 0.10.59

---
+++ pkgs/goffice/default.nix
@@ -0,0 +1,70 @@
+{
+  fetchurl,
+  lib,
+  stdenv,
+  pkg-config,
+  intltool,
+  glib,
+  gtk3,
+  lasem,
+  libgsf,
+  libxml2,
+  libxslt,
+  cairo,
+  pango,
+  librsvg,
+  autoreconfHook,
+  gtk-doc,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "goffice";
+  version = "0.10.59";
+
+  outputs = [
+    "out"
+    "dev"
+    "devdoc"
+  ];
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/goffice/${lib.versions.majorMinor finalAttrs.version}/goffice-${finalAttrs.version}.tar.xz";
+    hash = "sha256-sI9xczJVlLcfu+pHajC1sxIMPa3/XAom0UDk5SSRZiI=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    intltool
+    autoreconfHook
+    gtk-doc
+    glib # for glib-genmarshal
+  ];
+
+  propagatedBuildInputs = [
+    glib
+    gtk3
+    libxml2
+    cairo
+    pango
+    libgsf
+    lasem
+  ];
+
+  buildInputs = [
+    libxslt
+    librsvg
+  ];
+
+  configureFlags = [
+    "--disable-introspection"
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "Glib/GTK set of document centric objects and utilities";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
