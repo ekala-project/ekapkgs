78e98282565aa300e84024422671dd53bb50fbef
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 09:56:33 -0700

    aptdec: init at 1.8.0

---
+++ pkgs/aptdec/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  libpng,
+  libsndfile,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "aptdec";
+  version = "1.8.0";
+
+  src = fetchFromGitHub {
+    owner = "Xerbo";
+    repo = "aptdec";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-5Pr2PlCPSEIWnThJXKcQEudmxhLJC2sVa9BfAOEKHB4=";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    pkg-config
+  ];
+
+  buildInputs = [
+    libpng
+    libsndfile
+  ];
+
+  cmakeFlags = [ (lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10") ];
+
+  meta = {
+    description = "NOAA APT satellite imagery decoding library";
+    mainProgram = "aptdec";
+    homepage = "https://github.com/Xerbo/aptdec";
+    license = lib.licenses.gpl2;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
