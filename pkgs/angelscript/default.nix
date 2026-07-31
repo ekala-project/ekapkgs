2983d77159c06083763aa516769f0468812b1047
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 09:51:11 -0700

    angelscript: init at 2.38.0

---
+++ pkgs/angelscript/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  unzip,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "angelscript";
+  version = "2.38.0";
+
+  src = fetchurl {
+    url = "https://www.angelcode.com/angelscript/sdk/files/angelscript_${finalAttrs.version}.zip";
+    sha256 = "sha256-sztdvNoQMX72fWKDU9gyRphM5vysEC1Nwq7RIeulLm8=";
+  };
+
+  nativeBuildInputs = [
+    unzip
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  preConfigure = ''
+    export ROOT=$PWD
+    cd angelscript/projects/cmake
+  '';
+
+  cmakeFlags = [ "-DBUILD_SHARED_LIBS=ON" ];
+
+  postInstall = ''
+    mkdir -p "$out/share/docs/angelscript"
+    cp -r $ROOT/docs/* "$out/share/docs/angelscript"
+  '';
+
+  meta = {
+    description = "Light-weight scripting library";
+    license = lib.licenses.zlib;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+    downloadPage = "https://www.angelcode.com/angelscript/downloads.html";
+    homepage = "https://www.angelcode.com/angelscript/";
+  };
+})
