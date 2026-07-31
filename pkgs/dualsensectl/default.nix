40ce5d1d1b89764e83c16b9711cf674156c13a1e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 14:56:53 -0700

    dualsensectl: init at 0.7

---
+++ pkgs/dualsensectl/default.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  installShellFiles,
+  meson,
+  ninja,
+  pkg-config,
+  dbus,
+  hidapi,
+  udev,
+  testers,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "dualsensectl";
+  version = "0.7";
+
+  src = fetchFromGitHub {
+    owner = "nowrep";
+    repo = "dualsensectl";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-/EPFZWpa7U4fmcdX2ycFkPgaqlKEA2cD84LBkcvVVhc=";
+  };
+
+  nativeBuildInputs = [
+    meson.configurePhaseHook
+    installShellFiles
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    dbus
+    hidapi
+    udev
+  ];
+
+  postInstall = ''
+    installShellCompletion --cmd dualsensectl \
+      --bash ../completion/dualsensectl \
+      --zsh ../completion/_dualsensectl
+  '';
+
+  passthru = {
+    tests.version = testers.testVersion { package = finalAttrs.finalPackage; };
+  };
+
+  meta = {
+    changelog = "https://github.com/nowrep/dualsensectl/releases/tag/v${finalAttrs.version}";
+    description = "Linux tool for controlling PS5 DualSense controller";
+    homepage = "https://github.com/nowrep/dualsensectl";
+    license = lib.licenses.gpl2Only;
+    mainProgram = "dualsensectl";
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
