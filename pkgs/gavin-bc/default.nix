36295067edc1fcac05d746253913c1b13f81628b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 15:18:20 -0700

    gavin-bc: init at 7.1.0

---
+++ pkgs/gavin-bc/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "gavin-bc";
+  version = "7.1.0";
+
+  src = fetchFromGitHub {
+    owner = "gavinhoward";
+    repo = "bc";
+    rev = finalAttrs.version;
+    hash = "sha256-bIQk0HzUzL1Ju4+iDpFj1n+GKCj9a3AUAbYA3yX5TNg=";
+  };
+
+  versionCheckProgram = "${placeholder "out"}/bin/bc";
+  meta = {
+    homepage = "https://github.com/gavinhoward/bc";
+    description = "Gavin Howard's BC calculator implementation";
+    changelog = "https://github.com/gavinhoward/bc/blob/${finalAttrs.version}/NEWS.md";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
