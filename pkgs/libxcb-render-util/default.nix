0a08aef5f3f0becf94e1b323d00f6221ac5c0c0e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 15:21:47 -0700

    libxcb-render-util: init at 0.3.10

---
+++ pkgs/libxcb-render-util/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  libxcb,
+  xorgproto,
+  writeScript,
+  testers,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libxcb-render-util";
+  version = "0.3.10";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/xcb/xcb-util-renderutil-${finalAttrs.version}.tar.xz";
+    hash = "sha256-PhXU8OItjdv7ufXXfbQ+rNejBAKb8lphZsxjyqltBLo=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    libxcb
+    xorgproto
+  ];
+
+  propagatedBuildInputs = [ libxcb ];
+
+  passthru = {
+    tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+  };
+
+  meta = {
+    description = "XCB convenience functions for the Render extension";
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libxcb-render-util";
+    license = with lib.licenses; [
+      hpndSellVariant
+      x11
+    ];
+    maintainers = [ ];
+    pkgConfigModules = [ "xcb-renderutil" ];
+    platforms = lib.platforms.unix;
+  };
+})
