aefbca7995b50509a5c508d4a335e48b72fe7351
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 14:20:31 -0700

    bsc: init at 3.3.12

---
+++ pkgs/bsc/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  llvmPackages,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "bsc";
+  version = "3.3.12";
+
+  src = fetchFromGitHub {
+    owner = "IlyaGrebnov";
+    repo = "libbsc";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-3dFwmThnDzbXB6m/rDfbSz4DZAlIsm4gUOT7YwexpKA=";
+  };
+
+  enableParallelBuilding = true;
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  buildInputs = lib.optional stdenv.hostPlatform.isDarwin llvmPackages.openmp;
+
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace-fail "check_c_compiler_flag(-march=native COMPILER_SUPPORTS_MARCH_NATIVE_C)" "" \
+      --replace-fail "check_cxx_compiler_flag(-march=native COMPILER_SUPPORTS_MARCH_NATIVE_CXX)" ""
+  '';
+
+  meta = {
+    description = "High performance block-sorting data compression library";
+    homepage = "http://libbsc.com/";
+    maintainers = [ ];
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.unix;
+    mainProgram = "bsc";
+  };
+})
