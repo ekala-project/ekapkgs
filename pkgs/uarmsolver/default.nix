2c1ffe1f126c8747f02771ae0db11c04de243718
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 15:13:55 -0700

    uarmsolver: init at 0.4.0

---
+++ pkgs/uarmsolver/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "uarmsolver";
+  version = "0.4.0";
+
+  src = fetchFromGitHub {
+    owner = "firefly-cpp";
+    repo = "uARMSolver";
+    rev = finalAttrs.version;
+    sha256 = "sha256-fJtGZ1Y1mL/JjuaDbLfXb+AjTESEGjoh3ZEmhBZKotA=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  installPhase = ''
+    install -D -t $out/bin uARMSolver
+  '';
+
+  meta = {
+    description = "Universal Association Rule Mining Solver";
+    mainProgram = "uARMSolver";
+    homepage = "https://github.com/firefly-cpp/uARMSolver";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
