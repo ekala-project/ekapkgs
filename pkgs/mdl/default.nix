a6de4288b94c5bad4f8e84c80ff1d454cfc0df31
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 14:02:56 -0700

    mdl: init at 0.13.0

---
+++ pkgs/mdl/default.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  bundlerApp,
+  bundlerUpdateScript,
+}:
+
+bundlerApp {
+  pname = "mdl";
+  gemdir = ./.;
+  exes = [ "mdl" ];
+  meta = {
+    description = "Tool to check markdown files and flag style issues";
+    homepage = "https://github.com/markdownlint/markdownlint";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}
