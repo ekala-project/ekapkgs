ddcf81fb81b5a8cc1a6a25ba4bda8a0978018b35
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 13:43:10 -0700

    sonivox: init at 4.0.1

---
+++ pkgs/sonivox/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "sonivox";
+  version = "4.0.1";
+
+  src = fetchFromGitHub {
+    owner = "EmbeddedSynth";
+    repo = "sonivox";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-6ybGHlgI/1uyFNzSiIC4l7FQ6gVJw35NrdpooygnnQo=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  meta = {
+    homepage = "https://github.com/EmbeddedSynth/sonivox";
+    description = "MIDI synthesizer library";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
