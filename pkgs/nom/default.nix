abbc600e946a655136c9fa30aad9e5df49b2c51b
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 16:15:45 -0700

    nom: init at 3.3.2

---
+++ pkgs/nom/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule (finalAttrs: {
+  pname = "nom";
+  version = "3.3.2";
+
+  src = fetchFromGitHub {
+    owner = "guyfedwards";
+    repo = "nom";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-ZsauLHdGkD6cJ9SYwoTyOLu2CDLp8tBGqXn4jDElAMA=";
+  };
+
+  vendorHash = "sha256-otrK4mTqgRr9Ntf2D1f0/deQcObejRWN7BaScV4q+FY=";
+
+  ldflags = [
+    "-X 'main.version=${finalAttrs.version}'"
+  ];
+
+  # only run xdg-specific test on linux
+  checkFlags = lib.optional stdenv.hostPlatform.isDarwin "-skip=^TestNewDefaultWithXDGConfigHome$";
+  meta = {
+    homepage = "https://github.com/guyfedwards/nom";
+    changelog = "https://github.com/guyfedwards/nom/releases/tag/v${finalAttrs.version}";
+    description = "RSS reader for the terminal";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+    mainProgram = "nom";
+  };
+})
