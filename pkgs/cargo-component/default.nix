ddeb3c6265dc6e84459dfa12f19391b8e71cc5a0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 13:38:43 -0700

    cargo-component: init at 0.21.1

---
+++ pkgs/cargo-component/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "cargo-component";
+  version = "0.21.1";
+
+  src = fetchFromGitHub {
+    owner = "bytecodealliance";
+    repo = "cargo-component";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Tlx14q/2k/0jZZ1nECX7zF/xNTeMCZg/fN+fhRM4uhc=";
+  };
+
+  cargoHash = "sha256-ZwxVhoqAzkaIgcH9GMR+IGkJ6IOQVtmt0qcDjdix6cU=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+  ];
+
+  # requires the wasm32-wasi target
+  doCheck = false;
+
+  doInstallCheck = true;
+  versionCheckProgramArg = "--version";
+
+  meta = {
+    description = "Cargo subcommand for creating WebAssembly components based on the component model proposal";
+    homepage = "https://github.com/bytecodealliance/cargo-component";
+    changelog = "https://github.com/bytecodealliance/cargo-component/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    mainProgram = "cargo-component";
+  };
+})
