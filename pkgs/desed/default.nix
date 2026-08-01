b18557b482aa2cf8d99c942075df395c4ad30aeb
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 16:17:06 -0700

    desed: init at 1.2.2

---
+++ pkgs/desed/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "desed";
+  version = "1.2.2";
+
+  src = fetchFromGitHub {
+    owner = "SoptikHa2";
+    repo = "desed";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-aKkOs8IhnHjoJkXq9ryGn9fN0AmZyVTHbD/Vano+Erw=";
+  };
+
+  cargoHash = "sha256-1vNs+viEPqmfA8AtFQaGcQwlLAbIBMHd8uMFmqm60eg=";
+
+  meta = {
+    changelog = "https://github.com/SoptikHa2/desed/releases/tag/v${finalAttrs.version}";
+    description = "Debugger for Sed: demystify and debug your sed scripts, from comfort of your terminal";
+    homepage = "https://github.com/SoptikHa2/desed";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+    mainProgram = "desed";
+  };
+})
