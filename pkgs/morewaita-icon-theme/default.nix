17f3f4654fecb6ca32af875f621fc611665c60b3
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 14:51:26 -0700

    morewaita-icon-theme: init at 49

---
+++ pkgs/morewaita-icon-theme/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+  adwaita-icon-theme,
+  gtk3,
+  xdg-utils,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "morewaita-icon-theme";
+  version = "49";
+
+  src = fetchFromGitHub {
+    owner = "somepaulo";
+    repo = "MoreWaita";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-DxZ7XnIIF3EKGMPXahD+aHp6lCLRmrnywn7+qWCVflo=";
+  };
+
+  postPatch = ''
+    patchShebangs install.sh
+  '';
+
+  nativeBuildInputs = [
+    gtk3
+    xdg-utils
+  ];
+
+  propagatedBuildInputs = [
+    adwaita-icon-theme
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    THEMEDIR="$out/share/icons/MoreWaita" ./install.sh
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Adwaita style extra icons theme for Gnome Shell";
+    homepage = "https://github.com/somepaulo/MoreWaita";
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
