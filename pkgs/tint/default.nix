7e6ca9c412dfc772cf500af1c89c241420e61229
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 16:19:21 -0700

    tint: init at 0.1.7

---
+++ pkgs/tint/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "tint";
+  version = "0.1.7";
+
+  src = fetchFromGitHub {
+    owner = "ashish0kumar";
+    repo = "tint";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Y2Jb/YF7rpEAmDVI5wEB+Sy7Ap2XxNrKQfnAogVdYSY=";
+  };
+
+  vendorHash = null;
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${finalAttrs.version}"
+  ];
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [
+  ];
+  meta = {
+    changelog = "https://github.com/ashish0kumar/tint/releases/tag/v${finalAttrs.version}";
+    description = "Command-line tool to recolor images using theme palettes";
+    homepage = "https://github.com/ashish0kumar/tint";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "tint";
+    platforms = lib.platforms.unix;
+  };
+})
