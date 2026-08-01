adb41443375f00db2d0ad3af85185d57758b0082
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 21:24:22 -0700

    egl-wayland2: init at 1.0.1

---
+++ pkgs/egl-wayland2/default.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  eglexternalplatform,
+  pkg-config,
+  meson,
+  ninja,
+  wayland-scanner,
+  libGL,
+  libgbm,
+  libdrm,
+  wayland,
+  wayland-protocols,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "egl-wayland2";
+  version = "1.0.1";
+
+  src = fetchFromGitHub {
+    owner = "NVIDIA";
+    repo = "egl-wayland2";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Udr+tihx/Si2ynFyM1FW2CIUgTg9SQn7AgrOPpGTxpY=";
+  };
+
+  depsBuildBuild = [
+    pkg-config
+  ];
+
+  nativeBuildInputs = [
+    meson.configurePhaseHook
+    meson
+    ninja
+    pkg-config
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    libGL
+    libgbm
+    libdrm
+    wayland
+    wayland-protocols
+    eglexternalplatform
+  ];
+
+  absolutizeEglExternalPlatformIcdJson = true;
+
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  meta = {
+    description = "Dma-buf-based Wayland external platform library";
+    homepage = "https://github.com/NVIDIA/egl-wayland2/";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
