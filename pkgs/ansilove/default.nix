87b8d082823d624c31f9944d2015dc40b2053319
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 08:07:15 -0700

    ansilove: init at 4.2.2

---
+++ pkgs/ansilove/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  libansilove,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "ansilove";
+  version = "4.2.2";
+
+  src = fetchFromGitHub {
+    owner = "ansilove";
+    repo = "ansilove";
+    tag = finalAttrs.version;
+    hash = "sha256-U8SKh+GBwtuJbHeB7x430YmbOdS38CIBsNXCWvs8XY8=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  buildInputs = [ libansilove ];
+
+  meta = {
+    description = "ANSI and ASCII art to PNG converter in C";
+    homepage = "https://github.com/ansilove/ansilove";
+    changelog = "https://github.com/ansilove/ansilove/blob/${finalAttrs.src.rev}/ChangeLog";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+    mainProgram = "ansilove";
+    platforms = lib.platforms.unix;
+  };
+})
