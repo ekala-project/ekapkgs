5a32ab5c44209be09ff4f57edd46bfd9e65c8f45
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 22:42:57 -0700

    font-util: init at 1.4.2

---
+++ pkgs/font-util/default.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  testers,
+  autoreconfHook,
+  util-macros,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "font-util";
+  version = "1.4.2";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    group = "xorg";
+    owner = "font";
+    repo = "util";
+    tag = "font-util-${finalAttrs.version}";
+    hash = "sha256-tB6A5ezfHwzhL3HsWPZjX3/d53Zkm4hBFbxOnTUgNZc=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    util-macros
+  ];
+
+  passthru = {
+    tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+  };
+
+  meta = {
+    description = "X.Org font package creation/installation utilities";
+    homepage = "https://gitlab.freedesktop.org/xorg/font/util";
+    license = with lib.licenses; [
+      mit
+      bsd2
+      bsdSourceCode
+      mitOpenGroup
+      # there is a bit of a diff, but i think its close enough
+      # it was probably just adapted a bit to fit to the repository structure
+      # or its an older version that the one on spdx
+      unicodeTOU
+    ];
+    maintainers = [ ];
+    pkgConfigModules = [ "fontutil" ];
+    platforms = lib.platforms.unix;
+  };
+})
