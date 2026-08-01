39fbf0efbe1e3d41454c60ac69634b2d99a426f6
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 15:43:43 -0700

    natscli: init at 0.4.0

---
+++ pkgs/natscli/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "natscli";
+  version = "0.4.0";
+
+  src = fetchFromGitHub {
+    owner = "nats-io";
+    repo = "natscli";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-NF2A4bkGczaH+TYwQnLSvt21uQIk5FZomQuVl22CP30=";
+  };
+
+  proxyVendor = true;
+  vendorHash = "sha256-yHLJWpdCUISdehE9nGiodHRrlnIR9j17ua1gBa3JGYA=";
+
+  subPackages = [ "nats" ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.version=${finalAttrs.version}"
+  ];
+  preCheck = ''
+    # Remove tests that depend on CLI output
+    substituteInPlace internal/asciigraph/asciigraph_test.go \
+      --replace-fail "TestPlot" "SkipPlot"
+  '';
+  meta = {
+    description = "NATS Command Line Interface";
+    homepage = "https://github.com/nats-io/natscli";
+    changelog = "https://github.com/nats-io/natscli/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    mainProgram = "nats";
+  };
+})
