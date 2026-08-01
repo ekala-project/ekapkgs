22a3088e984fcc3bffc5b97531e7114b1d7fd070
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 21:24:23 -0700

    font-cursor-misc: init at 1.0.4

---
+++ pkgs/font-cursor-misc/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  bdftopcf,
+  mkfontscale,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "font-cursor-misc";
+  version = "1.0.4";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/font/font-cursor-misc-${finalAttrs.version}.tar.xz";
+    hash = "sha256-JdnJWVATy4yghCBQmZOmQ0yRflPKH+w/Y6zUWhnU+YI=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    bdftopcf
+    mkfontscale
+  ];
+
+  meta = {
+    description = "X Cursor as a pcf font";
+    homepage = "https://gitlab.freedesktop.org/xorg/font/cursor-misc";
+    # "These ""glyphs"" are unencumbered"
+    license = lib.licenses.publicDomain;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
