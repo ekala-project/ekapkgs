2f74ced0522a72d2675399c67819b5e3790ad152
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 09:10:48 -0700

    pacparser: init at 1.5.1

---
+++ pkgs/pacparser/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "pacparser";
+  version = "1.5.1";
+
+  src = fetchFromGitHub {
+    owner = "manugarg";
+    repo = "pacparser";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-CkaRgm5xZHKiewPDSp0bzVkgAOeTbuGrY3FM4HaN97I=";
+  };
+
+  makeFlags = [
+    "NO_INTERNET=1"
+    "PREFIX=${placeholder "out"}"
+    "VERSION=v${finalAttrs.version}"
+  ];
+
+  enableParallelBuilding = true;
+
+  preConfigure = ''
+    patchShebangs tests/runtests.sh
+    cd src
+  '';
+
+  hardeningDisable = [ "format" ];
+
+  versionCheckProgramArg = "-v";
+  meta = {
+    description = "Library to parse proxy auto-config (PAC) files";
+    homepage = "https://pacparser.manugarg.com/";
+    license = lib.licenses.lgpl3;
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+    mainProgram = "pactester";
+  };
+})
