1ed1edd84aff5649da337ca7cad8bb2e1c46a033
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 22:42:57 -0700

    font-misc-cyrillic: init at 1.0.4

---
+++ pkgs/font-misc-cyrillic/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  bdftopcf,
+  mkfontscale,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "font-misc-cyrillic";
+  version = "1.0.4";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/font/font-misc-cyrillic-${finalAttrs.version}.tar.xz";
+    hash = "sha256-dgIaf1MGQAGRSlf9CO+uV/draPCiTcqKsbJFR07o6ZM=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    bdftopcf
+    mkfontscale
+  ];
+
+  meta = {
+    description = "Misc Cyrillic pcf fonts";
+    homepage = "https://gitlab.freedesktop.org/xorg/font/misc-cyrillic";
+    license = with lib.licenses; [
+      publicDomain
+      cronyx
+      # misc free
+      # "May be distributed and modified without restrictions."
+      free
+    ];
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
