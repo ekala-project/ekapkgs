ed82d7366e06f929e254bd06761126188bf98709
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 07:23:40 -0700

    clipper2: init at 2.0.1

---
+++ pkgs/clipper2/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "clipper2";
+  version = "2.0.1";
+
+  src = fetchFromGitHub {
+    owner = "AngusJohnson";
+    repo = "Clipper2";
+    tag = "Clipper2_${finalAttrs.version}";
+    hash = "sha256-Pqmrj9SDooM+VU4ObQrtaU9+GN//FsD+Brp+OsN0cPM=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/CPP";
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  cmakeFlags = [
+    "-DCLIPPER2_EXAMPLES=OFF"
+    "-DCLIPPER2_TESTS=OFF"
+    "-DBUILD_SHARED_LIBS=ON"
+  ];
+
+  meta = {
+    description = "Polygon Clipping and Offsetting - C++ Only";
+    longDescription = ''
+      The Clipper2 library performs intersection, union, difference and XOR boolean operations on both simple and
+      complex polygons. It also performs polygon offsetting.
+    '';
+    homepage = "https://github.com/AngusJohnson/Clipper2";
+    license = lib.licenses.boost;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
