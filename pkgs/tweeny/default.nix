6c1caa290842d641ec62f5dd6815677c6bcf0728
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 16:55:09 -0700

    tweeny: init at 3.2.1

---
+++ pkgs/tweeny/default.nix
@@ -0,0 +1,33 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tweeny";
+  version = "3.2.1";
+
+  src = fetchFromGitHub {
+    owner = "mobius3";
+    repo = "tweeny";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-9nFEI4gignIJaBFW9GDuSJJqeWli6YcKs/uYEL89VsE=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  doCheck = true;
+
+  meta = {
+    description = "Modern C++ tweening library";
+    license = lib.licenses.mit;
+    homepage = "http://mobius3.github.io/tweeny";
+    maintainers = [ ];
+    platforms = with lib.platforms; darwin ++ linux;
+  };
+})
