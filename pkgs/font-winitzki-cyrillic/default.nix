e86e4eb547d34a8de89dd224f78abc527e3781ae
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 22:42:57 -0700

    font-winitzki-cyrillic: init at 1.0.4

---
+++ pkgs/font-winitzki-cyrillic/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  bdftopcf,
+  mkfontscale,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "font-winitzki-cyrillic";
+  version = "1.0.4";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/font/font-winitzki-cyrillic-${finalAttrs.version}.tar.xz";
+    hash = "sha256-O22CEi3BR3bjr82HeDOng04fkAxT/Bx7stZ8eBz6l6g=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    bdftopcf
+    mkfontscale
+  ];
+
+  meta = {
+    description = "Winitzki Proof Cyrillic pcf font";
+    homepage = "https://gitlab.freedesktop.org/xorg/font/winitzki-cyrillic";
+    license = lib.licenses.publicDomain;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
