0907391e9694b1124b70b8e0899451941f64c121
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 15:38:56 -0700

    chess-tui: init at 2.7.1

---
+++ pkgs/chess-tui/default.nix
@@ -0,0 +1,48 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  openssl,
+  pkg-config,
+  alsa-lib,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "chess-tui";
+  version = "2.7.1";
+
+  src = fetchFromGitHub {
+    owner = "thomas-mauran";
+    repo = "chess-tui";
+    tag = finalAttrs.version;
+    hash = "sha256-B6CpUha5e2W82HnWOxV2arHAiqJCyL5bwkhELYQPxMg=";
+  };
+
+  cargoHash = "sha256-Vik4FceQSYnziDpAqz7r7gpabUp2JL5u40iT0r8fnAw=";
+
+  checkFlags = [
+    # assertion failed: result.is_ok()
+    "--skip=tests::test_config_create"
+  ];
+
+  buildInputs = [
+    openssl
+  ]
+  # alsa-lib is required for the alsa-sys. alsa-lib does not compile on darwin
+  ++ lib.optionals stdenv.hostPlatform.isLinux [ alsa-lib ]
+  # bindgenHook is required for coreaudio-sys on darwin
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [ rustPlatform.bindgenHook ];
+
+  nativeBuildInputs = [ pkg-config ];
+
+  env.PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
+
+  meta = {
+    description = "Chess TUI implementation in rust";
+    homepage = "https://github.com/thomas-mauran/chess-tui";
+    maintainers = [ ];
+    license = lib.licenses.mit;
+    mainProgram = "chess-tui";
+  };
+})
