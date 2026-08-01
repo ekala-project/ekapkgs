4f4d7153fbd3937bd276b55f065567ac8b10aa69
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 16:05:34 -0700

    countryfetch: init at 0.2.0

---
+++ pkgs/countryfetch/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "countryfetch";
+  version = "0.2.0";
+
+  src = fetchFromGitHub {
+    owner = "nik-rev";
+    repo = "countryfetch";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-povKd1Y/2Mi+6yJd9+RsJ4F19/wvXvBOK2Jgbs4UnP0=";
+  };
+
+  cargoHash = "sha256-0ZBhRheJGapPqVieXbIpoboVV4RLXan042u5SSgrYQk=";
+
+  env = {
+    OPENSSL_NO_VENDOR = true;
+  };
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ openssl ];
+
+  cargoBuildFlags = [ "--package=countryfetch" ];
+
+  meta = {
+    description = "Command-line tool similar to Neofetch for obtaining information about your country";
+    homepage = "https://github.com/nik-rev/countryfetch";
+    changelog = "https://github.com/nik-rev/countryfetch/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "countryfetch";
+  };
+})
