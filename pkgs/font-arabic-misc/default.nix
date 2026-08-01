40cdb7af2864e347dc7d55ae765f6e1ced662ebf
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 21:24:22 -0700

    font-arabic-misc: init at 1.0.4

---
+++ pkgs/font-arabic-misc/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  bdftopcf,
+  mkfontscale,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "font-arabic-misc";
+  version = "1.0.4";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/font/font-arabic-misc-${finalAttrs.version}.tar.xz";
+    hash = "sha256-Rv/mG1LHih0tynD/IKny2E1pdEY5yrmghcen7hdmNGc=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    bdftopcf
+    mkfontscale
+  ];
+
+  meta = {
+    description = "Arabic newspaper pcf font";
+    homepage = "https://gitlab.freedesktop.org/xorg/font/arabic-misc";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
