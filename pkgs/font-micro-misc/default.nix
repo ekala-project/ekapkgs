8c5e53afecd102223a27df816c4f94a3b6f3b3b9
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 22:42:57 -0700

    font-micro-misc: init at 1.0.4

---
+++ pkgs/font-micro-misc/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  bdftopcf,
+  mkfontscale,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "font-micro-misc";
+  version = "1.0.4";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/font/font-micro-misc-${finalAttrs.version}.tar.xz";
+    hash = "sha256-LuC51r166Emv8b2C76tEobazaPu14R0S/38BWj32+UM=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    bdftopcf
+    mkfontscale
+  ];
+
+  meta = {
+    description = "Micro pcf font";
+    homepage = "https://gitlab.freedesktop.org/xorg/font/micro-misc";
+    license = lib.licenses.publicDomain;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
