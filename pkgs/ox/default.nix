f4a5db979b7aff5aa647ca6ab2b6d2112f380628
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 14:22:23 -0700

    ox: init at 0.7.7

---
+++ pkgs/ox/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "ox";
+  version = "0.7.7";
+
+  src = fetchFromGitHub {
+    owner = "curlpipe";
+    repo = "ox";
+    tag = finalAttrs.version;
+    hash = "sha256-h4oC+TRLPKgXid4YIn2TdTxgEBvbBDy66jfbyA5ia4o=";
+  };
+
+  cargoHash = "sha256-Vf5Y/rXykaYkrnTjVMShnGYikDIu2b1l2oDOiB0O95I=";
+
+  nativeInstallCheckInputs = [
+  ];
+  doInstallCheck = true;
+  versionCheckProgram = "${placeholder "out"}/bin/${finalAttrs.meta.mainProgram}";
+
+  meta = {
+    description = "Independent Rust text editor that runs in your terminal";
+    homepage = "https://github.com/curlpipe/ox";
+    changelog = "https://github.com/curlpipe/ox/releases/tag/${finalAttrs.version}";
+    license = lib.licenses.gpl2Only;
+    maintainers = [ ];
+    mainProgram = "ox";
+  };
+})
