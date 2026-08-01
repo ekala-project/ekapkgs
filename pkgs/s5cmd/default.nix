d18667eb0a6eb5e07b75b2a796259994f9c24155
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 12:50:05 -0700

    s5cmd: init at 2.3.0

---
+++ pkgs/s5cmd/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "s5cmd";
+  version = "2.3.0";
+
+  src = fetchFromGitHub {
+    owner = "peak";
+    repo = "s5cmd";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-+wSVJkXmu+1BzvO1o31jhKZLXeG7y+YkABIZZ1TlK/g=";
+  };
+
+  vendorHash = null;
+
+  ldflags = [ "-X github.com/peak/s5cmd/v2/version.Version=v${finalAttrs.version}" ];
+
+  # Skip e2e tests requiring network access
+  excludedPackages = [ "./e2e" ];
+
+  # Fix tests creating network sockets on macOS
+  __darwinAllowLocalNetworking = true;
+  meta = {
+    homepage = "https://github.com/peak/s5cmd";
+    description = "Parallel S3 and local filesystem execution tool";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "s5cmd";
+  };
+})
