d92993fa5b7fc73be2b40c28ab63a735e9a5c6f0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 15:19:50 -0700

    tap: init at 0.1.10

---
+++ pkgs/tap/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  __structuredAttrs = true;
+
+  pname = "tap";
+  version = "0.1.10";
+
+  src = fetchFromGitHub {
+    owner = "bluesky-social";
+    repo = "indigo";
+    rev = "tap-v${finalAttrs.version}";
+    hash = "sha256-nDOLIRWTyj/R0h+70+bGi85RVe2OKLNbnSaKyyqc93Q=";
+  };
+
+  vendorHash = "sha256-s1S+b+QbptqJ2mxqkvsn7M5VWfLrlwpWgRjg6lq2WVE=";
+
+  subPackages = [
+    "cmd/tap"
+  ];
+
+  postPatch = ''
+    substituteInPlace cmd/tap/main.go \
+      --replace-fail "versioninfo.Short()" '"${finalAttrs.version}"' \
+      --replace-fail '"github.com/earthboundkid/versioninfo/v2"' ""
+  '';
+
+  meta = {
+    description = "ATProtocol firehose sync utility";
+    homepage = "https://github.com/bluesky-social/indigo/tree/main/cmd/tap/README.md";
+    license = with lib.licenses; [
+      mit
+      asl20
+    ];
+    maintainers = [ ];
+    mainProgram = "tap";
+  };
+})
