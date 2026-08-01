fa292f841f66954e3a470d76da562dd190a41177
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 08:22:34 -0700

    font-alias: init at 1.0.6

---
+++ pkgs/font-alias/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitLab,
+  autoreconfHook,
+  font-util,
+  util-macros,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "font-alias";
+  version = "1.0.6";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    group = "xorg";
+    owner = "font";
+    repo = "alias";
+    tag = "font-alias-${finalAttrs.version}";
+    hash = "sha256-WGCC4OTerSRf+2sGNqggSBzVVv7gcuP6s3QQHBLahdM=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    font-util
+    util-macros
+  ];
+
+  meta = {
+    description = "Common aliases for Xorg fonts";
+    homepage = "https://gitlab.freedesktop.org/xorg/font/alias";
+    license = with lib.licenses; [
+      cronyx
+      mit
+    ];
+    maintainers = [ ];
+  };
+})
