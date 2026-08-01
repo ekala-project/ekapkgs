74d2c0c22fd63ff87034c57ad1e7f2123501cfbd
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 08:14:43 -0700

    libversion: init at 3.0.4

---
+++ pkgs/libversion/default.nix
@@ -0,0 +1,34 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  lib,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libversion";
+  version = "3.0.4";
+
+  src = fetchFromGitHub {
+    owner = "repology";
+    repo = "libversion";
+    rev = finalAttrs.version;
+    hash = "sha256-USgSwAdRHEepq9ZTDHVWkPsZjljfh9sEWOZRfu0H7Go=";
+  };
+
+  nativeBuildInputs = [
+    cmake.configurePhaseHook
+    cmake
+  ];
+
+  checkTarget = "test";
+  doCheck = true;
+
+  meta = {
+    description = "Advanced version string comparison library";
+    homepage = "https://github.com/repology/libversion";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
