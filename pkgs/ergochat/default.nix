2536ab95b7c1aae3a267a2893e5cc3911699e9d2
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 15:27:29 -0700

    ergochat: init at 2.17.0

---
+++ pkgs/ergochat/default.nix
@@ -0,0 +1,29 @@
+{
+  buildGoModule,
+  fetchFromGitHub,
+  lib,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "ergo";
+  version = "2.17.0";
+
+  src = fetchFromGitHub {
+    owner = "ergochat";
+    repo = "ergo";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-ajLecAgE74Et7XRGtpGoA9DAcSzBEtRzLm47nHn1Amo=";
+  };
+
+  vendorHash = null;
+
+  meta = {
+    changelog = "https://github.com/ergochat/ergo/blob/v${finalAttrs.version}/CHANGELOG.md";
+    description = "Modern IRC server (daemon/ircd) written in Go";
+    mainProgram = "ergo";
+    homepage = "https://github.com/ergochat/ergo";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
