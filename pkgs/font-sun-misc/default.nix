b7daa5399f8f851c90abc48ac769e1e01654f555
Jonathan Ringer <jonringer117@gmail.com>
2026-07-31 22:42:57 -0700

    font-sun-misc: init at 1.0.4

---
+++ pkgs/font-sun-misc/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  bdftopcf,
+  mkfontscale,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "font-sun-misc";
+  version = "1.0.4";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/font/font-sun-misc-${finalAttrs.version}.tar.xz";
+    hash = "sha256-3YTdEW2Sev+k+g+ilyez7PwPBkI4gXwKHlUqCsOE258=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    bdftopcf
+    mkfontscale
+  ];
+
+  meta = {
+    description = "Open Look Glyph and Cursor pcf fonts";
+    homepage = "https://gitlab.freedesktop.org/xorg/font/sun-misc/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+})
