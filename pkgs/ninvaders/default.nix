fe7ce1bd604e2a31406993c2eb35380491fff3fb
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 16:03:22 -0700

    ninvaders: init at 0.1.2

---
+++ pkgs/ninvaders/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  ncurses,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "ninvaders";
+  version = "0.1.2";
+
+  src = fetchFromGitHub {
+    owner = "sf-refugees";
+    repo = "ninvaders";
+    rev = "v${finalAttrs.version}";
+    sha256 = "1wmwws1zsap4bfc2439p25vnja0hnsf57k293rdxw626gly06whi";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+  buildInputs = [ ncurses ];
+
+  meta = {
+    description = "Space Invaders clone based on ncurses";
+    mainProgram = "ninvaders";
+    homepage = "https://ninvaders.sourceforge.net/";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
