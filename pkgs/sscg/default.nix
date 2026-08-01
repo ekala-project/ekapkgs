5c25faff34d89c756063f67dc8b4773f73d1fc38
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 14:16:01 -0700

    sscg: init at 4.0.3

---
+++ pkgs/sscg/default.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  pkg-config,
+  openssl,
+  ding-libs,
+  talloc,
+  popt,
+  help2man,
+  ninja,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "sscg";
+  version = "4.0.3";
+
+  src = fetchFromGitHub {
+    owner = "sgallagher";
+    repo = "sscg";
+    tag = "sscg-${finalAttrs.version}";
+    hash = "sha256-0t3ntUxfh1jFukWGwmdbt0axFfUiH1QEq6wFrGoI7Jk=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    pkg-config
+    ninja
+  ];
+
+  buildInputs = [
+    openssl
+    ding-libs
+    talloc
+    popt
+    help2man
+  ];
+
+  meta = {
+    description = "Simple Signed Certificate Generator";
+    homepage = "https://github.com/sgallagher/sscg";
+    changelog = "https://github.com/sgallagher/sscg/blob/sscg-${finalAttrs.version}";
+    license = lib.licenses.gpl3;
+    maintainers = [ ];
+    mainProgram = "sscg";
+  };
+})
