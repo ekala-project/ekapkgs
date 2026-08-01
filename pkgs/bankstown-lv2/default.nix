c628ef3ed84e3e4382dc27ab774fa83613b087f4
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 14:46:13 -0700

    bankstown-lv2: init at 1.1.0

---
+++ pkgs/bankstown-lv2/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  stdenv,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "bankstown-lv2";
+  version = "1.1.0";
+
+  src = fetchFromGitHub {
+    owner = "chadmed";
+    repo = "bankstown";
+    rev = finalAttrs.version;
+    hash = "sha256-IThXEY+mvT2MCw0PSWU/182xbUafd6dtm6hNjieLlKg=";
+  };
+
+  postPatch = ''
+    substituteInPlace Makefile \
+      --replace-fail "target/release" \
+                     "target/${stdenv.hostPlatform.rust.cargoShortTarget}/$cargoBuildType"
+  '';
+
+  cargoHash = "sha256-eMN95QNnQtC7QDix9g3dwb9ZbtQuiVBj8+R+opFs0KI=";
+
+  dontCargoInstall = true;
+
+  installFlags = [
+    "DESTDIR=$(out)"
+    "LIBDIR=lib"
+  ];
+
+  meta = {
+    homepage = "https://github.com/chadmed/bankstown";
+    description = "Lightweight psychoacoustic bass enhancement plugin";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
