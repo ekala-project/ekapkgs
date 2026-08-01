1c11fd667a10479461816def43c56abfeb884924
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 16:28:38 -0700

    tocaia: init at 0.9.0

---
+++ pkgs/tocaia/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tocaia";
+  version = "0.9.0";
+
+  src = fetchFromGitHub {
+    owner = "manipuladordedados";
+    repo = "tocaia";
+    tag = finalAttrs.version;
+    hash = "sha256-Np+Awn5KGoAbeoUEkcAeVwnNCqI2Iy+19Zj1RkNfgXU=";
+  };
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  nativeInstallCheckInputs = [
+  ];
+  doInstallCheck = true;
+  meta = {
+    description = "Portable TUI Gopher client written in C89 for POSIX systems";
+    homepage = "https://github.com/manipuladordedados/tocaia";
+    changelog = "https://github.com/manipuladordedados/tocaia/releases/tag/${finalAttrs.version}";
+    license = lib.licenses.bsd2;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+    mainProgram = "tocaia";
+  };
+})
