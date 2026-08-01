4659f5a6eb9d49c37298e0ac3b1927d2c5c0aa56
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 15:23:15 -0700

    tarlz: init at 0.29

---
+++ pkgs/tarlz/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  lzip,
+  lzlib,
+  texinfo,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tarlz";
+  version = "0.29";
+  outputs = [
+    "out"
+    "man"
+    "info"
+  ];
+
+  nativeBuildInputs = [
+    lzip
+    texinfo
+  ];
+  buildInputs = [ lzlib ];
+
+  src = fetchurl {
+    url = "mirror://savannah/lzip/tarlz/tarlz-${finalAttrs.version}.tar.lz";
+    sha256 = "sha256-fhJ/HhsbYspuNBf94y864EgbpYhvVqhVOiFbGdgcbBk=";
+  };
+
+  enableParallelBuilding = true;
+  makeFlags = [ "CXX:=$(CXX)" ];
+
+  doCheck = false; # system clock issues
+
+  meta = {
+    homepage = "https://www.nongnu.org/lzip/tarlz.html";
+    description = "Massively parallel combined implementation of the tar archiver and the lzip compressor";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.all;
+    mainProgram = "tarlz";
+  };
+})
