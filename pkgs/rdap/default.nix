7e7614e3ba0622127150db72239241ba9f94087c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 20:30:30 -0700

    rdap: init at 0.10.0

---
+++ pkgs/rdap/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "rdap";
+  version = "0.10.0";
+
+  src = fetchFromGitHub {
+    owner = "openrdap";
+    repo = "rdap";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-dlRIKf/NikCxiKub6qFmC+e3J1XllaVodzVZvUyvycE=";
+  };
+
+  vendorHash = "sha256-F9kwlUwrV6cUT9C/xZx5TyDPoqTt8mt/uh+QYaSCiUw=";
+
+  doCheck = false;
+
+  ldflags = [
+    "-s"
+    "-X=github.com/openrdap/rdap.version=${finalAttrs.version}"
+  ];
+  meta = {
+    description = "Command line client for the Registration Data Access Protocol (RDAP)";
+    homepage = "https://www.openrdap.org/";
+    changelog = "https://github.com/openrdap/rdap/releases/tag/v${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "rdap";
+  };
+})
