606ac2770d4148dcb6b90bd001b178b7dd03ad8f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 18:17:01 -0700

    wayclip: init at 0.5

---
+++ pkgs/wayclip/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  stdenv,
+  fetchFromSourcehut,
+  wayland,
+  wayland-scanner,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "wayclip";
+  version = "0.5";
+
+  outputs = [
+    "out"
+    "man"
+  ];
+
+  src = fetchFromSourcehut {
+    owner = "~noocsharp";
+    repo = "wayclip";
+    rev = finalAttrs.version;
+    hash = "sha256-Uej5ggtlPeDid1yKSfZt5FlCen1GLea6EWa4lL+BPRM=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ wayland-scanner ];
+
+  buildInputs = [ wayland ];
+
+  makeFlags = [ "PREFIX=${placeholder "out"}" ];
+
+  meta = {
+    description = "Wayland clipboard utility";
+    homepage = "https://sr.ht/~noocsharp/wayclip/";
+    license = lib.licenses.isc;
+    maintainers = [ ];
+    mainProgram = "waycopy";
+    inherit (wayland.meta) platforms;
+  };
+})
