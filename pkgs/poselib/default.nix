e68537a93da25cc6c9ab3b122f279bc051a203e0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 19:26:28 -0700

    poselib: init at 2.0.5

---
+++ pkgs/poselib/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  eigen,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "poselib";
+  version = "2.0.5";
+
+  src = fetchFromGitHub {
+    owner = "PoseLib";
+    repo = "PoseLib";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-fARRKT2UoPuuk9FOOsBdrACwGiGXWg/mLV4R0QIjeak=";
+  };
+
+  buildInputs = [ eigen ];
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    pkg-config
+  ];
+  meta = {
+    description = "A collection of minimal solvers for camera pose estimation";
+    homepage = "https://github.com/PoseLib/PoseLib";
+    changelog = "https://github.com/PoseLib/PoseLib/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
