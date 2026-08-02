b0935cd415c1ba86ed52f933189dc687f34644d5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 17:54:26 -0700

    opensmtpd-filter-rspamd: init at 0.1.8

---
+++ pkgs/opensmtpd-filter-rspamd/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "opensmtpd-filter-rspamd";
+  version = "0.1.8";
+
+  src = fetchFromGitHub {
+    owner = "poolpOrg";
+    repo = "filter-rspamd";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-Ud1irvEyYr9QDsm2PsnWoWkXoDH0WWeH73k/IbLrVf4=";
+  };
+
+  vendorHash = "sha256-sNF2c+22FMvKoROkA/3KtSnRdJh4YZLaIx35HD896HI=";
+
+  passthru.tests = {
+  };
+
+  meta = {
+    description = "OpenSMTPD filter integration for the Rspamd daemon";
+    homepage = "https://github.com/poolpOrg/filter-rspamd";
+    license = lib.licenses.isc;
+    maintainers = [ ];
+    mainProgram = "filter-rspamd";
+  };
+})
