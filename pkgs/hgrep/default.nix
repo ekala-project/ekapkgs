8ba502bebd0c7708660a86cf2d6a327169c54ba5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 18:05:58 -0700

    hgrep: init at 0.3.9

---
+++ pkgs/hgrep/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+let
+  version = "0.3.9";
+in
+rustPlatform.buildRustPackage {
+  pname = "hgrep";
+  inherit version;
+
+  src = fetchFromGitHub {
+    owner = "rhysd";
+    repo = "hgrep";
+    tag = "v${version}";
+    hash = "sha256-xBLpEs0PvYb7sIca9yb3vhi2Bsr1BFqB0jlD+bZT2EI=";
+  };
+
+  cargoHash = "sha256-TP+PClv7FX3kRBwJ0RAKbKoTKpi7hTZgw/Z/ktFKbwQ=";
+
+  meta = {
+    description = "Grep with human-friendly search results";
+    homepage = "https://github.com/rhysd/hgrep";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "hgrep";
+  };
+}
