08da9cfbef060c08ebc2abf2d10e0dfec30846c4
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 17:05:33 -0700

    uni-algo: init at 1.2.0

---
+++ pkgs/uni-algo/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "uni-algo";
+  version = "1.2.0";
+
+  src = fetchFromGitHub {
+    owner = "uni-algo";
+    repo = "uni-algo";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-IyQrL/DWDj87GplSGJC4iQJAzNURLh9TRko5l+EIfuU=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  meta = {
+    description = "Unicode Algorithms Implementation for C/C++";
+    homepage = "https://github.com/uni-algo/uni-algo";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+  };
+})
