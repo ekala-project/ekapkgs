a0a177850b01b1b46e992c13d029aec96b3b523d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 18:44:21 -0700

    wtype: init at 0.4

---
+++ pkgs/wtype/default.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+
+  meson,
+  ninja,
+  pkg-config,
+
+  libxkbcommon,
+  wayland,
+  wayland-scanner,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "wtype";
+  version = "0.4";
+
+  src = fetchFromGitHub {
+    owner = "atx";
+    repo = "wtype";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-TfpzAi0mkXugQn70MISyNFOXIJpDwvgh3enGv0Xq8S4=";
+  };
+
+  strictDeps = true;
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    wayland-scanner
+  ];
+  buildInputs = [
+    libxkbcommon
+    wayland
+  ];
+
+  meta = {
+    description = "xdotool type for wayland";
+    homepage = "https://github.com/atx/wtype";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+    mainProgram = "wtype";
+  };
+})
