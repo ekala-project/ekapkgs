081284ec5bc4c3c1db59344a9879ac7de20569ee
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 20:06:35 -0700

    zarchive: init at 0.1.2

---
+++ pkgs/zarchive/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  zstd,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "zarchive";
+  version = "0.1.2";
+
+  src = fetchFromGitHub {
+    owner = "Exzap";
+    repo = "ZArchive";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-hX637O/mVLTzmG0a9swJu9w+3o26VHo+K/9RhMuf1lI=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+  buildInputs = [ zstd ];
+
+  meta = {
+    description = "File archive format supporting random-access reads";
+    homepage = "https://github.com/Exzap/ZArchive";
+    license = lib.licenses.mit0;
+    maintainers = [ ];
+    mainProgram = "zarchive";
+  };
+})
