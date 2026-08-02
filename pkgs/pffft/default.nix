0003015b0ac6995c2d7dd17099bf95d2bbacd1c0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 18:40:07 -0700

    pffft: init at 0-unstable-2025-06-09

---
+++ pkgs/pffft/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+}:
+
+stdenv.mkDerivation {
+  pname = "pffft";
+  version = "0-unstable-2025-06-09";
+
+  src = fetchFromGitHub {
+    owner = "marton78";
+    repo = "pffft";
+    rev = "9ae907aae7a39c08cea398778b9496ba7484423a";
+    sha256 = "sha256-+efWiBrJzC188tDSPHMARRDArzx/4E8GYPMfDHAND8k=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  meta = {
+    description = "Pretty Fast FFT (PFFFT) library";
+    homepage = "https://github.com/marton78/pffft";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
