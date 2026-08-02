7cfbfdac0d1bc646ab8840b6c352808a4fed758f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-01 18:33:31 -0700

    wlrctl: init at 0.2.2

---
+++ pkgs/wlrctl/default.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  stdenv,
+  fetchFromSourcehut,
+  meson,
+  pkg-config,
+  scdoc,
+  ninja,
+  libxkbcommon,
+  wayland,
+  wayland-scanner,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "wlrctl";
+  version = "0.2.2";
+
+  src = fetchFromSourcehut {
+    owner = "~brocellous";
+    repo = "wlrctl";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-5mDcCSHbZMbfXbksAO4YhELznKpanse7jtbtfr09HL0=";
+  };
+
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  depsBuildBuild = [
+    pkg-config
+  ];
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    pkg-config
+    scdoc
+    ninja
+    wayland-scanner
+  ];
+  buildInputs = [
+    libxkbcommon
+    wayland
+  ];
+
+  env.NIX_CFLAGS_COMPILE = "-Wno-error=type-limits";
+
+  meta = {
+    description = "Command line utility for miscellaneous wlroots Wayland extensions";
+    homepage = "https://git.sr.ht/~brocellous/wlrctl";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+    mainProgram = "wlrctl";
+  };
+})
