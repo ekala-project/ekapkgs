a4a0de8130b8d78a829d47050a69688f9001eb65
Jonathan Ringer <jonringer117@gmail.com>
2026-08-03 22:59:23 -0700

    onesixtyone: init at 0.3.4-unstable-2025-08-30

---
+++ pkgs/onesixtyone/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation {
+  pname = "onesixtyone";
+  version = "0.3.4-unstable-2025-08-30";
+
+  src = fetchFromGitHub {
+    owner = "trailofbits";
+    repo = "onesixtyone";
+    rev = "3bedd7cab7fe1bcfc2def208f87fbf10a013efc7";
+    hash = "sha256-9gvulDEaEFZdGl/x5oNHTuMNbBK56dgOydQRyzGO29Q=";
+  };
+
+  buildPhase = ''
+    $CC -o onesixtyone onesixtyone.c
+  '';
+
+  installPhase = ''
+    install -D onesixtyone $out/bin/onesixtyone
+  '';
+  meta = {
+    description = "Fast SNMP Scanner";
+    homepage = "https://github.com/trailofbits/onesixtyone";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+    mainProgram = "onesixtyone";
+  };
+}
