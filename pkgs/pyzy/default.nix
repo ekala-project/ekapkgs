02085baa8b1cc4dbe29d411efdbbfcc4ca14c6c7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-15 08:46:16 -0700

    pyzy: init at 1.1-unstable-2026-02-28

---
+++ pkgs/pyzy/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+  python3,
+  glib,
+  libuuid,
+  sqlite,
+}:
+
+stdenv.mkDerivation {
+  pname = "pyzy";
+  version = "1.1-unstable-2026-02-28";
+
+  src = fetchFromGitHub {
+    owner = "openSUSE";
+    repo = "pyzy";
+    rev = "5ac51d833777a881e80f0b23d704345cf0feb0d0";
+    hash = "sha256-OiFdog34kjmgF2DCnA8LjlZseZPQ8iCYQD4HZKNnCVU=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    python3
+  ];
+
+  buildInputs = [
+    glib
+    libuuid
+    sqlite
+  ];
+
+  postPatch = ''
+    patchShebangs ./data/db/android/create_db.py
+  '';
+  meta = {
+    description = "Chinese PinYin and Bopomofo conversion library";
+    homepage = "https://github.com/openSUSE/pyzy";
+    license = lib.licenses.lgpl21;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+}
