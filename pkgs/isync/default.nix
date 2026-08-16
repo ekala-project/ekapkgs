64a652e46d4e17ca3db2c5ea138446d567be7452
Jonathan Ringer <jonringer117@gmail.com>
2026-08-16 07:46:57 -0700

    isync: init at 1.5.1

---
+++ pkgs/isync/default.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  stdenv,
+  fetchgit,
+  pkg-config,
+  perl,
+  openssl,
+  db,
+  cyrus_sasl,
+  zlib,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "isync";
+  version = "1.5.1";
+
+  src = fetchgit {
+    url = "https://git.code.sf.net/p/isync/isync";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-l0jL4CzAdFtQGekbywic1Kuihy3ZQi4ozhSEcbJI0t0=";
+  };
+
+  env.NIX_CFLAGS_COMPILE = "-DQPRINTF_BUFF=4000";
+
+  autoreconfPhase = ''
+    echo "${finalAttrs.version}" > VERSION
+    ./autogen.sh
+  '';
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    perl
+  ];
+
+  buildInputs = [
+    openssl
+    db
+    cyrus_sasl
+    zlib
+  ];
+
+  doCheck = true;
+
+  meta = {
+    homepage = "https://isync.sourceforge.io";
+    changelog = "https://sourceforge.net/p/isync/isync/ci/v${finalAttrs.version}/tree/NEWS";
+    description = "Free IMAP and MailDir mailbox synchronizer";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+    mainProgram = "mbsync";
+  };
+})
