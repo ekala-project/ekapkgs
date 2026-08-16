63f22bb8e0bf6a7c0b6bc3920d7650db9ce620b7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-16 07:03:50 -0700

    volk: init at 3.2.0

---
+++ pkgs/volk/default.nix
@@ -0,0 +1,46 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  cmake,
+  python3,
+  removeReferencesTo,
+  enableModTool ? true,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "volk";
+  version = "3.2.0";
+
+  src = fetchFromGitHub {
+    owner = "gnuradio";
+    repo = "volk";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-9bURoGyjdNoKCcgGvZL9VygQqUQxOrwthp154Was2/s=";
+    fetchSubmodules = true;
+  };
+
+  cmakeFlags = [ (lib.cmakeBool "ENABLE_MODTOOL" enableModTool) ];
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    python3
+    python3.pkgs.mako
+    removeReferencesTo
+  ];
+
+  postInstall = ''
+    remove-references-to -t ${stdenv.cc} $(readlink -f "''${!outputLib}"/lib/libvolk${stdenv.hostPlatform.extensions.sharedLibrary})
+  '';
+
+  doCheck = true;
+
+  meta = {
+    homepage = "http://libvolk.org/";
+    description = "Vector Optimized Library of Kernels";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
