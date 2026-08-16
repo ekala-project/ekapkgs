e2a1526684d8301477947568700ad753dd9a1323
Jonathan Ringer <jonringer117@gmail.com>
2026-08-16 01:51:57 -0700

    libnftnl: init at 1.3.1

---
+++ pkgs/libnftnl/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  libmnl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  version = "1.3.1";
+  pname = "libnftnl";
+
+  src = fetchurl {
+    url = "https://netfilter.org/projects/libnftnl/files/libnftnl-${finalAttrs.version}.tar.xz";
+    hash = "sha256-YH2ijbpm+97M+O8Tld3tkHfo0Z8plfmk1FqcLwvP+6g=";
+  };
+
+  configureFlags = lib.optional (
+    stdenv.cc.bintools.isLLVM && lib.versionAtLeast stdenv.cc.bintools.version "17"
+  ) "LDFLAGS=-Wl,--undefined-version";
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ libmnl ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "Userspace library providing a low-level netlink API to the in-kernel nf_tables subsystem";
+    homepage = "https://netfilter.org/projects/libnftnl/";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
