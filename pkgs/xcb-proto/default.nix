791449c098fd6ab6a19a45c5a1488df54e03128b
Jonathan Ringer <jonringer117@gmail.com>
2026-08-15 23:40:27 -0700

    xcb-proto: init at 1.17.0

---
+++ pkgs/xcb-proto/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  python3,
+  writeScript,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "xcb-proto";
+  version = "1.17.0";
+
+  src = fetchurl {
+    url = "mirror://xorg/individual/proto/xcb-proto-${finalAttrs.version}.tar.xz";
+    hash = "sha256-LBus0hEPR5n3TebrtxS5TPb4D7ESMWsSGUgP0iViFIw=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    pkg-config
+    python3
+  ];
+
+  meta = {
+    description = "XML-XCB protocol descriptions used by libxcb for the X11 protocol & extensions";
+    homepage = "https://gitlab.freedesktop.org/xorg/proto/xcbproto";
+    license = lib.licenses.x11;
+    maintainers = [ ];
+    pkgConfigModules = [ "xcb-proto" ];
+    platforms = lib.platforms.unix;
+  };
+})
