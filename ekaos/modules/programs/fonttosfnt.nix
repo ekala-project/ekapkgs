8e7eef2d6e24b8c333cbcf0b102b337d840404ba
Jonathan Ringer <jonringer117@gmail.com>
2026-08-16 17:19:02 -0700

    ekaos/programs/fonttosfnt: init module

---
+++ ekaos/modules/programs/fonttosfnt.nix
@@ -0,0 +1,33 @@
+# System-wide fonttosfnt configuration
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.programs.fonttosfnt;
+in
+
+{
+  options.programs.fonttosfnt = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Whether to install fonttosfnt system-wide.";
+    };
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.fonttosfnt;
+      description = "fonttosfnt package to use.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+  };
+}
