daebab244c6a9b8c09672bd0c96139e2fb3976ac
Jonathan Ringer <jonringer117@gmail.com>
2026-08-18 13:59:30 -0700

    simple-scan: init at 50.0

---
+++ pkgs/simple-scan/default.nix
@@ -0,0 +1,80 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  meson,
+  ninja,
+  pkg-config,
+  gettext,
+  itstool,
+  wrapGAppsHook4,
+  cairo,
+  gdk-pixbuf,
+  colord,
+  glib,
+  libadwaita,
+  gtk4,
+  gusb,
+  packagekit ? null,
+  libwebp,
+  libxml2,
+  sane-backends,
+  vala,
+  gobject-introspection,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "simple-scan";
+  version = "50.0";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/simple-scan/${lib.versions.major finalAttrs.version}/simple-scan-${finalAttrs.version}.tar.xz";
+    hash = "sha256-zDK1Ya4icYLTGpRGZjLjEXI3VuOskFOMPH4qLJqqSgk=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    gettext
+    itstool
+    pkg-config
+    wrapGAppsHook4
+    libxml2
+    gobject-introspection # For setup hook
+    vala
+  ];
+
+  buildInputs = [
+    cairo
+    gdk-pixbuf
+    colord
+    glib
+    gusb
+    libadwaita
+    gtk4
+    libwebp
+    sane-backends
+  ]
+  ++ lib.optionals (packagekit != null) [ packagekit ];
+
+  doCheck = true;
+
+  meta = {
+    description = "Simple scanning utility";
+    mainProgram = "simple-scan";
+    longDescription = ''
+      A really easy way to scan both documents and photos. You can crop out the
+      bad parts of a photo and rotate it if it is the wrong way round. You can
+      print your scans, export them to pdf, or save them in a range of image
+      formats. Basically a frontend for SANE - which is the same backend as
+      XSANE uses. This means that all existing scanners will work and the
+      interface is well tested.
+    '';
+    homepage = "https://gitlab.gnome.org/GNOME/simple-scan";
+    changelog = "https://gitlab.gnome.org/GNOME/simple-scan/-/blob/${finalAttrs.version}/NEWS?ref_type=tags";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
