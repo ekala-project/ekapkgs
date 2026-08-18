6a0bfa4a808b054149b68fe32a0dd6136ca86fd9
Jonathan Ringer <jonringer117@gmail.com>
2026-08-18 14:04:02 -0700

    libsigcxx30: init at 3.8.1

---
+++ pkgs/libsigcxx30/default.nix
@@ -0,0 +1,43 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  pkg-config,
+  meson,
+  ninja,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libsigc++";
+  version = "3.8.1";
+
+  src = fetchFromGitHub {
+    owner = "libsigcplusplus";
+    repo = "libsigcplusplus";
+    tag = finalAttrs.version;
+    hash = "sha256-ZV1gcq/efFaf4MkkDZP9Z1isNqwnvUWWouVwtTnpyhc=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    meson.configurePhaseHook
+    ninja
+  ];
+
+  doCheck = true;
+
+  meta = {
+    homepage = "https://libsigcplusplus.github.io/libsigcplusplus/";
+    changelog = "https://github.com/libsigcplusplus/libsigcplusplus/blob/${finalAttrs.src.tag}/NEWS";
+    description = "Typesafe callback system for standard C++";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
