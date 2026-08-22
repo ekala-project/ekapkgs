fcbe96701c0c04fd33c2306c841de6802130663b
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 07:22:46 -0700

    libcec: init at 7.1.1

---
+++ pkgs/libcec/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  udev,
+  libcec_platform,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libcec";
+  version = "7.1.1";
+
+  src = fetchFromGitHub {
+    owner = "Pulse-Eight";
+    repo = "libcec";
+    rev = "libcec-${finalAttrs.version}";
+    sha256 = "sha256-t8GUQKWTcxjyaAlsTP4C+heYiVYowG7x+fmjHPND7As=";
+  };
+
+  # Fix dlopen path
+  postPatch = ''
+    substituteInPlace include/cecloader.h --replace "\"libcec." "\"$out/lib/libcec."
+  '';
+
+  nativeBuildInputs = [
+    pkg-config
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  buildInputs = [
+    libcec_platform
+    udev
+  ];
+
+  cmakeFlags = [
+    "-DBUILD_SHARED_LIBS=1"
+    "-DHAVE_LINUX_API=1"
+  ];
+
+  meta = {
+    description = "Allows you (with the right hardware) to control your device with your TV remote control using existing HDMI cabling";
+    homepage = "http://libcec.pulse-eight.com";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
