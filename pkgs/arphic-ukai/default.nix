54ab0dbeb904ccbe832931f41c3e6095362cf33e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 04:40:03 -0700

    arphic-ukai: init at 0.2.20080216.2

---
+++ pkgs/arphic-ukai/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchurl,
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "arphic-ukai";
+  version = "0.2.20080216.2";
+
+  src = fetchurl {
+    url = "mirror://ubuntu/pool/main/f/fonts-arphic-ukai/fonts-arphic-ukai_${version}.orig.tar.bz2";
+    hash = "sha256-tJaNc1GfT4dH6FVI+4XSG2Zdob8bqQCnxJmXbmqK49I=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    install -D -v ukai.ttc $out/share/fonts/truetype/arphic-ukai.ttc
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "CJK Unicode font Kai style";
+    homepage = "https://www.freedesktop.org/wiki/Software/CJKUnifonts/";
+    license = lib.licenses.arphicpl;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}
