fc8a30cdb30bf68bbedd3736d7b286a21b8759d0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 00:57:23 -0700

    slop: init at 7.7

---
+++ pkgs/slop/default.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  glew,
+  glm,
+  libGLU,
+  libGL,
+  libx11,
+  libxext,
+  libxrender,
+  icu,
+  libsm,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "slop";
+  version = "7.7";
+
+  src = fetchFromGitHub {
+    owner = "naelstrof";
+    repo = "slop";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-oUvzkIGrUTLVLR9Jf//Wh7AmnaNS2JLC3vXWg+w5W6g=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    pkg-config
+  ];
+
+  buildInputs = [
+    glew
+    glm
+    libGLU
+    libGL
+    libx11
+    libxext
+    libxrender
+    icu
+    libsm
+  ];
+
+  meta = {
+    inherit (finalAttrs.src.meta) homepage;
+    description = "Queries for a selection from the user and prints the region to stdout";
+    platforms = lib.platforms.linux;
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+    mainProgram = "slop";
+  };
+})
