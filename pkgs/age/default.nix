1ca3e9575c6c6c802a402efb81464770303609ac
Jonathan Ringer <jonringer117@gmail.com>
2026-08-21 20:23:23 -0700

    age: init at 1.3.1

---
+++ pkgs/age/default.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  buildGo126Module,
+  fetchFromGitHub,
+  installShellFiles,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "age";
+  version = "1.3.1";
+
+  src = fetchFromGitHub {
+    owner = "FiloSottile";
+    repo = "age";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Qs/q3zQYV0PukABBPf/aU5V1oOhw95NG6K301VYJk8A=";
+  };
+
+  vendorHash = "sha256-iVDkYXXR2pXlUVywPgVRNMORxOOEhAmzpSM0xqSQMSQ=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.Version=v${finalAttrs.version}"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  preInstall = ''
+    installManPage doc/*.1
+  '';
+
+  # plugin test is flaky, see https://github.com/FiloSottile/age/issues/517
+  checkFlags = [
+    "-skip"
+    "TestScript/plugin"
+  ];
+
+  meta = {
+    changelog = "https://github.com/FiloSottile/age/releases/tag/v${finalAttrs.version}";
+    homepage = "https://age-encryption.org/";
+    description = "Modern encryption tool with small explicit keys";
+    license = lib.licenses.bsd3;
+    mainProgram = "age";
+    maintainers = [ ];
+  };
+})
