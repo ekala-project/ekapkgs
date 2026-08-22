3258d727fb15e5e61ca243886c694854fa7e03f5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 09:32:36 -0700

    golangci-lint-langserver: init at 0.0.12

---
+++ pkgs/golangci-lint-langserver/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  buildGo126Module,
+  fetchFromGitHub,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "golangci-lint-langserver";
+  version = "0.0.12";
+
+  src = fetchFromGitHub {
+    owner = "nametake";
+    repo = "golangci-lint-langserver";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Sm+y9ccM5zhatzDZ/qDYH4HT2foteCniItdojqnFSxI=";
+  };
+
+  vendorHash = "sha256-kbGTORTTxfftdU8ffsfh53nT7wZldOnBZ/1WWzN89Uc=";
+
+  subPackages = [ "." ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Language server for golangci-lint";
+    homepage = "https://github.com/nametake/golangci-lint-langserver";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "golangci-lint-langserver";
+  };
+})
