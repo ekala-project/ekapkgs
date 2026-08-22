a9630609d06f94d00eae7408b239f370a7e14965
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 09:34:26 -0700

    harper: init at 2.7.0

---
+++ pkgs/harper/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "harper";
+  version = "2.7.0";
+
+  src = fetchFromGitHub {
+    owner = "Automattic";
+    repo = "harper";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-neXBLfpqrrT7GWTEVs03AA5+ixSLIrRUuzRdXsXfS4Q=";
+  };
+
+  cargoHash = "sha256-dp2VPKoOVmB1rD8ET5I3zAvGKZQMmw5wdWUwr3TMk+k=";
+
+  cargoBuildFlags = [
+    "--package=harper-cli"
+    "--package=harper-ls"
+  ];
+
+  cargoTestFlags = [
+    "--package=harper-cli"
+    "--package=harper-ls"
+  ];
+
+  meta = {
+    description = "Grammar Checker for Developers";
+    homepage = "https://github.com/Automattic/harper";
+    changelog = "https://github.com/Automattic/harper/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    mainProgram = "harper-ls";
+  };
+})
