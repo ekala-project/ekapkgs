fac0bb65f61971e629b957c6c356990ff89dacb6
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 09:33:43 -0700

    ifuse: init at 1.2.1

---
+++ pkgs/ifuse/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+  fuse3 ? null,
+  usbmuxd ? null,
+  libimobiledevice ? null,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "ifuse";
+  version = "1.2.1";
+
+  src = fetchFromGitHub {
+    owner = "libimobiledevice";
+    repo = "ifuse";
+    tag = finalAttrs.version;
+    hash = "sha256-STMELfxbWf2W6NKKqBxgbQLZpYXv9N0cDLgHho5PRYM=";
+  };
+
+  env = {
+    VER = finalAttrs.version;
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+
+  buildInputs = [
+    fuse3
+    usbmuxd
+    libimobiledevice
+  ];
+
+  meta = {
+    homepage = "https://github.com/libimobiledevice/ifuse";
+    description = "Fuse filesystem implementation to access the contents of iOS devices";
+    license = lib.licenses.lgpl21Plus;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+    mainProgram = "ifuse";
+  };
+})
