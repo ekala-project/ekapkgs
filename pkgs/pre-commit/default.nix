6ceb84eec478132e85c613fac849bc19883107ca
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 04:54:20 -0700

    pre-commit: init at 4.5.1

---
+++ pkgs/pre-commit/default.nix
@@ -0,0 +1,68 @@
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+  gitMinimal,
+}:
+
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "pre-commit";
+  version = "4.5.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pre-commit";
+    repo = "pre-commit";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-3E/haU7TzTr+Qj3KadC7BYwuECZPa2Q+NvG5e4SSKSA=";
+  };
+
+  patches = [
+    ./languages-use-the-hardcoded-path-to-python-binaries.patch
+    ./hook-tmpl.patch
+    ./pygrep-pythonpath.patch
+  ];
+
+  build-system = with python3Packages; [
+    setuptools
+  ];
+
+  dependencies = with python3Packages; [
+    cfgv
+    identify
+    nodeenv
+    pyyaml
+    toml
+    virtualenv
+  ];
+
+  postPatch = ''
+    substituteInPlace pre_commit/resources/hook-tmpl \
+      --subst-var-by pre-commit $out
+    substituteInPlace pre_commit/languages/python.py \
+      --subst-var-by virtualenv ${python3Packages.virtualenv}
+    substituteInPlace pre_commit/languages/node.py \
+      --subst-var-by nodeenv ${python3Packages.nodeenv}
+
+    patchShebangs pre_commit/resources/hook-tmpl
+  '';
+
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "pre_commit"
+  ];
+
+  # add gitMinimal as fallback, if git is not installed
+  preFixup = ''
+    makeWrapperArgs+=(--suffix PATH : ${lib.makeBinPath [ gitMinimal ]})
+  '';
+
+  meta = {
+    description = "Framework for managing and maintaining multi-language pre-commit hooks";
+    homepage = "https://pre-commit.com/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "pre-commit";
+  };
+})
