50e37c1d9d0119406f5d8a908578a4ef56bbb3fe
Jonathan Ringer <jonringer117@gmail.com>
2026-08-21 20:24:35 -0700

    git-sizer: init at 1.5.0

---
+++ pkgs/git-sizer/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildGo126Module,
+  fetchFromGitHub,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "git-sizer";
+  version = "1.5.0";
+
+  src = fetchFromGitHub {
+    owner = "github";
+    repo = "git-sizer";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-On7QBTzKfnuuzwMQ8m1odxGqfIKL+EDg5V05Kxuhmqw=";
+  };
+
+  vendorHash = "sha256-oRlsD99XiI/0ZWibjyRcycmGab+vMbXrV5hIdIyUDYg=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.BuildVersion=${finalAttrs.version}"
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Compute various size metrics for a Git repository";
+    homepage = "https://github.com/github/git-sizer";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "git-sizer";
+  };
+})
