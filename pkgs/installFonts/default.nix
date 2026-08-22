8e75994ebb18e8cfd6a4e860d6978e4c87353aa1
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 07:35:21 -0700

    installFonts: init

---
+++ pkgs/installFonts/default.nix
@@ -0,0 +1,13 @@
+{
+  lib,
+  makeSetupHook,
+}:
+
+makeSetupHook {
+  name = "install-fonts-hook";
+  meta = {
+    description = "Copies standard font extension into their respective installation path";
+    maintainers = [ ];
+    license = lib.licenses.mit;
+  };
+} ./install-fonts.sh
