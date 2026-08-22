366cb05e7c5f59f4865786b6105c40cfb9fa8af8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-21 20:24:46 -0700

    git-filter-repo: init at 2.47.0

---
+++ pkgs/git-filter-repo/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+  fetchPypi,
+  installShellFiles,
+}:
+
+python3Packages.buildPythonPackage rec {
+  pname = "git-filter-repo";
+  version = "2.47.0";
+  docs_version = "71d71d4be238628bf9cb9b27be79b8bb824ed1a9";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "git_filter_repo";
+    inherit version;
+    hash = "sha256-QRsn5ooIDAemnCM8tSbbwthIsJpy8QR39ERN0IIs8pA=";
+  };
+
+  docs = fetchFromGitHub {
+    owner = "newren";
+    repo = "git-filter-repo";
+    rev = docs_version;
+    hash = "sha256-m9NI7bLR5F+G7f3Dyi4sP6n4qz2i8cdBRuIn0OcpHAw=";
+  };
+
+  postInstall = ''
+    installManPage ${docs}/man1/git-filter-repo.1
+  '';
+
+  build-system = with python3Packages; [ setuptools-scm ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  doCheck = false;
+
+  pythonImportsCheck = [ "git_filter_repo" ];
+
+  meta = {
+    description = "Quickly rewrite git repository history";
+    homepage = "https://github.com/newren/git-filter-repo";
+    changelog = "https://github.com/newren/git-filter-repo/releases/tag/v${version}";
+    license = with lib.licenses; [
+      mit
+      gpl2Plus
+    ];
+    maintainers = [ ];
+    mainProgram = "git-filter-repo";
+  };
+}
