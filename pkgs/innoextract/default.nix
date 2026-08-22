c3d317d19b7627ded137aa0cdaa49cc340e1f639
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 05:21:20 -0700

    innoextract: init at 1.9-unstable-2025-02-06

---
+++ pkgs/innoextract/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  boost,
+  xz,
+}:
+
+stdenv.mkDerivation {
+  pname = "innoextract";
+  version = "1.9-unstable-2025-02-06";
+
+  src = fetchFromGitHub {
+    owner = "dscharrer";
+    repo = "innoextract";
+    rev = "6e9e34ed0876014fdb46e684103ef8c3605e382e";
+    hash = "sha256-bgACPDo1phjIiwi336JEB1UAJKyL2NmCVOhyZxBFLJo=";
+  };
+
+  buildInputs = [
+    xz
+    boost
+  ];
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  strictDeps = true;
+
+  meta = {
+    description = "Tool to unpack installers created by Inno Setup";
+    homepage = "https://constexpr.org/innoextract/";
+    license = lib.licenses.zlib;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    mainProgram = "innoextract";
+  };
+}
