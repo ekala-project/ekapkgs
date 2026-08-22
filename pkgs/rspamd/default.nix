65531f3b8a32406fcdd40a27822677bd38f86a72
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 00:31:56 -0700

    rspamd: init at 4.1.2

---
+++ pkgs/rspamd/default.nix
@@ -0,0 +1,106 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+
+  # build
+  cmake,
+  glib,
+  perl,
+  pkg-config,
+
+  # runtime
+  blas ? null,
+  fmt,
+  icu,
+  jemalloc,
+  lapack ? null,
+  libarchive,
+  libsodium,
+  lua,
+  luajit ? null,
+  openssl,
+  pcre2,
+  ragel,
+  sqlite,
+  vectorscan ? null,
+  xxhash,
+  zstd,
+
+  # flags
+  withBlas ? false,
+  withLuaJIT ? (luajit != null),
+}:
+
+let
+  inherit (lib) cmakeFeature;
+  cmakeBool' = feature: condition: cmakeFeature feature (if condition then "ON" else "OFF");
+in
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "rspamd";
+  version = "4.1.2";
+
+  src = fetchFromGitHub {
+    owner = "rspamd";
+    repo = "rspamd";
+    tag = finalAttrs.version;
+    hash = "sha256-LqFLzOcLyZeDT5kRlENmyTzUhyVC0HBhoSKInMFpqZA=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+    pkg-config
+    perl
+    ragel
+  ];
+
+  buildInputs = [
+    fmt
+    glib
+    icu
+    jemalloc
+    libarchive
+    libsodium
+    (if withLuaJIT then luajit else lua)
+    openssl
+    pcre2
+    ragel
+    sqlite
+    xxhash
+    zstd
+  ]
+  ++ lib.optionals (vectorscan != null) [ vectorscan ]
+  ++ lib.optionals withBlas (
+    lib.filter (x: x != null) [
+      blas
+      lapack
+    ]
+  );
+
+  cmakeFlags = [
+    (cmakeFeature "RUNDIR" "/run/rspamd")
+    (cmakeFeature "DBDIR" "/var/lib/rspamd")
+    (cmakeFeature "LOGDIR" "/var/log/rspamd")
+    (cmakeFeature "LOCAL_CONFDIR" "/etc/rspamd")
+    (cmakeBool' "ENABLE_BLAS" withBlas)
+    (cmakeBool' "ENABLE_HYPERSCAN" (vectorscan != null))
+    (cmakeBool' "ENABLE_JEMALLOC" true)
+    (cmakeBool' "ENABLE_LUAJIT" withLuaJIT)
+    (cmakeBool' "ENABLE_PCRE2" true)
+    (cmakeBool' "SYSTEM_DOCTEST" false)
+    (cmakeBool' "SYSTEM_XXHASH" true)
+    (cmakeBool' "SYSTEM_ZSTD" true)
+  ];
+
+  meta = {
+    changelog = "https://github.com/rspamd/rspamd/releases/tag/${finalAttrs.src.tag}";
+    homepage = "https://rspamd.com";
+    license = lib.licenses.asl20;
+    description = "Advanced spam filtering system";
+    maintainers = [ ];
+    mainProgram = "rspamd";
+    platforms = with lib.platforms; linux;
+  };
+})
