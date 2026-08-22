1f58ac944ad8b0d3a0adae3e3a11c28d698134fa
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 09:34:25 -0700

    chars: init at 0.7.0

---
+++ pkgs/chars/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "chars";
+  version = "0.7.0";
+
+  src = fetchFromGitHub {
+    owner = "boinkor-net";
+    repo = "chars";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-mBtwdPzIc6RgEFTyReStFlhS4UhhRWjBTKT6gD3tzpQ=";
+  };
+
+  cargoHash = "sha256-Df+twOjzfq+Vxzuv+APiy94XmhBajgk+6+1BRFf+xm0=";
+
+  meta = {
+    description = "Commandline tool to display information about unicode characters";
+    mainProgram = "chars";
+    homepage = "https://github.com/boinkor-net/chars";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
