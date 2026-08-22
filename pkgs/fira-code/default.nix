b06452929fbc4dcc5e2dc43448937c925e17f862
Jonathan Ringer <jonringer117@gmail.com>
2026-08-21 23:26:09 -0700

    fira-code: init at 6.2

---
+++ pkgs/fira-code/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+  useVariableFont ? true,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "fira-code";
+  version = "6.2";
+
+  src = fetchzip {
+    url = "https://github.com/tonsky/FiraCode/releases/download/${finalAttrs.version}/Fira_Code_v${finalAttrs.version}.zip";
+    stripRoot = false;
+    hash = "sha256-UHOwZL9WpCHk6vZaqI/XfkZogKgycs5lWg1p0XdQt0A=";
+  };
+
+  # only extract the variable font because everything else is a duplicate
+  preInstall = "cd ${lib.optionalString useVariableFont "variable_"}ttf";
+
+  installPhase = ''
+    runHook preInstall
+    find . -iname '*.ttf' -exec install -m644 -D -t "$out/share/fonts/truetype" {} +
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://github.com/tonsky/FiraCode";
+    description = "Monospace font with programming ligatures";
+    longDescription = ''
+      Fira Code is a monospace font extending the Fira Mono font with
+      a set of ligatures for common programming multi-character
+      combinations.
+    '';
+    license = lib.licenses.ofl;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
