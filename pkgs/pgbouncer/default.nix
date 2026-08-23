7db6ec36aea987991c7e5abc2b203a2bd2473960
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 07:32:26 -0700

    pgbouncer: init at 1.25.2

---
+++ pkgs/pgbouncer/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  openssl,
+  libevent,
+  c-ares,
+  pkg-config,
+  python3,
+  pandoc ? null,
+  systemd,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "pgbouncer";
+  version = "1.25.2";
+
+  src = fetchurl {
+    url = "https://www.pgbouncer.org/downloads/files/${finalAttrs.version}/pgbouncer-${finalAttrs.version}.tar.gz";
+    hash = "sha256-kkrTURP9CnHI4tvoW10DRFUy4rezep+KSJg77qI4szI=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    python3
+    pandoc
+  ];
+
+  buildInputs = [
+    libevent
+    openssl
+    c-ares
+  ]
+  ++ lib.optional stdenv.hostPlatform.isLinux systemd;
+
+  enableParallelBuilding = true;
+
+  configureFlags = lib.optional stdenv.hostPlatform.isLinux "--with-systemd";
+
+  meta = {
+    homepage = "https://www.pgbouncer.org/";
+    mainProgram = "pgbouncer";
+    description = "Lightweight connection pooler for PostgreSQL";
+    changelog = "https://github.com/pgbouncer/pgbouncer/releases/tag/pgbouncer_${
+      lib.replaceStrings [ "." ] [ "_" ] finalAttrs.version
+    }";
+    license = lib.licenses.isc;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
