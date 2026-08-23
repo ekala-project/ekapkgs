4567dcb7c2a38656494e25f375b342a2e3c506e4
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 21:51:07 -0700

    cargo-leptos: init at 0.3.7

---
+++ pkgs/cargo-leptos/default.nix
@@ -0,0 +1,41 @@
+{
+  fetchFromGitHub,
+  lib,
+  rustPlatform,
+  pkg-config,
+  openssl,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "cargo-leptos";
+  version = "0.3.7";
+
+  src = fetchFromGitHub {
+    owner = "leptos-rs";
+    repo = "cargo-leptos";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-w1kd/eOjNHYAramUvHdgj0ogFqgHDQ1P+ItKTTLL9hU=";
+  };
+
+  cargoHash = "sha256-p0mku5B9RtU0E7ny1Izhr2diBLgDH8HR2/B92MvBfws=";
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ openssl ];
+
+  env = {
+    OPENSSL_NO_VENDOR = 1;
+  };
+
+  # https://github.com/leptos-rs/cargo-leptos#dependencies
+  buildFeatures = [ "no_downloads" ];
+  doCheck = false;
+
+  meta = {
+    description = "Build tool for the Leptos web framework";
+    mainProgram = "cargo-leptos";
+    homepage = "https://github.com/leptos-rs/cargo-leptos";
+    changelog = "https://github.com/leptos-rs/cargo-leptos/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
