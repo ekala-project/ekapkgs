5ffaa2166cfd464b571d737ef1d464929be4defe
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 05:31:32 -0700

    csvkit: init at 2.2.0

---
+++ pkgs/csvkit/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  python3Packages,
+  fetchPypi,
+}:
+
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "csvkit";
+  version = "2.2.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-FHMYqNuuwHwLu5KRwUt43l+jLtPUpcI5blKoPAow32s=";
+  };
+
+  pythonRemoveDeps = [ "agate-dbf" ];
+
+  build-system = with python3Packages; [ setuptools ];
+
+  dependencies = with python3Packages; [
+    agate
+    agate-excel
+    agate-sql
+    setuptools
+  ];
+
+  doCheck = false;
+
+  meta = {
+    homepage = "https://github.com/wireservice/csvkit";
+    description = "Suite of command-line tools for converting to and working with CSV";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
