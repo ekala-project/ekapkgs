55ed2791df97d07cd0acc32cc2c49c1fdb6da7e3
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 21:51:37 -0700

    goredo: init at 2.6.0

---
+++ pkgs/goredo/default.nix
@@ -0,0 +1,50 @@
+{
+  buildGo126Module,
+  fetchurl,
+  lib,
+  zstd,
+  python3 ? null,
+  perl ? null,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "goredo";
+  version = "2.6.0";
+
+  src = fetchurl {
+    url = "http://www.goredo.cypherpunks.ru/download/goredo-${finalAttrs.version}.tar.zst";
+    hash = "sha256-XTL/otfCKC55TsUBBVors2kgFpOFh+6oekOOafOhcUs=";
+  };
+
+  nativeBuildInputs = [ zstd ];
+
+  vendorHash = null;
+
+  modRoot = "./src";
+  subPackages = [ "." ];
+
+  postBuild = ''
+    ( cd $GOPATH/bin; ./goredo -symlinks )
+    cd ..
+  '';
+
+  doCheck = false;
+
+  postInstall = ''
+    mkdir -p "$out/share/info"
+    cp goredo.info "$out/share/info"
+  '';
+
+  outputs = [
+    "out"
+    "info"
+  ];
+
+  meta = {
+    outputsToInstall = [ "out" ];
+    description = "Makefile replacement that sucks less";
+    homepage = "https://www.goredo.cypherpunks.ru";
+    license = lib.licenses.gpl3;
+    maintainers = [ ];
+  };
+})
