9422b70adb68cf0d947538888aae81127ee829ba
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 04:44:50 -0700

    fdroidcl: init at 0.8.1

---
+++ pkgs/fdroidcl/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildGo126Module,
+  fetchFromGitHub,
+  android-tools ? null,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "fdroidcl";
+  version = "0.8.1";
+
+  src = fetchFromGitHub {
+    owner = "Hoverth";
+    repo = "fdroidcl";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-SmPtMNHxktyhc0/izWmAzcfCXqF2BpPNJjsrqRlU1K0=";
+  };
+
+  vendorHash = "sha256-PNj5gkFj+ruxv1I4SezJxebDO2e1qGTYp3ZgekRLNt0=";
+
+  postPatch = lib.optionalString (android-tools != null) ''
+    substituteInPlace adb/{server,device}.go \
+      --replace 'exec.Command("adb"' 'exec.Command("${android-tools}/bin/adb"'
+  '';
+
+  # TestScript/search attempts to connect to fdroid
+  doCheck = false;
+
+  meta = {
+    description = "F-Droid command line interface written in Go";
+    mainProgram = "fdroidcl";
+    homepage = "https://github.com/Hoverth/fdroidcl";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
