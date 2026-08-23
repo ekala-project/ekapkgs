989a7a07dc398445e92800fc04a01d27ab1e9901
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 04:53:09 -0700

    kubelogin: init at 0.2.14

---
+++ pkgs/kubelogin/default.nix
@@ -0,0 +1,42 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  buildGo126Module,
+  installShellFiles,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "kubelogin";
+  version = "0.2.14";
+
+  src = fetchFromGitHub {
+    owner = "Azure";
+    repo = "kubelogin";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-s9W5wvA4L0Qbn5vimLU03oqx10XqCybE3YvC9gV3y7A=";
+  };
+
+  vendorHash = "sha256-CWgvbN8NnroSVqfKF8UG6kXqVWrQ0TmKwri1f218K+M=";
+
+  ldflags = [
+    "-X main.gitTag=v${finalAttrs.version}"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    $out/bin/kubelogin completion bash >kubelogin.bash
+    $out/bin/kubelogin completion fish >kubelogin.fish
+    $out/bin/kubelogin completion zsh >kubelogin.zsh
+    installShellCompletion kubelogin.{bash,fish,zsh}
+  '';
+
+  meta = {
+    description = "Kubernetes credential plugin implementing Azure authentication";
+    mainProgram = "kubelogin";
+    inherit (finalAttrs.src.meta) homepage;
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
