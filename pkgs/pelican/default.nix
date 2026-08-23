a1522a15cca19c40804f9cebbb8d0cd394fcf643
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 05:24:58 -0700

    pelican: init at 4.12.0

--- pkgs/pelican/default.nix
+++ pkgs/pelican/default.nix
@@ -1,10 +1,10 @@
 {
   lib,
-  python3,
+  python3Packages,
   fetchFromGitHub,
 }:
 
-python3.pkgs.buildPythonPackage rec {
+python3Packages.buildPythonApplication rec {
   pname = "pelican";
   version = "4.12.0";
   pyproject = true;
@@ -19,11 +19,11 @@ python3.pkgs.buildPythonPackage rec {
     '';
   };
 
-  build-system = [ python3.pkgs.pdm-backend ];
+  build-system = with python3Packages; [ pdm-backend ];
 
   pythonRelaxDeps = [ "pygments" ];
 
-  dependencies = with python3.pkgs; [
+  dependencies = with python3Packages; [
     blinker
     docutils
     feedgenerator
@@ -37,10 +37,9 @@ python3.pkgs.buildPythonPackage rec {
     watchfiles
   ];
 
+  # Tests require complex setup
   doCheck = false;
 
-  # We only want to patch shebangs in /bin, and not those
-  # of the project scripts that are created by Pelican.
   dontPatchShebangs = true;
 
   postFixup = ''
@@ -52,7 +51,6 @@ python3.pkgs.buildPythonPackage rec {
   meta = {
     description = "Static site generator that requires no database or server-side logic";
     homepage = "https://getpelican.com/";
-    changelog = "https://github.com/getpelican/pelican/blob/${src.tag}/docs/changelog.rst";
     license = lib.licenses.agpl3Only;
     maintainers = [ ];
   };
