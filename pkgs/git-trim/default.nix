65af8717528bfbc04ec345c0c1a42486da8c26bc
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 22:25:35 -0700

    git-trim: init at 0.4.4

---
+++ pkgs/git-trim/default.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  fetchpatch,
+  pkg-config,
+  openssl,
+  libgit2,
+  zlib,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "git-trim";
+  version = "0.4.4";
+
+  src = fetchFromGitHub {
+    owner = "foriequal0";
+    repo = "git-trim";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-XAO3Qg5I2lYZVNx4+Z5jKHRIFdNwBJsUQwJXFb4CbvM=";
+  };
+
+  cargoHash = "sha256-irgekVTWCujzSbZQMNJw3NZ3cjaUftpSJha6iZQqYJ8=";
+
+  cargoPatches = [
+    (fetchpatch {
+      url = "https://github.com/foriequal0/git-trim/commit/4355cd1d6f605455087c4d7ad16bfb92ffee941f.patch";
+      sha256 = "sha256-C1pX4oe9ZCgvqYTBJeSjMdr0KFyjv2PNVMJDlwCAngY=";
+    })
+  ];
+
+  env.OPENSSL_NO_VENDOR = 1;
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    openssl
+    libgit2
+    zlib
+  ];
+
+  postInstall = ''
+    install -Dm644 -t $out/share/man/man1/ docs/git-trim.1
+  '';
+
+  doCheck = false;
+
+  meta = {
+    description = "Automatically trims your branches whose tracking remote refs are merged or gone";
+    homepage = "https://github.com/foriequal0/git-trim";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "git-trim";
+  };
+})
