2c3e4a9ba0a6d1b6e109cff6ef15e028f49da784
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 05:15:11 -0700

    lxgw-wenkai: init at 1.522

---
+++ pkgs/lxgw-wenkai/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchurl,
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "lxgw-wenkai";
+  version = "1.522";
+
+  src = fetchurl {
+    url = "https://github.com/lxgw/LxgwWenKai/releases/download/v${version}/lxgw-wenkai-v${version}.tar.gz";
+    hash = "sha256-aBp31dACF146nhrw/G+iIBZMya1sFPHoQqU5h4584aQ=";
+  };
+
+  dontPatch = true;
+  dontConfigure = true;
+  dontBuild = true;
+  doCheck = false;
+  dontFixup = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm644 -t $out/share/fonts/truetype/ *.ttf 2>/dev/null || true
+    install -Dm644 -t $out/share/fonts/opentype/ *.otf 2>/dev/null || true
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://lxgw.github.io/";
+    description = "Open-source Chinese font derived from Fontworks' Klee One";
+    license = lib.licenses.ofl;
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+  };
+}
