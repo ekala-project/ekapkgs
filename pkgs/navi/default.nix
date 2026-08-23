db31d752711c3d545bd50a0bd457b403cf6dced8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 05:32:35 -0700

    navi: init at 2.24.0

---
+++ pkgs/navi/default.nix
@@ -0,0 +1,48 @@
+{
+  fetchFromGitHub,
+  lib,
+  makeWrapper,
+  rustPlatform,
+  wget,
+  fzf,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "navi";
+  version = "2.24.0";
+
+  src = fetchFromGitHub {
+    owner = "denisidoro";
+    repo = "navi";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-zvqxVu147u/m/4B3fhbuQ46txGMrlgQv9d4GGiR8SoQ=";
+  };
+
+  cargoHash = "sha256-tQCm8KMVWo6KiKVOMDitHtDXwYGM7INXcT+7fEEiIiI=";
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  postInstall = ''
+    wrapProgram $out/bin/navi \
+      --prefix PATH : "$out/bin" \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          wget
+          fzf
+        ]
+      }
+  '';
+
+  checkFlags = [
+    "--skip=test_parse_variable_line"
+  ];
+
+  meta = {
+    description = "Interactive cheatsheet tool for the command-line and application launchers";
+    homepage = "https://github.com/denisidoro/navi";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.unix;
+    mainProgram = "navi";
+    maintainers = [ ];
+  };
+})
