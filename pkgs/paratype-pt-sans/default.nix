c1ec90ca73a6bb87b65f0d5abbf5e95994fd0283
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 21:57:52 -0700

    paratype-pt-sans: init at 2.003

---
+++ pkgs/paratype-pt-sans/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "paratype-pt-sans";
+  version = "2.003";
+
+  src = fetchFromGitHub {
+    owner = "google";
+    repo = "fonts";
+    rev = "a4f3deeca2d7547351ff746f7bf3b51f5528dbcf";
+    hash = "sha256-o766Pcq/eOyC96TRTRqxBPxiszufhPaZJHtRyDYj0oQ=";
+    rootDir = "ofl/ptsans";
+  };
+
+  dontPatch = true;
+  dontConfigure = true;
+  dontBuild = true;
+  doCheck = false;
+  dontFixup = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    find . -name '*.ttf' -exec install -Dm644 -t $out/share/fonts/truetype/ {} +
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Open Paratype font";
+    homepage = "https://www.paratype.ru/catalog/font/pt/pt-sans";
+    license = lib.licenses.ofl;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}
