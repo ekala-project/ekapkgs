656472b7d5af0595d5470f7f12ed445e502c70a8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 21:51:38 -0700

    roboto-mono: init at 3.001

---
+++ pkgs/roboto-mono/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+  installFonts,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "roboto-mono";
+  version = "3.001";
+
+  outputs = [
+    "out"
+    "webfont"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "googlefonts";
+    repo = "robotomono";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-i0r8x4VgaOYW/pYXK+AXw7jMwhA8Hs9VQ1lq5f/xTe0=";
+  };
+
+  nativeBuildInputs = [ installFonts ];
+
+  meta = {
+    homepage = "https://fonts.google.com/specimen/Roboto+Mono";
+    description = "Google Roboto Mono fonts";
+    longDescription = ''
+      Roboto Mono is a monospaced addition to the Roboto type family. Like
+      the other members of the Roboto family, the fonts are optimized for
+      readability on screens across a wide variety of devices and reading
+      environments. While the monospaced version is related to its variable
+      width cousin, it doesn't hesitate to change forms to better fit the
+      constraints of a monospaced environment. For example, narrow glyphs
+      like 'I', 'l' and 'i' have added serifs for more even texture while
+      wider glyphs are adjusted for weight. Curved caps like 'C' and 'O'
+      take on the straighter sides from Roboto Condensed.
+    '';
+    license = lib.licenses.ofl;
+    platforms = lib.platforms.all;
+    maintainers = [ ];
+  };
+})
