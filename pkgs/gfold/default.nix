a1eb86aa68dcfe3d419eca653cd2f768a59e6b9d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 05:30:19 -0700

    gfold: init at 2026.3.0

---
+++ pkgs/gfold/default.nix
@@ -0,0 +1,28 @@
+{
+  fetchFromGitHub,
+  lib,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "gfold";
+  version = "2026.3.0";
+
+  src = fetchFromGitHub {
+    owner = "nickgerace";
+    repo = "gfold";
+    tag = finalAttrs.version;
+    hash = "sha256-iQWcRApAxWGrztEPtsKeaTWcM8gO0CQUA8tNia+bZ1I=";
+  };
+
+  cargoHash = "sha256-N7dgB0yzL5JSdQOAhNL9pnCSpV/Mo0Phe6ljwipLD/8=";
+
+  meta = {
+    description = "CLI tool to help keep track of your Git repositories, written in Rust";
+    homepage = "https://github.com/nickgerace/gfold";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    mainProgram = "gfold";
+  };
+})
