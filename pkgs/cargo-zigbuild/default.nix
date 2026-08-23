d081872ac86591ac3dfd2e377d1d3490e6fa2d0e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 21:51:08 -0700

    cargo-zigbuild: init at 0.23.0

---
+++ pkgs/cargo-zigbuild/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  makeWrapper,
+  zig,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "cargo-zigbuild";
+  version = "0.23.0";
+
+  src = fetchFromGitHub {
+    owner = "rust-cross";
+    repo = "cargo-zigbuild";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Y73aPGsrSAZVxNJ1r1lS9uXmfpAwthq6NW4urKS8ab0=";
+  };
+
+  cargoHash = "sha256-dt2s18B9RVwEBlun5cegoqfW1KYXFqjdScdc/Q2aDlI=";
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  postInstall = ''
+    wrapProgram $out/bin/cargo-zigbuild \
+      --prefix PATH : ${zig}/bin
+  '';
+
+  meta = {
+    description = "Tool to compile Cargo projects with zig as the linker";
+    mainProgram = "cargo-zigbuild";
+    homepage = "https://github.com/rust-cross/cargo-zigbuild";
+    changelog = "https://github.com/rust-cross/cargo-zigbuild/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
