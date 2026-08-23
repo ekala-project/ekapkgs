2c2cf46510ee6701d55ef43929290e41f4f0d4a0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 05:26:39 -0700

    mkdocs: init at 1.6.1

--- pkgs/mkdocs/default.nix
+++ pkgs/mkdocs/default.nix
@@ -1,10 +1,10 @@
 {
   lib,
-  python3,
+  python3Packages,
   fetchFromGitHub,
 }:
 
-python3.pkgs.buildPythonPackage rec {
+python3Packages.buildPythonApplication rec {
   pname = "mkdocs";
   version = "1.6.1";
   pyproject = true;
@@ -20,17 +20,13 @@ python3.pkgs.buildPythonPackage rec {
     ./click-8.3.0-compat.patch
   ];
 
-  build-system =
-    with python3.pkgs;
-    [
-      hatchling
-      babel
-    ]
-    ++ lib.optionals (lib.versionAtLeast python3.version "3.12") [
-      python3.pkgs.setuptools
-    ];
+  build-system = with python3Packages; [
+    hatchling
+    babel
+    setuptools
+  ];
 
-  dependencies = with python3.pkgs; [
+  dependencies = with python3Packages; [
     click
     ghp-import
     jinja2
@@ -51,12 +47,11 @@ python3.pkgs.buildPythonPackage rec {
   pythonImportsCheck = [ "mkdocs" ];
 
   meta = {
-    changelog = "https://github.com/mkdocs/mkdocs/releases/tag/${version}";
     description = "Project documentation with Markdown / static website generator";
     mainProgram = "mkdocs";
     homepage = "http://mkdocs.org/";
     license = lib.licenses.bsd2;
-    maintainers = [ ];
     platforms = lib.platforms.unix;
+    maintainers = [ ];
   };
 }
