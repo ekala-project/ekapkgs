08604b34fc07f6a5ebd52ffc81d49e6773dab619
Jonathan Ringer <jonringer117@gmail.com>
2026-08-22 22:44:51 -0700

    noti: init at 3.8.0

---
+++ pkgs/noti/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  buildGo126Module,
+  fetchFromCodeberg,
+  installShellFiles,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "noti";
+  version = "3.8.0";
+
+  src = fetchFromCodeberg {
+    owner = "roble";
+    repo = "noti";
+    tag = finalAttrs.version;
+    hash = "sha256-WCkzSvJoSwlloxKD2AseYIwOZTMnSU69y75J7RAl/XE=";
+  };
+
+  vendorHash = null;
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  subPackages = [ "cmd/noti" ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/variadico/noti/internal/command.Version=${finalAttrs.version}"
+  ];
+
+  preCheck = ''
+    export PATH=$out/bin:$PATH
+  '';
+
+  postInstall = ''
+    installManPage docs/man/dist/*
+  '';
+
+  meta = {
+    description = "Monitor a process and trigger a notification";
+    homepage = "https://codeberg.org/roble/noti";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "noti";
+  };
+})
