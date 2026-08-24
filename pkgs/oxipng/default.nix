c5aa1d385dab463e2f21df51feff249a262f2812
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 03:07:31 -0700

    oxipng: 10.1.1 -> 10.2.0

--- pkgs/oxipng/default.nix
+++ pkgs/oxipng/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "oxipng";
-  version = "10.1.1";
+  version = "10.2.0";
 
   src = fetchFromGitHub {
     owner = "shssoichiro";
     repo = "oxipng";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-G06GAlxEVOqt2xHq+JOLSYbsa++aArbu+sb0ypQn9u4=";
+    hash = "sha256-GwpXPkEkGqF55YOszXze0iZPi+sjaxtpcKpznc9CQbI=";
   };
 
-  cargoHash = "sha256-gRWDpxZGy01lWgCIse4Tf7gjwxzosozONB3LD5pX5KQ=";
+  cargoHash = "sha256-rxb2qKS9sNM/+65YVhZ0jUvvZf8lfDJgU3ltY2Vht00=";
 
   postPatch = ''
     rm .cargo/config.toml
