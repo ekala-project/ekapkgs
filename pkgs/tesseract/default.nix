a21812265f1a74cfdd16e0f2426a7126cc475b91
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 06:56:06 -0700

    tesseract: 5.5.1 -> 5.5.3

--- pkgs/tesseract/default.nix
+++ pkgs/tesseract/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation rec {
   pname = "tesseract";
-  version = "5.5.1";
+  version = "5.5.3";
 
   src = fetchFromGitHub {
     owner = "tesseract-ocr";
     repo = "tesseract";
     rev = version;
-    sha256 = "sha256-bLTYdT9CNfgrmmjP6m0rRqJDHiSOkcuGVCFwPqT12jk=";
+    sha256 = "sha256-n+ZtLAVi6+dOusK040i/sSjJqw58Ef62uTeimYbMUHk=";
   };
 
   enableParallelBuilding = true;
