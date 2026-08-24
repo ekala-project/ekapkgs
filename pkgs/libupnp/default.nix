44d9bd73c1648b24a0c5b70a5ac2642b9ff77f20
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 23:37:45 -0700

    libupnp: 1.14.18 -> 22.0.6

--- pkgs/libupnp/default.nix
+++ pkgs/libupnp/default.nix
@@ -7,7 +7,7 @@
 
 stdenv.mkDerivation rec {
   pname = "libupnp";
-  version = "1.14.18";
+  version = "22.0.6";
 
   outputs = [
     "out"
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
     owner = "pupnp";
     repo = "pupnp";
     rev = "release-${version}";
-    sha256 = "sha256-eQKtZioZjI53J1fsoer032pzqebbK5IabOnkAXwBPos=";
+    sha256 = "sha256-RWdPQXlGpg/m6eyshvlQBKizpvLoojdhC4FTeYySSm4=";
   };
 
   nativeBuildInputs = [
