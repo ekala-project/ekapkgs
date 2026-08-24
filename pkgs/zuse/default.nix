d33deb9f560ef3736444e1b307c5eccf66ca06e2
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 19:41:20 -0700

    zuse: init at 1.0

---
+++ pkgs/zuse/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildGo126Module
+, fetchFromGitHub
+,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "zuse";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "babycommando";
+    repo = "zuse";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-zKAAHvbaR1HnRe6CmAEi/6iAOHCcbGVS1/jSrQwksRk=";
+  };
+
+  vendorHash = "sha256-P7T244d1OpFDqwRu3gdEI1OAqq9EbpL1YO2mlTw0Jew=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  meta = {
+    description = "IRC client for the terminal made in Go with Bubbletea";
+    homepage = "https://github.com/babycommando/zuse";
+    changelog = "https://github.com/babycommando/zuse/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    mainProgram = "zuse";
+  };
+})
