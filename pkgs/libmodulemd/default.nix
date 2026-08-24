82a254b43234f9b04bb96051f876d447507ca2da
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 23:09:42 -0700

    libmodulemd: 2.15.0 -> 2.15.3

--- pkgs/libmodulemd/default.nix
+++ pkgs/libmodulemd/default.nix
@@ -2,7 +2,6 @@
   stdenv,
   lib,
   fetchFromGitHub,
-  fetchpatch2,
   pkg-config,
   meson,
   ninja,
@@ -18,7 +17,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libmodulemd";
-  version = "2.15.0";
+  version = "2.15.3";
 
   outputs = [
     "bin"
@@ -32,23 +31,8 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "fedora-modularity";
     repo = "libmodulemd";
     rev = "libmodulemd-${finalAttrs.version}";
-    sha256 = "sha256-mIyrdksyEk1AKV+vw4g8LUwlQRzwwMkPDuCbw2IiNcA=";
+    sha256 = "sha256-mmaW0Yxn4c3FxzSbHoFbamQoza15/4hJx3zbMcm3NYw=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      url = "https://github.com/fedora-modularity/libmodulemd/commit/9d2809090cc0cccd7bab67453dc00cf43a289082.patch";
-      hash = "sha256-dMtc6GN6lIDjUReFUhEFJ/8wosASo3tLu4ve72BCXQ8=";
-    })
-    (fetchpatch2 {
-      url = "https://github.com/fedora-modularity/libmodulemd/commit/29c339a31b1c753dcdef041e5c2e0e600e48b59d.patch";
-      hash = "sha256-uniHrQdbcXlJk2hq106SgV/E330LfxDc07E4FbOMLr0=";
-    })
-    (fetchpatch2 {
-      url = "https://github.com/fedora-modularity/libmodulemd/commit/f3336199b4e69af3305f156abc7533bed9e9a762.patch";
-      hash = "sha256-Rvg+/KTKiEBXVEK7tlcTDf53HkaW462g/rg1rHPzaZA=";
-    })
-  ];
 
   nativeBuildInputs = [
     pkg-config
