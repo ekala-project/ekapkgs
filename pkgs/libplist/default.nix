c371cd2fdfa6f0401c3724b85d5c6eb69d0cda49
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 23:16:09 -0700

    libplist: 2.6.0 -> 2.7.0

--- pkgs/libplist/default.nix
+++ pkgs/libplist/default.nix
@@ -8,7 +8,7 @@
 
 stdenv.mkDerivation rec {
   pname = "libplist";
-  version = "2.6.0";
+  version = "2.7.0";
 
   outputs = [
     "bin"
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
     owner = "libimobiledevice";
     repo = "libplist";
     rev = version;
-    hash = "sha256-hitRcOjbF+L9Og9/qajqFqOhKfRn9+iWLoCKmS9dT80=";
+    hash = "sha256-Rc1KwJR+Pb2lN8019q5ywERrR7WA2LuLRiEvNsZSxXc=";
   };
 
   nativeBuildInputs = [
