5909e2b764b25f007d95734e0868fad44d65f556
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 00:27:28 -0700

    micronaut: 4.10.9 -> 5.1.1

--- pkgs/micronaut/default.nix
+++ pkgs/micronaut/default.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "micronaut";
-  version = "4.10.9";
+  version = "5.1.1";
 
   src = fetchzip {
     url = "https://github.com/micronaut-projects/micronaut-starter/releases/download/v${finalAttrs.version}/micronaut-cli-${finalAttrs.version}.zip";
-    hash = "sha256-/byUsXGPsIkVU7T0FENUkxx/TjkhzI+lX2Veo4JGOCw=";
+    hash = "sha256-hLjvf9BBpcSME/Ed1OEm5rDv9Q47gOiGGmUMAtZIYng=";
   };
 
   nativeBuildInputs = [
