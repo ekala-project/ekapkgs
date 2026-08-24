72767a1cf36fee709a1637c3d87218698897bd59
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 22:20:38 -0700

    lesscpy: 0.15.1 -> 0.15.2

--- pkgs/lesscpy/default.nix
+++ pkgs/lesscpy/default.nix
@@ -2,26 +2,17 @@
   lib,
   python3Packages,
   fetchPypi,
-  fetchpatch,
 }:
 
 python3Packages.buildPythonPackage rec {
   pname = "lesscpy";
-  version = "0.15.1";
+  version = "0.15.2";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-EEXRepj2iGRsp1jf8lTm6cA3RWSOBRoIGwOVw7d8gkw=";
+    hash = "sha256-T2t/NMIsXOE35+4oDcsZhK3g1QC22rWR1KTYQlLRjek=";
   };
-
-  patches = [
-    # remove use of pkg_resources (drop on next release)
-    (fetchpatch {
-      url = "https://github.com/lesscpy/lesscpy/commit/bd8949579713c9d4ff9e15799a26fcecdf73530e.patch";
-      hash = "sha256-U1VDqZqHYaUmND5qCkARyU/eDv2QRhGcCDzuN4+XTbo=";
-    })
-  ];
 
   build-system = with python3Packages; [ setuptools ];
 
