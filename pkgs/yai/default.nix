b6226c87b2712b06a72b127aee8ff182cf7ffb4e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 19:39:07 -0700

    yai: init at 0.6.0

---
+++ pkgs/yai/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildGo126Module
+, fetchFromGitHub
+,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "yai";
+  version = "0.6.0";
+
+  src = fetchFromGitHub {
+    owner = "ekkinox";
+    repo = "yai";
+    rev = finalAttrs.version;
+    sha256 = "sha256-MoblXLfptlIYJbXQTpbc8GBo2a3Zgxdvwra8IUEGiZs==";
+  };
+
+  vendorHash = "sha256-+NhYK8FXd5B3GsGUPJOMM7Tt3GS1ZJ7LeApz38Xkwx8=";
+
+  ldflags = [
+    "-w"
+    "-s"
+    "-X main.buildVersion=${finalAttrs.version}"
+  ];
+
+  preCheck = ''
+    export USER=test
+  '';
+
+  meta = {
+    homepage = "https://github.com/ekkinox/yai";
+    description = "Your AI powered terminal assistant";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "yai";
+  };
+})
