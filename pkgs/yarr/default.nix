0e9cfeb9510def5ccb46a6913a231c986bc9bc75
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 19:22:40 -0700

    yarr: init at 2.6

---
+++ pkgs/yarr/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, buildGo126Module
+, fetchFromGitHub
+,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "yarr";
+  version = "2.6";
+
+  src = fetchFromGitHub {
+    owner = "nkanaev";
+    repo = "yarr";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-D/049qH6CFNL7MY5e54guA9i84pbAwGf2UPHnVQWCkU=";
+  };
+
+  vendorHash = null;
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.Version=${finalAttrs.version}"
+    "-X main.GitHash=none"
+  ];
+
+  tags = [
+    "sqlite_foreign_keys"
+    "sqlite_json"
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Yet another rss reader";
+    mainProgram = "yarr";
+    homepage = "https://github.com/nkanaev/yarr";
+    changelog = "https://github.com/nkanaev/yarr/blob/v${finalAttrs.version}/doc/changelog.md";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
