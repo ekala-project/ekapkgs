b4e0efa70cdc7598dea857384aa20a2f056fa0dc
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 11:15:44 -0700

    zdns: 2.0.5 -> 2.1.1

--- pkgs/zdns/default.nix
+++ pkgs/zdns/default.nix
@@ -6,21 +6,23 @@
 
 buildGoModule (finalAttrs: {
   pname = "zdns";
-  version = "2.0.5";
+  version = "2.1.1";
 
   src = fetchFromGitHub {
     owner = "zmap";
     repo = "zdns";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-e01+TjJETpWNrdtG+lHHGmS9ZS9RijOo5wRnEv6w5jk=";
+    hash = "sha256-wQXw68Ffa6i09+RkIDUQhIhezDR27eSZ0ppBB8/ihOo=";
   };
 
-  vendorHash = "sha256-YlSm4uMDw5I/R4VRpoo5+t/zTwY7J62faodwKlrfbTs=";
+  vendorHash = "sha256-yvrIj2XBBWRlhTYl8sveq9eu6LYRo3aMzRy1Jgj7+f0=";
 
   preCheck = ''
     # Tests require network access
     substituteInPlace src/cli/worker_manager_test.go \
       --replace-fail "TestConvertNameServerStringToNameServer" "SkipTestConvertNameServerStringToNameServer"
+    substituteInPlace src/zdns/zdns_test.go \
+      --replace-fail "TestNetworkConditions" "SkipTestNetworkConditions"
   '';
 
   meta = {
