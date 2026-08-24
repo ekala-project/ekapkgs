360e5f761e799147148f0f1e543ae1705f4bc9ae
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 08:30:23 -0700

    wavpack: 5.8.1 -> 5.9.0

--- pkgs/wavpack/default.nix
+++ pkgs/wavpack/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wavpack";
-  version = "5.8.1";
+  version = "5.9.0";
 
   src = fetchFromGitHub {
     owner = "dbry";
     repo = "WavPack";
     rev = version;
-    hash = "sha256-V9jRIuDpZYIBohJRouGr2TI32BZMXSNVfavqPl56YO0=";
+    hash = "sha256-bG2RGYoJyNX2NObccA3TF1O0Lj/R531hlm/CiNCOCmM=";
   };
 
   outputs = [
