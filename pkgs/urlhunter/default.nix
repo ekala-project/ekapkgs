0081b382c0f8fa28d67a8a34d634693be0532c0d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 19:16:41 -0700

    urlhunter: init at 0.2.0

---
+++ pkgs/urlhunter/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, buildGo126Module
+, fetchFromGitHub
+,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "urlhunter";
+  version = "0.2.0";
+
+  src = fetchFromGitHub {
+    owner = "utkusen";
+    repo = "urlhunter";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-QRQLN8NFIIvlK+sHNj0MMs7tlBODMKHdWJFh/LwnysI=";
+  };
+
+  vendorHash = "sha256-tlFCovCzqgaLcxcGmWXLYUjaAvFG0o11ei8uMzWJs6Q=";
+
+  meta = {
+    description = "Recon tool that allows searching shortened URLs";
+    mainProgram = "urlhunter";
+    homepage = "https://github.com/utkusen/urlhunter";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
