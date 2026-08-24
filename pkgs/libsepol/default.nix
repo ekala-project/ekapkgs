c9c21edb40a9ba77725c0ff325058b6b64a4875b
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 23:24:46 -0700

    libsepol: 3.8.1 -> 3.9

--- pkgs/libsepol/default.nix
+++ pkgs/libsepol/default.nix
@@ -7,7 +7,7 @@
 
 stdenv.mkDerivation rec {
   pname = "libsepol";
-  version = "3.8.1";
+  version = "3.11";
   se_url = "https://github.com/SELinuxProject/selinux/releases/download";
 
   outputs = [
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "${se_url}/${version}/libsepol-${version}.tar.gz";
-    sha256 = "sha256-DnhwUwX5VavUwGVNN6VHfuJjSat0254rA6eGiJeuHd8=";
+    sha256 = "sha256-efPSyI9Et+tc9U2XkuAyMil+F/l6F5Fj8nUAmaAPFk0=";
   };
 
   postPatch = lib.optionalString stdenv.hostPlatform.isStatic ''
