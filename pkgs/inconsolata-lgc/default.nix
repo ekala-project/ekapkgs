5996a53b544b4bc8b8c5ba0cf83edaa4b00ed74b
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 21:00:44 -0700

    inconsolata-lgc: 1.3 -> 3.300

--- pkgs/inconsolata-lgc/default.nix
+++ pkgs/inconsolata-lgc/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "inconsolata-lgc";
-  version = "1.3";
+  version = "3.300";
 
   src = fetchFromGitHub {
     owner = "MihailJP";
     repo = "Inconsolata-LGC";
     rev = "8adfef7a7316fcd2e9a5857054c7cdb2babeb35d";
-    sha256 = "0dqjj3mlc28s8ljnph6l086b4j9r5dly4fldq59crycwys72zzai";
+    sha256 = "sha256-Uf0vjvac+cxSwY064mkrOUmyDALUwGslRRoJRuuQEjc=";
   };
 
   nativeBuildInputs = [ fontforge ];
