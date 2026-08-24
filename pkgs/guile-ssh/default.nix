00435635b83aceacca2a44483bd528a1876ecb13
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 20:07:19 -0700

    guile-ssh: 0.18.0 -> 1.0.0

--- pkgs/guile-ssh/default.nix
+++ pkgs/guile-ssh/default.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "guile-ssh";
-  version = "0.18.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "artyom-poptsov";
     repo = "guile-ssh";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-q7P/ehafnDtJhHOAWbswOfztkKHVtEw8OgcXKufVAX4=";
+    hash = "sha256-gaCFQxn69KENqdET/331Bnznl9uNwgUO2xuIE4Zpxgo=";
   };
 
   patches = [
