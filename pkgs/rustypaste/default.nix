e881c7b709c5e5059bb690ae2b14af6b9ec26bc5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 04:55:56 -0700

    rustypaste: 0.16.1 -> 0.18.0

--- pkgs/rustypaste/default.nix
+++ pkgs/rustypaste/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rustypaste";
-  version = "0.16.1";
+  version = "0.18.0";
 
   src = fetchFromGitHub {
     owner = "orhun";
     repo = "rustypaste";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-Jfi2Q6551g58dfOqtHtWxkbxwYV71f7MIuLB8RbaR94=";
+    sha256 = "sha256-0x28oil0Cn/3ESZU9jN3tT68cTHErdWX3eefgXYLnuQ=";
   };
 
-  cargoHash = "sha256-10tBbn4XtdUNhfzb+KpwFGZAc7YVIEQRaqNLzJC1GGI=";
+  cargoHash = "sha256-Oggu++740APpcTX0U7iXcbMWJosjdo5yeU/4QeVMFrs=";
 
   dontUseCargoParallelTests = true;
 
@@ -23,6 +23,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
     # requires internet access
     "--skip=paste::tests::test_paste_data"
     "--skip=server::tests::test_upload_remote_file"
+    "--skip=util::tests::test_validate_remote_url_valid_http"
+    "--skip=util::tests::test_validate_remote_url_valid_https"
   ];
 
   __darwinAllowLocalNetworking = true;
