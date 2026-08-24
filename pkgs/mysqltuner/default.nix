741851865d0877aeef3221e87f7fbf53ae40a614
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 00:57:45 -0700

    mysqltuner: 2.7.0 -> 2.9.2

--- pkgs/mysqltuner/default.nix
+++ pkgs/mysqltuner/default.nix
@@ -6,13 +6,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "mysqltuner";
-  version = "2.7.0";
+  version = "2.9.2";
 
   src = fetchFromGitHub {
     owner = "major";
     repo = "MySQLTuner-perl";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-v0+iFmAzbFelVyZSRvcSd0AgW73N6no0/n6LuBooKN4=";
+    hash = "sha256-e49JKGwpZv8lr08HtqLKPm0IESTQdna3DmSnPg1rrnE=";
   };
 
   postPatch = ''
