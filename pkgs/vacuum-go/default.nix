0fca1d1f5961ff770137eba1552e2ab8359fc1ad
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 19:31:57 -0700

    vacuum-go: init at 0.30.0

---
+++ pkgs/vacuum-go/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildGo126Module
+, fetchFromGitHub
+,
+}:
+
+buildGo126Module (finalAttrs: {
+  pname = "vacuum-go";
+  version = "0.30.0";
+
+  src = fetchFromGitHub {
+    owner = "daveshanley";
+    repo = "vacuum";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-jWOCStvDpY+luK17H09qysveMXCW+LYbXk+/1Y/4zgs=";
+  };
+
+  vendorHash = "sha256-GOZNxnzk0rpm+rWKSEVsgVyITQRV6hv+sOwOQ9dByF4=";
+
+  env.CGO_ENABLED = 0;
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=v${finalAttrs.version}"
+  ];
+
+  subPackages = [ "./vacuum.go" ];
+
+  doCheck = false;
+
+  meta = {
+    description = "World's fastest OpenAPI & Swagger linter";
+    homepage = "https://quobix.com/vacuum";
+    changelog = "https://github.com/daveshanley/vacuum/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    mainProgram = "vacuum";
+    maintainers = [ ];
+  };
+})
