ec5bf31d112a1bb23998ddc5bb5cc1e7e9db1589
Jonathan Ringer <jonringer117@gmail.com>
2026-08-26 11:26:47 -0700

    hyprland-protocols: init at 0.7.0

---
+++ pkgs/hyprland-protocols/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  gcc15Stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+}:
+gcc15Stdenv.mkDerivation (finalAttrs: {
+  pname = "hyprland-protocols";
+  version = "0.7.0";
+
+  src = fetchFromGitHub {
+    owner = "hyprwm";
+    repo = "hyprland-protocols";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-+KEVnKBe8wz+a6dTLq8YDcF3UrhQElwsYJaVaHXJtoI=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+  ];
+
+  meta = {
+    homepage = "https://github.com/hyprwm/hyprland-protocols";
+    description = "Wayland protocol extensions for Hyprland";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
