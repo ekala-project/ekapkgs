359e119cbb3ee1ad6893cefb92b64840dc99cfa2
Jonathan Ringer <jonringer117@gmail.com>
2026-08-26 17:40:55 -0700

    ekaos/gnome-keyring: init service module

---
+++ ekaos/modules/services/gnome-keyring.nix
@@ -0,0 +1,43 @@
+# GNOME Keyring daemon service
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.services.gnome.gnome-keyring;
+in
+
+{
+  options.services.gnome.gnome-keyring = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to enable GNOME Keyring daemon, a service designed to
+        take care of the user's security credentials, such as user names
+        and passwords.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.gnome-keyring ];
+
+    # TODO: services.dbus.packages = [ pkgs.gnome-keyring pkgs.gcr ];
+    # TODO: xdg.portal.extraPortals = [ pkgs.gnome-keyring ];
+
+    security.pam.services.login.enableGnomeKeyring = true;
+
+    security.wrappers.gnome-keyring-daemon = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_ipc_lock=ep";
+      source = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon";
+    };
+  };
+}
