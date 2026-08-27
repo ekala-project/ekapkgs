d6c1834dbe61c5c10c56334cc478ec630abbc6da
Jonathan Ringer <jonringer117@gmail.com>
2026-08-26 17:40:42 -0700

    gnome-taquin: init at 3.38.1

---
+++ pkgs/gnome-taquin/default.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  fetchpatch,
+  pkg-config,
+  adwaita-icon-theme,
+  gtk3,
+  wrapGAppsHook3,
+  librsvg,
+  gsound,
+  gettext,
+  itstool,
+  # TODO: libxml2 not yet available
+  meson,
+  ninja,
+  vala,
+  python3,
+  desktop-file-utils,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "gnome-taquin";
+  version = "3.38.1";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/gnome-taquin/${lib.versions.majorMinor finalAttrs.version}/gnome-taquin-${finalAttrs.version}.tar.xz";
+    hash = "sha256-lts9j61JeSSnOQrRCKPvckMqHWLSRWyuXhk0BXAYgU8=";
+  };
+
+  patches = [
+    # Fix build with recent Vala.
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gnome-taquin/-/commit/99dea5e7863e112f33f16e59898c56a4f1a547b3.patch";
+      hash = "sha256-U7djuMhb1XJaKAPyogQjaunOkbBK24r25YD7BgH05P4=";
+    })
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gnome-taquin/-/commit/66be44dc20d114e449fc33156e3939fd05dfbb16.patch";
+      hash = "sha256-RN41RCLHlJyXTARSH9qjsmpYi1UFeMRssoYxRsbngDQ=";
+    })
+  ];
+
+  nativeBuildInputs = [
+    pkg-config
+    wrapGAppsHook3
+    meson
+    ninja
+    python3
+    gettext
+    itstool
+    # TODO: libxml2 not yet available
+    vala
+    desktop-file-utils
+  ];
+
+  buildInputs = [
+    gtk3
+    librsvg
+    gsound
+    adwaita-icon-theme
+  ];
+
+  meta = {
+    homepage = "https://gitlab.gnome.org/GNOME/gnome-taquin";
+    description = "Move tiles so that they reach their places";
+    mainProgram = "gnome-taquin";
+    maintainers = [ ];
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.unix;
+  };
+})
