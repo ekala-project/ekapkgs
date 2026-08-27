e21f8c225c5af91aa0c7884d970dde043f579217
Jonathan Ringer <jonringer117@gmail.com>
2026-08-27 07:46:51 -0700

    cage: init at 0.3.1

---
+++ pkgs/cage/default.nix
@@ -0,0 +1,72 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  wayland-scanner,
+  scdoc,
+  makeWrapper,
+  wlroots,
+  wayland,
+  wayland-protocols,
+  pixman,
+  libxkbcommon,
+  libxcb-wm,
+  libGL,
+  libx11,
+  xwayland ? null,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "cage";
+  version = "0.3.1";
+
+  src = fetchFromGitHub {
+    owner = "cage-kiosk";
+    repo = "cage";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-FHIOicRBL881Kvvui4HTKy0g7K9HcQ0ineLECh6MqFI=";
+  };
+
+  depsBuildBuild = [
+    pkg-config
+  ];
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    wayland-scanner
+    scdoc
+    makeWrapper
+  ];
+
+  buildInputs = [
+    wlroots
+    wayland
+    wayland-protocols
+    pixman
+    libxkbcommon
+    libxcb-wm
+    libGL
+    libx11
+  ];
+
+  mesonBuildType = "release";
+
+  postFixup = lib.optionalString wlroots.enableXWayland ''
+    wrapProgram $out/bin/cage --prefix PATH : "${xwayland}/bin"
+  '';
+
+  meta = {
+    description = "Wayland kiosk that runs a single, maximized application";
+    homepage = "https://www.hjdskes.nl/projects/cage/";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+    mainProgram = "cage";
+  };
+})
