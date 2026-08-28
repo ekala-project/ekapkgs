0c134bf16ba4d84aa88a4475201e24280b6bd70b
Jonathan Ringer <jonringer117@gmail.com>
2026-08-27 21:36:46 -0700

    cosmic-protocols: init at 0-unstable-2026-08-14

---
+++ pkgs/cosmic-protocols/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  wayland-scanner,
+}:
+
+stdenv.mkDerivation {
+  pname = "cosmic-protocols";
+  version = "0-unstable-2026-08-14";
+
+  src = fetchFromGitHub {
+    owner = "pop-os";
+    repo = "cosmic-protocols";
+    rev = "a2da48188362c4ea05d33de2f6c67d8148deba88";
+    hash = "sha256-hqsOzu0mlkE2jtgL5HvbT9vtOKiMSniNwV+xk4UzTkc=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ wayland-scanner ];
+
+  makeFlags = [ "PREFIX=${placeholder "out"}" ];
+
+  meta = {
+    homepage = "https://github.com/pop-os/cosmic-protocols";
+    description = "Additional wayland-protocols used by the COSMIC desktop environment";
+    license = with lib.licenses; [
+      mit
+      gpl3Only
+    ];
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+}
