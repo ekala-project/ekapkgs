3fe39852e5c272a16ce05b157b0908ba7c5b88ec
Jonathan Ringer <jonringer117@gmail.com>
2026-09-11 16:57:36 -0700

    nvidia-vaapi-driver: init at 0.0.17

---
+++ pkgs/nvidia-vaapi-driver/default.nix
@@ -0,0 +1,58 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  lib,
+  meson,
+  ninja,
+  pkg-config,
+  libdrm,
+  libGL,
+  gstreamer,
+  nv-codec-headers,
+  libva,
+  addDriverRunpath,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "nvidia-vaapi-driver";
+  version = "0.0.17";
+
+  src = fetchFromGitHub {
+    owner = "elFarto";
+    repo = "nvidia-vaapi-driver";
+    rev = "v${version}";
+    sha256 = "sha256-eJ523lEmB4s+R/QN4J8t6LZ4zw2rEQsaaRBJdjH8Amo=";
+  };
+
+  patches = [
+    ./0001-hardcode-install_dir.patch
+  ];
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    addDriverRunpath
+  ];
+
+  buildInputs = [
+    libdrm
+    libGL
+    gstreamer.gstreamer
+    gstreamer.gst-plugins-bad
+    nv-codec-headers
+    libva
+  ];
+
+  postFixup = ''
+    addDriverRunpath "$out/lib/dri/nvidia_drv_video.so"
+  '';
+
+  meta = {
+    homepage = "https://github.com/elFarto/nvidia-vaapi-driver";
+    description = "VA-API implementation using NVIDIA's NVDEC";
+    changelog = "https://github.com/elFarto/nvidia-vaapi-driver/releases/tag/v${version}";
+    license = lib.licenses.mit;
+  };
+}
