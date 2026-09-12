fb399e5c4341c10d4cadf68ddca61cdaebbc8557
Jonathan Ringer <jonringer117@gmail.com>
2026-09-12 16:08:55 -0700

    typstyle: init at 0.15.0

---
+++ pkgs/typstyle/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "typstyle";
+  version = "0.15.0";
+
+  src = fetchFromGitHub {
+    owner = "typstyle-rs";
+    repo = "typstyle";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-CIIuj8sKy0XTCKbpNGHZQOQp0SszIutjeqhjmDeU2UU=";
+  };
+
+  cargoHash = "sha256-TLYefRh32AP4WozN2nLdXiENbqnXHRqGT4BRJX52MLI=";
+
+  # Disabling tests requiring network access
+  checkFlags = [
+    "--skip=e2e"
+  ];
+
+  meta = {
+    changelog = "https://github.com/typstyle-rs/typstyle/blob/${finalAttrs.src.tag}/CHANGELOG.md";
+    description = "Format your typst source code";
+    homepage = "https://github.com/typstyle-rs/typstyle";
+    license = lib.licenses.asl20;
+    mainProgram = "typstyle";
+  };
+})
