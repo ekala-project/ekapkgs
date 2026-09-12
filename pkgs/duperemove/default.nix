223762532d9d71e8418c0cfa522fad7bb445a486
Jonathan Ringer <jonringer117@gmail.com>
2026-09-12 07:30:34 -0700

    duperemove: init at 0.15.2

---
+++ pkgs/duperemove/default.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  libbsd,
+  libgcrypt,
+  xxhash,
+  pkg-config,
+  glib,
+  linuxHeaders ? stdenv.cc.libc.linuxHeaders,
+  sqlite,
+  util-linux,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "duperemove";
+  version = "0.15.2";
+
+  src = fetchFromGitHub {
+    owner = "markfasheh";
+    repo = "duperemove";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Y3HIqq61bLfZi4XR2RtSyuCPmcWrTxeWvqpTh+3hUjc=";
+  };
+
+  outputs = [
+    "out"
+    "man"
+  ];
+
+  postPatch = ''
+    substituteInPlace util.c --replace-fail \
+      "lscpu" "${lib.getExe' util-linux "lscpu"}"
+  '';
+
+  strictDeps = true;
+  enableParallelBuilding = true;
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [
+    libbsd
+    libgcrypt
+    glib
+    linuxHeaders
+    sqlite
+    util-linux
+    xxhash
+  ];
+
+  makeFlags = [
+    "PREFIX=${placeholder "out"}"
+    "VERSION=v${finalAttrs.version}"
+  ];
+
+  meta = {
+    description = "Simple tool for finding duplicated extents and submitting them for deduplication";
+    homepage = "https://github.com/markfasheh/duperemove";
+    license = lib.licenses.gpl2Only;
+    platforms = lib.platforms.linux;
+    mainProgram = "duperemove";
+  };
+})
