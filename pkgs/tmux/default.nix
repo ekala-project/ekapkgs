44f5a216dc2f9bf0f5efe721a9241f5d7c971f0f
Jonathan Ringer <jonringer117@gmail.com>
2026-09-11 23:44:03 -0700

    tmux: init at 3.7c

---
+++ pkgs/tmux/default.nix
@@ -0,0 +1,73 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  bison,
+  libevent,
+  ncurses,
+  pkg-config,
+  runCommand,
+  withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemdLibs,
+  systemdLibs,
+  withUtf8proc ? !(stdenv.hostPlatform.is32bit),
+  utf8proc,
+  withUtempter ? stdenv.hostPlatform.isLinux,
+  libutempter,
+  withSixel ? true,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tmux";
+  version = "3.7c";
+
+  outputs = [
+    "out"
+    "man"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "tmux";
+    repo = "tmux";
+    tag = finalAttrs.version;
+    hash = "sha256-TpZXTeXKQv6MV1vAPu5MIT52d3Pl6dYcOReZa7QANZY=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    autoreconfHook
+    bison
+  ];
+
+  buildInputs = [
+    ncurses
+    libevent
+  ]
+  ++ lib.optionals withSystemd [ systemdLibs ]
+  ++ lib.optionals withUtf8proc [ utf8proc ]
+  ++ lib.optionals withUtempter [ libutempter ];
+
+  configureFlags = [
+    "--sysconfdir=/etc"
+    "--localstatedir=/var"
+  ]
+  ++ lib.optionals withSystemd [ "--enable-systemd" ]
+  ++ lib.optionals withSixel [ "--enable-sixel" ]
+  ++ lib.optionals withUtempter [ "--enable-utempter" ]
+  ++ lib.optionals withUtf8proc [ "--enable-utf8proc" ];
+
+  enableParallelBuilding = true;
+
+  passthru.terminfo = runCommand "tmux-terminfo" { nativeBuildInputs = [ ncurses ]; } ''
+    mkdir -p $out/share/terminfo/t
+    ln -sv ${ncurses}/share/terminfo/t/{tmux,tmux-256color,tmux-direct} $out/share/terminfo/t
+  '';
+
+  meta = {
+    homepage = "https://tmux.github.io/";
+    description = "Terminal multiplexer";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.unix;
+    mainProgram = "tmux";
+  };
+})
