f72045448b90076f27e7acf2c7b91b539d835d15
Jonathan Ringer <jonringer117@gmail.com>
2026-09-12 16:08:19 -0700

    enblend-enfuse: init at 4.3-unstable-2022-03-06

---
+++ pkgs/enblend-enfuse/default.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  stdenv,
+  fetchhg,
+  autoreconfHook,
+  boost,
+  libglut,
+  glew,
+  gsl,
+  lcms2,
+  libjpeg,
+  libpng,
+  libtiff,
+  libGLU,
+  libGL,
+  vigra,
+  help2man,
+  pkg-config,
+  perl,
+}:
+
+stdenv.mkDerivation {
+  pname = "enblend-enfuse";
+  version = "4.3-unstable-2022-03-06";
+
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  src = fetchhg {
+    url = "http://hg.code.sf.net/p/enblend/code";
+    rev = "0f423c72e51872698fe2985ca3bd453961ffe4e0";
+    hash = "sha256-0gCUSdg3HR3YeIbOByEBCZh2zGlYur6DeCOzUM53fdc=";
+  };
+
+  buildInputs = [
+    boost
+    libglut
+    glew
+    gsl
+    lcms2
+    libjpeg
+    libpng
+    libtiff
+    libGLU
+    libGL
+    vigra
+  ];
+
+  nativeBuildInputs = [
+    autoreconfHook
+    help2man
+    perl
+    pkg-config
+  ];
+
+  preConfigure = ''
+    patchShebangs src/embrace
+  '';
+
+  meta = {
+    homepage = "https://enblend.sourceforge.net/";
+    description = "Blends away the seams in a panoramic image mosaic using a multiresolution spline";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+  };
+}
