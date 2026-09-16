35c03d9e5c7945ca54d77c44a86a9c8b35bb47e9
Jonathan Ringer <jonringer117@gmail.com>
2026-09-16 07:47:34 -0700

    lynx: fix hardening flag pie -> pic

--- pkgs/lynx/default.nix
+++ pkgs/lynx/default.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  hardeningEnable = [ "pie" ];
+  hardeningEnable = [ "pic" ];
 
   configureFlags = [
     "--enable-default-colors"
