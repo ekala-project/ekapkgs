9acf402d1e4d2bca47ad91c5f277a24d3b4e0458
Jonathan Ringer <jonringer117@gmail.com>
2026-09-16 08:17:47 -0700

    top-level: remove libportal-gtk3 alias

--- top-level.nix
+++ top-level.nix
@@ -8,7 +8,6 @@ final: prev: {
   jre = final.jdk;
   libmpg123 = final.mpg123;
   libdbusmenu-gtk3 = final.libdbusmenu.override { withGtk3 = true; };
-  libportal-gtk3 = final.libportal.gtk3;
   docbook_xsl = final.docbook-xsl;
   wafHook = final.waf.hook;
   wrapGAppsHook3 = final.wrapGAppsNoGuiHook.override {
